b79b4043693b66502085a075715ba1e8bab822c3
superworldsun <superworldsun@gmail.com>
2019-04-25 19:54:20 -0400

    Add Items to ItemList.java

--- src/main/java/io/superworldsun/superslegend/init/ItemList.java
+++ src/main/java/io/superworldsun/superslegend/init/ItemList.java
@@ -19,4 +19,68 @@ public class ItemList
 	public static final Item RED_RUPEE = new ItemBase("red_rupee");
 	public static final Item ORANGE_RUPEE = new ItemBase("orange_rupee");
 	
+	public static final Item KOKIRI_SWORD = new ItemBase("kokiri_sword");
+	public static final Item RAZOR_SWORD = new ItemBase("razor_sword");
+	public static final Item GILDED_SWORD = new ItemBase("gilded_sword");
+	
+	public static final Item RUPEE_PICKAXE = new ItemBase("rupee_pickaxe");
+	public static final Item BLUE_RUPEE_PICKAXE = new ItemBase("blue_rupee_pickaxe");
+	public static final Item RED_RUPEE_PICKAXE = new ItemBase("red_rupee_pickaxe");
+	public static final Item ORANGE_RUPEE_PICKAXE = new ItemBase("orange_rupee_pickaxe");
+	
+	public static final Item RUPEE_AXE = new ItemBase("rupee_axe");
+	public static final Item BLUE_RUPEE_AXE = new ItemBase("blue_rupee_axe");
+	public static final Item RED_RUPEE_AXE = new ItemBase("red_rupee_axe");
+	public static final Item ORANGE_RUPEE_AXE = new ItemBase("orange_rupee_axe");
+	
+	public static final Item RUPEE_HOE = new ItemBase("rupee_hoe");
+	public static final Item BLUE_RUPEE_HOE = new ItemBase("blue_rupee_hoe");
+	public static final Item RED_RUPEE_HOE = new ItemBase("red_rupee_hoe");
+	public static final Item ORANGE_RUPEE_HOE = new ItemBase("orange_rupee_hoe");
+	
+	public static final Item RUPEE_SHOVEL = new ItemBase("rupee_shovel");
+	public static final Item BLUE_RUPEE_SHOVEL = new ItemBase("blue_rupee_shovel");
+	public static final Item RED_RUPEE_SHOVEL = new ItemBase("red_rupee_shovel");
+	public static final Item ORANGE_RUPEE_SHOVEL = new ItemBase("orange_rupee_shovel");
+	
+	public static final Item MASK_CLAY = new ItemBase("mask_clay");
+	public static final Item MASK_POSTMANSHAT = new ItemBase("mask_postmanshat");
+	public static final Item MASK_ALLNIGHTMASK = new ItemBase("mask_allnightmask");
+	public static final Item MASK_BLASTMASK = new ItemBase("mask_blastmask");
+	public static final Item MASK_STONEMASK = new ItemBase("mask_stonemask");
+	public static final Item MASK_GREATFAIRYMASK = new ItemBase("mask_greatfairymask");
+	public static final Item MASK_DEKUMASK = new ItemBase("mask_dekumask");
+	public static final Item MASK_KEATONMASK = new ItemBase("mask_keatonmask");
+	public static final Item MASK_BREMENMASK = new ItemBase("mask_bremenmask");
+	public static final Item MASK_BUNNYHOOD = new ItemBase("mask_bunnyhood");
+	public static final Item MASK_DONGEROSMASK = new ItemBase("mask_dongerosmask");
+	public static final Item MASK_MASKOFSCENTS = new ItemBase("mask_maskofscents");
+	public static final Item MASK_GORONMASK = new ItemBase("mask_goronmask");
+	public static final Item MASK_ROMANISMASK = new ItemBase("mask_romanismask");
+	public static final Item MASK_TROUPELEADERSMASK= new ItemBase("mask_troupeleadersmask");
+	public static final Item MASK_KAFEISMASK = new ItemBase("mask_troupeleadersmask");
+	public static final Item MASK_COUPLESMASK = new ItemBase("mask_couplesmask");
+	public static final Item MASK_MASKOFTRUTH = new ItemBase("mask_maskoftruth");
+	public static final Item MASK_ZORAMASK = new ItemBase("mask_zoramask");
+	public static final Item MASK_KAMAROSMASK = new ItemBase("mask_kamarosmask");
+	public static final Item MASK_GIBDOMASK = new ItemBase("mask_gibdomask");
+	public static final Item MASK_GAROSMASK = new ItemBase("mask_garosmask");
+	public static final Item MASK_CAPTAINSHAT = new ItemBase("mask_captainshat");
+	public static final Item MASK_GIANTSMASK = new ItemBase("mask_captainshat");
+	public static final Item MASK_FIERCEDEITYSMASK = new ItemBase("mask_fiercedeitysmask");
+
+	public static final Item KOKIRI_CAP = new ItemBase("kokiri_cap");
+	public static final Item KOKIRI_TUNIC = new ItemBase("kokiri_tunic");
+	public static final Item KOKIRI_LEGGINGS = new ItemBase("kokiri_leggings");
+	public static final Item KOKIRI_BOOTS = new ItemBase("kokiri_boots");
+	public static final Item ZORA_CAP = new ItemBase("kokiri_cap");
+	public static final Item ZORA_TUNIC = new ItemBase("zora_tunic");
+	public static final Item ZORA_LEGGINGS = new ItemBase("zora_leggings");
+	public static final Item IRON_BOOTS = new ItemBase("iron_boots");
+	public static final Item GORON_CAP = new ItemBase("kokiri_cap");
+	public static final Item GORON_TUNIC = new ItemBase("kokiri_cap");
+	public static final Item GORON_LEGGINGS = new ItemBase("kokiri_cap");
+	public static final Item HOVER_BOOTS = new ItemBase("kokiri_cap");
+	
+	
 }
