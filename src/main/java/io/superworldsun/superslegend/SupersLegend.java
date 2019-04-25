a9bb6fb6e53c88c1685771c4ce3e225853a8b038
superworldsun <superworldsun@gmail.com>
2019-04-25 00:06:38 -0400

    Update SupersLegend.java

--- src/main/java/io/superworldsun/superslegend/SupersLegend.java
+++ src/main/java/io/superworldsun/superslegend/SupersLegend.java
@@ -18,6 +18,7 @@ public class SupersLegend
     public static final String MODID = "superslegend";
     public static final String NAME = "SupersLegend";
     public static final String VERSION = "1.0";
+    public static final String ACCEPTED_VERSIONS = "[1.12.2]";
 
     public static Logger logger;
 
