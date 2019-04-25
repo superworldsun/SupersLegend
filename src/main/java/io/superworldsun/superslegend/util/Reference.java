08bf97139a4a3c54eb903e63609b6cb743fcd19e
superworldsun <superworldsun@gmail.com>
2019-04-25 04:37:05 -0400

    Add ReferenceTable

---
+++ src/main/java/io/superworldsun/superslegend/util/Reference.java
@@ -0,0 +1,12 @@
+package io.superworldsun.superslegend.util;
+
+public class Reference 
+{
+	public static final String MODID = "superslegend";
+	public static final String NAME = "SupersLegend";
+	public static final String VERSION = "1.0";
+	public static final String ACCEPTED_VERSIONS = "[1.12.2]";
+	public static final String CLIENT_PROXY_CLASS = "io.superworldsun.superslegend.proxy.ClientProxy";
+	public static final String COMMON_PROXY_CLASS = "io.superworldsun.superslegend.proxy.CommonProxy";
+	
+}
