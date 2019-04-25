26af0d477237ddac24c3838de8f4510e79602fac
superworldsun <superworldsun@gmail.com>
2019-04-25 05:33:21 -0400

    Delete and ReDid ItemList

---
+++ src/main/java/io/superworldsun/superslegend/init/ItemList.java
@@ -0,0 +1,19 @@
+package io.superworldsun.superslegend.init;
+
+import net.minecraft.item.Item;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.superworldsun.superslegend.items.ItemBase;
+
+//TODO: Document this class and methods inside
+public class ItemList
+{
+	public static final List<Item> ITEMS = new ArrayList<Item>();
+	/* 
+	 *  Items
+	 */
+	public static final Item RUPEE = new ItemBase("rupee");
+	
+}
