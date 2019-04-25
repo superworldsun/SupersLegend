3db104b6c5bcfd12c0e0cb1a1ab2da3183bf53c7
yooksi <yooksi@zoho.com>
2019-04-25 04:58:34 +0200

    Make main logger pubilc

--- src/main/java/io/superworldsun/superslegend/SupersLegend.java
+++ src/main/java/io/superworldsun/superslegend/SupersLegend.java
@@ -13,7 +13,7 @@ public class SupersLegend
     public static final String NAME = "SupersLegend";
     public static final String VERSION = "1.0";
 
-    private static Logger logger;
+    public static Logger logger;
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event)
