16e90d4bb38aea45f169c993b85d9e712fb420a2
superworldsun <superworldsun@gmail.com>
2019-04-25 04:42:40 -0400

    Change Voids and Added Handlers

--- src/main/java/io/superworldsun/superslegend/SupersLegend.java
+++ src/main/java/io/superworldsun/superslegend/SupersLegend.java
@@ -7,6 +7,7 @@ import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import io.superworldsun.superslegend.util.Reference;
@@ -14,6 +15,7 @@ import io.superworldsun.superslegend.util.Reference;
 import org.apache.logging.log4j.Logger;
 
 import io.superworldsun.superslegend.lists.*;
+import io.superworldsun.superslegend.proxy.CommonProxy;
 
 @Mod(modid = Reference.MODID, name = Reference.NAME, version = Reference.VERSION)
 public class SupersLegend
@@ -23,22 +25,30 @@ public class SupersLegend
 	public static SupersLegend instance;
 	
 	@SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.COMMON_PROXY_CLASS)
-	
+	public static CommonProxy proxy;
 
 
     public static Logger logger;
 
     @EventHandler
-    public void preInit(FMLPreInitializationEvent event)
+    public static void preInit(FMLPreInitializationEvent event)
     {
         logger = event.getModLog();
     }
 
     @EventHandler
-    public void init(FMLInitializationEvent event)
+    public static void init(FMLInitializationEvent event)
     {
         logger.info("Initializing SupersLegend mod...");
     }
+    
+    @EventHandler
+    public static void Postinit(FMLPostInitializationEvent event)
+    {
+        logger.info("Initializing SupersLegend mod...");
+    }
+    
+    
     
     @Mod.EventBusSubscriber(modid = Reference.MODID)
     public static class RegistrationHandler {
