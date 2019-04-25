a97fa6e166491bd82a71d47ec72a02fce8061a38
superworldsun <superworldsun@gmail.com>
2019-04-25 04:36:40 -0400

    Add Client&Server Proxy

---
+++ src/main/java/io/superworldsun/superslegend/proxy/ClientProxy.java
@@ -0,0 +1,6 @@
+package io.superworldsun.superslegend.proxy;
+
+public class ClientProxy extends CommonProxy
+{
+
+}
---
+++ src/main/java/io/superworldsun/superslegend/proxy/CommonProxy.java
@@ -0,0 +1,5 @@
+package io.superworldsun.superslegend.proxy;
+
+public class CommonProxy {
+
+}
