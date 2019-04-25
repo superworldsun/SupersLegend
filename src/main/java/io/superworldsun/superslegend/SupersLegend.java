2b180e10b2c9ebd9cc3a9eb30ea66d5a3171f623
yooksi <yooksi@zoho.com>
2019-04-25 03:07:04 +0200

    Add mod main class

---
+++ src/main/java/io/superworldsun/superslegend/SupersLegend.java
@@ -0,0 +1,30 @@
+package io.superworldsun.superslegend;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = SupersLegend.MODID, name = SupersLegend.NAME, version = SupersLegend.VERSION)
+public class SupersLegend
+{
+    public static final String MODID = "superslegend";
+    public static final String NAME = "SupersLegend";
+    public static final String VERSION = "1.0";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("Initializing SupersLegend mod...");
+    }
+}
