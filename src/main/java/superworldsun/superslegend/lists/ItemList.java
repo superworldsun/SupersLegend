78d4bb7136c9af20dba9f6b981622cd358d9b03f
superworldsun <superworldsun@gmail.com>
2019-08-15 03:33:52 -0400

    Add Items

--- src/main/java/superworldsun/superslegend/lists/ItemList.java
+++ src/main/java/superworldsun/superslegend/lists/ItemList.java
@@ -1,9 +1,86 @@
 package superworldsun.superslegend.lists;
 
 import net.minecraft.item.Item;
+import superworldsun.superslegend.items.ArmorGoronEffects;
+import superworldsun.superslegend.items.ArmorZoraEffects;
+
+
 
 public class ItemList 
 {
 	//items
 		public static Item rupee;
+		public static Item blue_rupee;
+		public static Item red_rupee;
+		public static Item orange_rupee;
+	//weapons
+		public static Item kokiri_sword;
+		public static Item razor_sword;
+		public static Item gilded_sword;
+		public static Item rupee_sword;
+		public static Item blue_rupee_sword;
+		public static Item red_rupee_sword;
+		public static Item orange_rupee_sword;
+	//tools
+		public static Item rupee_pickaxe;
+		public static Item blue_rupee_pickaxe;
+		public static Item red_rupee_pickaxe;
+		public static Item orange_rupee_pickaxe;
+		
+		public static Item rupee_axe;
+		public static Item blue_rupee_axe;
+		public static Item red_rupee_axe;
+		public static Item orange_rupee_axe;
+		
+		public static Item rupee_hoe;
+		public static Item blue_rupee_hoe;
+		public static Item red_rupee_hoe;
+		public static Item orange_rupee_hoe;
+		
+		public static Item rupee_shovel;
+		public static Item blue_rupee_shovel;
+		public static Item red_rupee_shovel;
+		public static Item orange_rupee_shovel;
+		
+		//masks
+		public static Item mask_clay;
+		public static Item mask_postmanshat;
+
+		public static Item mask_blastmask;
+
+		public static Item mask_greatfairymask;
+		public static Item mask_dekumask;
+		public static Item mask_keatonmask;
+		public static Item mask_bremenmask;
+
+		
+		public static Item mask_maskofscents;
+		public static Item mask_goronmask;
+		public static Item mask_romanismask;
+		public static Item mask_troupeleadersmask;
+		public static Item mask_kafeismask;
+		public static Item mask_couplesmask;
+		public static Item mask_maskoftruth;
+		public static Item mask_zoramask;
+		public static Item mask_kamarosmask;
+		public static Item mask_gibdomask;
+		public static Item mask_garosmask;
+		public static Item mask_captainshat;
+		public static Item mask_giantsmask;
+		public static Item mask_fiercedeitysmask;
+		
+		
+	//armor
+		public static Item kokiri_cap;
+		public static Item kokiri_tunic;
+		public static Item kokiri_leggings;
+		public static Item kokiri_boots;
+		public static ArmorZoraEffects zora_cap;
+		public static ArmorZoraEffects zora_tunic;
+		public static ArmorZoraEffects zora_leggings;
+		public static ArmorZoraEffects iron_boots;
+		public static ArmorGoronEffects goron_cap;
+		public static ArmorGoronEffects goron_tunic;
+		public static ArmorGoronEffects goron_leggings;
+		public static ArmorGoronEffects hover_boots;
 }
