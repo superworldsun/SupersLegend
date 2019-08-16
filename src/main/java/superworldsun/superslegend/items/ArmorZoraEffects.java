83c1cfa56920a031c87cfb174a7b4263c67f2f8d
superworldsun <superworldsun@gmail.com>
2019-08-16 05:08:22 -0400

    Add Armor Effects

--- src/main/java/superworldsun/superslegend/items/ArmorZoraEffects.java
+++ src/main/java/superworldsun/superslegend/items/ArmorZoraEffects.java
@@ -1,11 +1,19 @@
 package superworldsun.superslegend.items;
 
+import java.util.List;
+
+import afu.org.checkerframework.checker.nullness.qual.Nullable;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.*;
 import net.minecraft.potion.Effect;
 import net.minecraft.potion.EffectInstance;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import superworldsun.superslegend.SupersLegend;
 import superworldsun.superslegend.lists.ArmourMaterialList;
 import superworldsun.superslegend.lists.ItemList;
@@ -17,6 +25,13 @@ public class ArmorZoraEffects extends ArmorItem {
     {
         super(ArmourMaterialList.zora, slot, new Item.Properties().group(SupersLegend.supers_legend));
         setRegistryName(SupersLegend.modid, name);
+    }
+        
+    @OnlyIn(Dist.CLIENT)
+    @Override
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
+        tooltip.add(new TranslationTextComponent(SupersLegend.modid + ".zora_tunic.description"));
+
     }
 
     @Override
