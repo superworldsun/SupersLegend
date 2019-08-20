f91ce7d43f6bdcdf5d9948201075920ecc656842
superworldsun <superworldsun@gmail.com>
2019-08-20 01:30:42 -0400

    Added Item description to flippers

--- src/main/java/superworldsun/superslegend/items/ArmorFlippersEffects.java
+++ src/main/java/superworldsun/superslegend/items/ArmorFlippersEffects.java
@@ -1,15 +1,18 @@
 package superworldsun.superslegend.items;
 
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.*;
 import net.minecraft.potion.Effect;
 import net.minecraft.potion.EffectInstance;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
 import superworldsun.superslegend.SupersLegend;
 import superworldsun.superslegend.lists.ArmourMaterialList;
 import superworldsun.superslegend.lists.ItemList;
-
+import net.minecraft.util.text.ITextComponent;
 
 public class ArmorFlippersEffects extends ArmorItem {
     public ArmorFlippersEffects(String name, EquipmentSlotType slot) 
@@ -18,6 +21,12 @@ public class ArmorFlippersEffects extends ArmorItem {
         super(ArmourMaterialList.flippers, slot, new Item.Properties().group(SupersLegend.supers_legend));
         setRegistryName(SupersLegend.modid, name);
     }
+    
+    public void addInformation(ItemStack stack, World world, java.util.List<ITextComponent> list, ITooltipFlag flag)
+	{
+		super.addInformation(stack, world, list, flag);				
+		list.add(new StringTextComponent(TextFormatting.BLUE + "Provides the ability to swim"));
+	}
 
     @Override
     public void onArmorTick(ItemStack stack, World world, PlayerEntity player) 
