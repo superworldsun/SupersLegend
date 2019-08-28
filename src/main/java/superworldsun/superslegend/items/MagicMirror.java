23e9346467c1277b5696f9bb2198a31996d42d28
superworldsun <superworldsun@gmail.com>
2019-08-28 01:33:09 -0400

    Added Dins Fire Sprite

--- src/main/java/superworldsun/superslegend/items/MagicMirror.java
+++ src/main/java/superworldsun/superslegend/items/MagicMirror.java
@@ -2,26 +2,12 @@ package superworldsun.superslegend.items;
 
 import net.minecraft.item.Item;
 
-import net.minecraft.item.ItemUseContext;
-import net.minecraft.util.ActionResultType;
-
 public class MagicMirror extends Item
-
 {
-	public MagicMirror(Properties properties)
-	{
-		super(properties);
-	}
-	
-	@Override
-	public ActionResultType onItemUse(ItemUseContext context) 
-	{
-		return null;
-		
-	}
 
-				
-			
-		
+	public MagicMirror(Properties properties) {
+		super(properties);
+		// TODO Auto-generated constructor stub
+	}
 	
-}
+};
