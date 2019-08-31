664651d93da1944edbf88689da242c89942f7d87
superworldsun <superworldsun@gmail.com>
2019-08-31 02:56:30 -0400

    Updated Group Icon

--- src/main/java/superworldsun/superslegend/SupersLegendItemGroup.java
+++ src/main/java/superworldsun/superslegend/SupersLegendItemGroup.java
@@ -14,6 +14,6 @@ public class SupersLegendItemGroup extends ItemGroup
 	@Override
 	public ItemStack createIcon() 
 	{
-		return new ItemStack(ItemList.rupee);
+		return new ItemStack(ItemList.triforce);
 	}
 }
