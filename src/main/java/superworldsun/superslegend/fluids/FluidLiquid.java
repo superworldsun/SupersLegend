b6b7e9adfe03f3185ce05126ea8656ce6b275a29
superworldsun <superworldsun@gmail.com>
2019-08-31 04:21:16 -0400

    Added Fluid

---
+++ src/main/java/superworldsun/superslegend/fluids/FluidLiquid.java
@@ -0,0 +1,13 @@
+package superworldsun.superslegend.fluids;
+
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fluids.Fluid;
+
+public class FluidLiquid extends Fluid 
+{
+	public FluidLiquid(String name, ResourceLocation still, ResourceLocation flow) 
+	{
+		super(name, still, flow);
+		this.setUnlocalizedName(name);
+	}
+}
---
+++ src/main/java/superworldsun/superslegend/init/FluidInit.java
@@ -0,0 +1,21 @@
+package superworldsun.superslegend.init;
+
+
+import superworldsun.superslegend.fluids.FluidLiquid;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fluids.Fluid;
+
+public class FluidInit
+{
+	public static final Fluid POISON_FLUID = new FluidLiquid("molten_copper", new ResourceLocation("superslegend:blocks/poison_still"), new ResourceLocation("superslegend:blocks/poison_flow"));
+
+	public static void registerFluids()
+	{
+		registerFluid(POISON_FLUID);
+	}
+	
+	public static void registerFluid(Fluid fluid)
+	{
+		
+	}
+}
