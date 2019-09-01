c7c5bce96b2b1b7326c3a4a8310482833c0878b9
superworldsun <superworldsun@gmail.com>
2019-09-01 03:35:18 -0400

    Added BlockInit

---
+++ src/main/java/superworldsun/superslegend/init/BlockInit.java
@@ -0,0 +1,15 @@
+package superworldsun.superslegend.init;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.minecraft.block.Block;
+//import net.minecraft.block.material.Material;
+
+public class BlockInit 
+{
+	public static final List<Block> BLOCKS = new ArrayList<Block>();
+	
+	//Liquids
+	//public static final Block POISON_FLUID_BLOCK = new BlockFluid("poison_fluid", FluidInit.POISON_FLUID, Material.WATER);
+}
