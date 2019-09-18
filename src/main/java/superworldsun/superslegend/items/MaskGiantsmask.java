fa08ac5bfd538e7839d395809d73e1b8f6e5cf70
superworldsun <superworldsun@gmail.com>
2019-09-18 00:02:15 -0400

    Updated Triforce Effects & Added description

--- src/main/java/superworldsun/superslegend/items/MaskGiantsmask.java
+++ src/main/java/superworldsun/superslegend/items/MaskGiantsmask.java
@@ -1,33 +1,34 @@
 package superworldsun.superslegend.items;
 
+
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.*;
-import net.minecraft.potion.Effect;
-import net.minecraft.potion.EffectInstance;
+import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
 import superworldsun.superslegend.SupersLegend;
 import superworldsun.superslegend.lists.ArmourMaterialList;
 import superworldsun.superslegend.lists.ItemList;
-import net.minecraft.util.text.ITextComponent;
 
-public class ArmorFlippersEffects extends ArmorItem {
-    public ArmorFlippersEffects(String name, EquipmentSlotType slot) 
+
+public class MaskGiantsmask extends ArmorItem {
+    public MaskGiantsmask(String name, EquipmentSlotType slot) 
     
     {
-        super(ArmourMaterialList.flippers, slot, new Item.Properties().group(SupersLegend.supers_legend));
+        super(ArmourMaterialList.giantsmask, slot, new Item.Properties().group(SupersLegend.supers_legend));
         setRegistryName(SupersLegend.modid, name);
     }
     
     public void addInformation(ItemStack stack, World world, java.util.List<ITextComponent> list, ITooltipFlag flag)
 	{
 		super.addInformation(stack, world, list, flag);				
-		list.add(new StringTextComponent(TextFormatting.AQUA + "Provides the ability to swim like a Zora"));
+		list.add(new StringTextComponent(TextFormatting.GRAY + "Takes a big spirit to wear this mask"));
 	}
-
+}
+/*    
     @Override
     public void onArmorTick(ItemStack stack, World world, PlayerEntity player) 
     {
@@ -35,14 +36,8 @@ public class ArmorFlippersEffects extends ArmorItem {
     	
     	
         if (!world.isRemote){
-                boolean isBootsOn = player.getItemStackFromSlot(EquipmentSlotType.FEET).getItem().equals(ItemList.zoras_flippers);
-                if(isBootsOn)
-                	{
-                	if(player.isInWater()) 
-                	{
-                		player.addPotionEffect(new EffectInstance(Effect.get(1), 5, 2, false, false));
-                	}
+                boolean isHelmeton = player.getItemStackFromSlot(EquipmentSlotType.HEAD).getItem().equals(ItemList.mask_giantsmask);
+                if(isHelmeton) PlayerEntity.STANDING_SIZE (2);
                 }
-        }
-    }
-}
+            }
+        }*/
--- src/main/java/superworldsun/superslegend/items/TriforceCourage.java
+++ src/main/java/superworldsun/superslegend/items/TriforceCourage.java
@@ -1,11 +1,19 @@
 package superworldsun.superslegend.items;
 
-import net.minecraft.entity.Entity;
+import java.util.List;
+
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.potion.Effect;
 import net.minecraft.potion.EffectInstance;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Hand;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
 
 public class TriforceCourage extends Item
@@ -16,23 +24,21 @@ public class TriforceCourage extends Item
 		super(properties);
 	}
 	
-	public void inventoryTick(ItemStack stack, World world, Entity entity, int itemSlot, boolean isSelected)
-	{		
-		if(entity instanceof PlayerEntity && !world.isRemote)
-		{
-			PlayerEntity player = (PlayerEntity)entity;
-
-			ItemStack equipped = player.getHeldItemMainhand();
-
-			if(!world.isRemote)
-			{
-				if(stack == equipped)
-		        {
+	 public ActionResult<ItemStack> onItemRightClick(World world, PlayerEntity player, Hand hand)
+	 {
+		 @SuppressWarnings("unused")
+		ItemStack stack = player.getHeldItem(hand);
+		        
 					player.addPotionEffect(new EffectInstance(Effect.get(1), 600, 0, false, true));
 					player.addPotionEffect(new EffectInstance(Effect.get(11), 600, 0, false, false));
-		        }
+					
+					return new ActionResult<>(ActionResultType.PASS, player.getHeldItem(hand));
 			}	
-			
-		}
+	 
+	 @Override
+		public void addInformation(ItemStack stack, World world, List<ITextComponent> list, ITooltipFlag flag)
+		{
+			super.addInformation(stack, world, list, flag);				
+			list.add(new StringTextComponent(TextFormatting.GREEN + "This will give you the Courage to fight for what you beleive in."));
+		}   
 	}
-}
