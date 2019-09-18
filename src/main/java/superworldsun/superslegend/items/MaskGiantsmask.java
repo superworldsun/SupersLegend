267f83fdf4216b89ac37c50e0a4477bae742fba1
superworldsun <superworldsun@gmail.com>
2019-09-18 03:51:34 -0400

    Update MaskGiantsmask.java

--- src/main/java/superworldsun/superslegend/items/MaskGiantsmask.java
+++ src/main/java/superworldsun/superslegend/items/MaskGiantsmask.java
@@ -2,7 +2,7 @@ package superworldsun.superslegend.items;
 
 
 import net.minecraft.client.util.ITooltipFlag;
-import net.minecraft.entity.player.PlayerEntity;
+
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.*;
 import net.minecraft.util.text.ITextComponent;
@@ -11,7 +11,7 @@ import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
 import superworldsun.superslegend.SupersLegend;
 import superworldsun.superslegend.lists.ArmourMaterialList;
-import superworldsun.superslegend.lists.ItemList;
+
 
 
 public class MaskGiantsmask extends ArmorItem {
@@ -22,22 +22,27 @@ public class MaskGiantsmask extends ArmorItem {
         setRegistryName(SupersLegend.modid, name);
     }
 
+    /*public void applyCustomModifiers(ItemStack stack, PlayerEntity player) {
+		
+    	
+		// TODO
+    	PlayerEntity.setSize(player, player.getWidth() * 3.0F, player.getHeight() * 3.0F);
+		if (player.world.isRemote) {
+			player.stepHeight += 1.0F;
+		}
+		 
+	}
+	public void removeModifiers(ItemStack stack, PlayerEntity player) {
+
+			if (player.world.isRemote) {
+				player.stepHeight -= 1.0F;
+			}
+		}*/
+	
+    
     public void addInformation(ItemStack stack, World world, java.util.List<ITextComponent> list, ITooltipFlag flag)
 	{
 		super.addInformation(stack, world, list, flag);				
 		list.add(new StringTextComponent(TextFormatting.GRAY + "Takes a big spirit to wear this mask"));
 	}
 }
-/*    
-    @Override
-    public void onArmorTick(ItemStack stack, World world, PlayerEntity player) 
-    {
-    	
-    	
-    	
-        if (!world.isRemote){
-                boolean isHelmeton = player.getItemStackFromSlot(EquipmentSlotType.HEAD).getItem().equals(ItemList.mask_giantsmask);
-                if(isHelmeton) PlayerEntity.STANDING_SIZE (2);
-                }
-            }
-        }*/ 
