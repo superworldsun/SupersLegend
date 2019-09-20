2154cedef349c86d82d41af7ab8746d44955a29e
superworldsun <superworldsun@gmail.com>
2019-09-20 04:28:28 -0400

    Updated TorchTowerBlock to break when on air

--- src/main/java/superworldsun/superslegend/blocks/TorchTowerBlock.java
+++ src/main/java/superworldsun/superslegend/blocks/TorchTowerBlock.java
@@ -4,12 +4,16 @@ import java.util.Random;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.particles.ParticleTypes;
 import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.IWorld;
+import net.minecraft.world.IWorldReader;
 import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
@@ -35,6 +39,15 @@ public class TorchTowerBlock extends Block
 	      worldIn.addParticle(ParticleTypes.SMOKE, d0, d1, d2, 0.0D, 0.0D, 0.0D);
 	      worldIn.addParticle(ParticleTypes.FLAME, d0, d1, d2, 0.0D, 0.0D, 0.0D);
 	   }
+	   
+	   @SuppressWarnings("deprecation")
+	public BlockState updatePostPlacement(BlockState stateIn, Direction facing, BlockState facingState, IWorld worldIn, BlockPos currentPos, BlockPos facingPos) {
+		      return facing == Direction.DOWN && !this.isValidPosition(stateIn, worldIn, currentPos) ? Blocks.AIR.getDefaultState() : super.updatePostPlacement(stateIn, facing, facingState, worldIn, currentPos, facingPos);
+		   }
+
+		   public boolean isValidPosition(BlockState state, IWorldReader worldIn, BlockPos pos) {
+		      return func_220055_a(worldIn, pos.down(), Direction.UP);
+		   }
 	   
 	   public BlockRenderLayer getRenderLayer() {
 		    return BlockRenderLayer.CUTOUT;
