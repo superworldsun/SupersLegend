17f7c15aa48ffeb9a6c8f7bd2b20f2670bae435d
superworldsun <superworldsun@gmail.com>
2019-10-04 05:14:14 -0400

    Create MaskDekumask.java

---
+++ src/main/java/superworldsun/superslegend/items/MaskDekumask.java
@@ -0,0 +1,47 @@
+package superworldsun.superslegend.items;
+
+import java.util.List;
+
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.*;
+import net.minecraft.potion.Effect;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.world.World;
+import superworldsun.superslegend.SupersLegend;
+import superworldsun.superslegend.lists.ArmourMaterialList;
+import superworldsun.superslegend.lists.ItemList;
+
+
+public class MaskDekumask extends ArmorItem {
+    public MaskDekumask(String name, EquipmentSlotType slot) 
+    
+    {
+        super(ArmourMaterialList.dekumask, slot, new Item.Properties().group(SupersLegend.supers_legend));
+        setRegistryName(SupersLegend.modid, name);
+    }
+    @Override
+	public void addInformation(ItemStack stack, World world, List<ITextComponent> list, ITooltipFlag flag)
+	{
+		super.addInformation(stack, world, list, flag);				
+		list.add(new StringTextComponent(TextFormatting.DARK_GREEN + "The face of a Deku"));
+		list.add(new StringTextComponent(TextFormatting.DARK_GRAY + "You are unable to be poisoned while worn"));
+	}
+    
+    @Override
+    public void onArmorTick(ItemStack stack, World world, PlayerEntity player) 
+    {
+    	
+    	
+    	
+        if (!world.isRemote){
+                boolean isHelmeton = player.getItemStackFromSlot(EquipmentSlotType.HEAD).getItem().equals(ItemList.mask_dekumask);
+                if(isHelmeton) 
+                	
+                	player.removePotionEffect(Effect.get(19));
+                }
+            }
+        }
