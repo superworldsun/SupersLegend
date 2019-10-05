e24f37dd22b672618565a312a16c80d9bdacb7e9
superworldsun <superworldsun@gmail.com>
2019-10-04 23:17:49 -0400

    Update MagicCape.java

--- src/main/java/superworldsun/superslegend/items/MagicCape.java
+++ src/main/java/superworldsun/superslegend/items/MagicCape.java
@@ -1,47 +1,51 @@
 package superworldsun.superslegend.items;
 
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.potion.Effect;
 import net.minecraft.potion.EffectInstance;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Hand;
 import net.minecraft.world.World;
 
 public class MagicCape extends Item
 {
 
-	private static final boolean isInvulnerable = true;
+	
+
+	private boolean isInvulnerable;
 
 	public MagicCape(Properties properties)
 	{
 		super(properties);
 	}
 	
-	public void inventoryTick(ItemStack stack, World world, Entity entity, int itemSlot, boolean isSelected)
-	{		
-		if(entity instanceof PlayerEntity && !world.isRemote)
+	public ActionResult<ItemStack> onItemRightClick(World world, PlayerEntity player, Hand hand)
+	 {
+		 @SuppressWarnings("unused")
+		ItemStack stack = player.getHeldItem(hand);
+		  
+		 if(!world.isRemote)
+	     {
+			 player.addPotionEffect(new EffectInstance(Effect.get(18), 5, 3, false, false));
+				player.addPotionEffect(new EffectInstance(Effect.get(17), 5, 0, false, false));
+				player.addPotionEffect(new EffectInstance(Effect.get(15), 60, 0, false, false));
+				player.addPotionEffect(new EffectInstance(Effect.get(14), 5, 0, false, false));
+				player.setInvulnerable(isInvulnerable);
+				player.addExhaustion(1);
+
+				
+	     }
+	 
+		 return new ActionResult<>(ActionResultType.PASS, player.getHeldItem(hand)); 
+	 }
+	
+	public void onPlayerStoppedUsing(ItemStack stack, World worldIn, PlayerEntity player, int timeLeft) 
+	
 		{
-			PlayerEntity player = (PlayerEntity)entity;
-
-			ItemStack equipped = player.getHeldItemMainhand();
-
-			if(!world.isRemote)
-			{
-				if(stack == equipped)
-		        {
-					player.addPotionEffect(new EffectInstance(Effect.get(18), 5, 3, false, false));
-					player.addPotionEffect(new EffectInstance(Effect.get(17), 5, 0, false, false));
-					player.addPotionEffect(new EffectInstance(Effect.get(15), 60, 0, false, false));
-					player.addPotionEffect(new EffectInstance(Effect.get(14), 5, 0, false, false));
-					player.setInvulnerable(isInvulnerable);
-		        }
-            	else
-            	{
-            		player.setInvulnerable(false);;
-            	}
-			}	
-			
-		}
-	}
+			player.setInvulnerable(false);;
+	    }
+	
 }
