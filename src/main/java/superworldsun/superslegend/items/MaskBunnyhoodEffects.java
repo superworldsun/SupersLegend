ca0bd028b54f3303ea8ea768445db0496f789b72
superworldsun <superworldsun@gmail.com>
2019-11-05 01:49:45 -0500

    Update MaskBunnyhoodEffects.java

--- src/main/java/superworldsun/superslegend/items/MaskBunnyhoodEffects.java
+++ src/main/java/superworldsun/superslegend/items/MaskBunnyhoodEffects.java
@@ -2,7 +2,6 @@ package superworldsun.superslegend.items;
 
 import java.util.List;
 
-import afu.org.checkerframework.checker.nullness.qual.Nullable;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.EquipmentSlotType;
@@ -10,10 +9,9 @@ import net.minecraft.item.*;
 import net.minecraft.potion.Effect;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.util.text.ITextComponent;
-import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
 import superworldsun.superslegend.SupersLegend;
 import superworldsun.superslegend.lists.ArmourMaterialList;
 import superworldsun.superslegend.lists.ItemList;
@@ -26,18 +24,10 @@ public class MaskBunnyhoodEffects extends ArmorItem {
         super(ArmourMaterialList.bunnyhood, slot, new Item.Properties().group(SupersLegend.supers_legend));
         setRegistryName(SupersLegend.modid, name);
     }
-    @OnlyIn(Dist.CLIENT)
-    @Override
-    public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
-        tooltip.add(new TranslationTextComponent(SupersLegend.modid + ".mask_bunnyhood.description"));
-    }
 
     @Override
     public void onArmorTick(ItemStack stack, World world, PlayerEntity player) 
     {
-    	
-    	
-    	
         if (!world.isRemote){
                 boolean isHelmeton = player.getItemStackFromSlot(EquipmentSlotType.HEAD).getItem().equals(ItemList.mask_bunnyhood);
                 
@@ -54,4 +44,11 @@ public class MaskBunnyhoodEffects extends ArmorItem {
                 	}
         		}
     		}
-     }
+    
+    @Override
+	public void addInformation(ItemStack stack, World world, List<ITextComponent> list, ITooltipFlag flag)
+	{
+		super.addInformation(stack, world, list, flag);				
+		list.add(new StringTextComponent(TextFormatting.YELLOW + "I am Speed"));
+	}
+}
