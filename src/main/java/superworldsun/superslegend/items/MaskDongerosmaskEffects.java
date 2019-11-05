f97db33b7d3e2357fc3cf0992521a0db6188be75
superworldsun <superworldsun@gmail.com>
2019-11-05 01:49:40 -0500

    Update MaskDongerosmaskEffects.java

--- src/main/java/superworldsun/superslegend/items/MaskDongerosmaskEffects.java
+++ src/main/java/superworldsun/superslegend/items/MaskDongerosmaskEffects.java
@@ -1,8 +1,5 @@
 package superworldsun.superslegend.items;
 
-import java.util.List;
-
-import afu.org.checkerframework.checker.nullness.qual.Nullable;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.EquipmentSlotType;
@@ -10,10 +7,9 @@ import net.minecraft.item.*;
 import net.minecraft.potion.Effect;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.util.text.ITextComponent;
-import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
 import superworldsun.superslegend.SupersLegend;
 import superworldsun.superslegend.lists.ArmourMaterialList;
 import superworldsun.superslegend.lists.ItemList;
@@ -26,21 +22,22 @@ public class MaskDongerosmaskEffects extends ArmorItem {
         super(ArmourMaterialList.dongerosmask, slot, new Item.Properties().group(SupersLegend.supers_legend));
         setRegistryName(SupersLegend.modid, name);
     }
-    @OnlyIn(Dist.CLIENT)
-    @Override
-    public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
-        tooltip.add(new TranslationTextComponent(SupersLegend.modid + ".mask_dongerosmask.description"));
-    }
 
     @Override
     public void onArmorTick(ItemStack stack, World world, PlayerEntity player) 
     {
-    	
-    	
-    	
-        if (!world.isRemote){
+        if (!world.isRemote)
+        {
                 boolean isHelmeton = player.getItemStackFromSlot(EquipmentSlotType.HEAD).getItem().equals(ItemList.mask_dongerosmask);
                 if(isHelmeton) player.addPotionEffect(new EffectInstance(Effect.get(8), 10, 1, false, false));
-                }
-            }
+                
         }
+            
+    }
+
+    public void addInformation(ItemStack stack, World world, java.util.List<ITextComponent> list, ITooltipFlag flag)
+ 	{
+ 		super.addInformation(stack, world, list, flag);				
+ 		list.add(new StringTextComponent(TextFormatting.DARK_GREEN + "This Hood is Hoppin!"));
+ 	}
+}
