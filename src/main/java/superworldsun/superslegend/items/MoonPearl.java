61bfbee33bc655139a1ebf23a15c4bf8a23f936a
superworldsun <superworldsun@gmail.com>
2019-11-05 01:39:45 -0500

    Update MoonPearl.java

--- src/main/java/superworldsun/superslegend/items/MoonPearl.java
+++ src/main/java/superworldsun/superslegend/items/MoonPearl.java
@@ -1,14 +1,19 @@
 package superworldsun.superslegend.items;
 
 import java.util.List;
+import java.util.Random;
 
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.particles.ParticleTypes;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvents;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TextFormatting;
@@ -34,11 +39,35 @@ public class MoonPearl extends Item
 		{
 			if(!(currentDim == 1))
 			{
+				Random rand = entity.world.rand;
+		        for (int i = 0; i < 45; i++)
+		        {
+		        	entity.world.addParticle(ParticleTypes.DRAGON_BREATH,
+		        			entity.posX + (rand.nextBoolean() ? -1 : 1) * Math.pow(rand.nextFloat(), 2) * 2,
+		        			entity.posY + rand.nextFloat() * 3 - 2,
+		        			entity.posZ + (rand.nextBoolean() ? -1 : 1) * Math.pow(rand.nextFloat(), 2) * 2,
+		                    0, 0.105D, 0);
+		        }
+		        
+		        BlockPos currentPos = entity.getPosition();
+				 world.playSound(null, currentPos.getX(), currentPos.getY(), currentPos.getZ(), SoundEvents.ITEM_CHORUS_FRUIT_TELEPORT, SoundCategory.PLAYERS, 1f, 1f);
 				entity.changeDimension(DimensionType.THE_NETHER);
 		    	return ActionResult.newResult(ActionResultType.SUCCESS, stack);
 			}
 			else if(((currentDim == 1)) && (entity.isSneaking()))
 			{
+				Random rand = entity.world.rand;
+		        for (int i = 0; i < 45; i++)
+		        {
+		        	entity.world.addParticle(ParticleTypes.DRAGON_BREATH,
+		        			entity.posX + (rand.nextBoolean() ? -1 : 1) * Math.pow(rand.nextFloat(), 2) * 2,
+		        			entity.posY + rand.nextFloat() * 3 - 2,
+		        			entity.posZ + (rand.nextBoolean() ? -1 : 1) * Math.pow(rand.nextFloat(), 2) * 2,
+		                    0, 0.105D, 0);
+		        }
+		        
+		        BlockPos currentPos = entity.getPosition();
+				 world.playSound(null, currentPos.getX(), currentPos.getY(), currentPos.getZ(), SoundEvents.ITEM_CHORUS_FRUIT_TELEPORT, SoundCategory.PLAYERS, 1f, 1f);
 				entity.changeDimension(DimensionType.OVERWORLD);
 		    	return ActionResult.newResult(ActionResultType.SUCCESS, stack);
 			}
@@ -53,6 +82,6 @@ public class MoonPearl extends Item
 		super.addInformation(stack, world, list, flag);				
 		list.add(new StringTextComponent(TextFormatting.RED + "Teleports the player to the Nether"));
 		list.add(new StringTextComponent(TextFormatting.GREEN + "Right-click to use"));
-		list.add(new StringTextComponent(TextFormatting.GREEN + "when used in the Nether it will Return player to Portal"));
+		list.add(new StringTextComponent(TextFormatting.GREEN + "When used in the Nether it will Return player to Portal"));
 	}  
 }
