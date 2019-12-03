aad2fd5f105185fd5ac8667ed1e4d392c3cf1261
superworldsun <superworldsun@gmail.com>
2019-12-03 00:33:55 -0500

    Added Desctiptions to Masks

---
+++ src/main/java/superworldsun/superslegend/items/MaskCaptainshat.java
@@ -0,0 +1,29 @@
+package superworldsun.superslegend.items;
+
+import java.util.List;
+
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.*;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.world.World;
+import superworldsun.superslegend.SupersLegend;
+import superworldsun.superslegend.lists.ArmourMaterialList;
+
+
+public class MaskCaptainshat extends ArmorItem {
+    public MaskCaptainshat(String name, EquipmentSlotType slot) 
+    
+    {
+        super(ArmourMaterialList.captainshat, slot, new Item.Properties().group(SupersLegend.supers_legend));
+        setRegistryName(SupersLegend.modid, name);
+    }
+    @Override
+	public void addInformation(ItemStack stack, World world, List<ITextComponent> list, ITooltipFlag flag)
+	{
+		super.addInformation(stack, world, list, flag);				
+		list.add(new StringTextComponent(TextFormatting.YELLOW + "Even through death, a leader stands"));
+	}
+}
