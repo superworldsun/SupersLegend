dd89ccd4f8d5a8c5c70bc0587b0bb2d015b8545c
superworldsun <superworldsun@gmail.com>
2020-01-07 02:18:08 -0500

    Create ArmorPurpleEffects.java

---
+++ src/main/java/superworldsun/superslegend/items/ArmorPurpleEffects.java
@@ -0,0 +1,26 @@
+package superworldsun.superslegend.items;
+
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.*;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.world.World;
+import superworldsun.superslegend.SupersLegend;
+import superworldsun.superslegend.lists.ArmourMaterialList;
+import net.minecraft.util.text.ITextComponent;
+
+public class ArmorPurpleEffects extends NonEnchantArmor {
+    public ArmorPurpleEffects(String name, EquipmentSlotType slot) 
+    
+    {
+        super(ArmourMaterialList.purple, slot, new Item.Properties().group(SupersLegend.supers_legend));
+        setRegistryName(SupersLegend.modid, name);
+    }
+    
+    public void addInformation(ItemStack stack, World world, java.util.List<ITextComponent> list, ITooltipFlag flag)
+	{
+		super.addInformation(stack, world, list, flag);				
+		list.add(new StringTextComponent(TextFormatting.LIGHT_PURPLE + "A rarer outfit not often seen"));
+	}
+}
