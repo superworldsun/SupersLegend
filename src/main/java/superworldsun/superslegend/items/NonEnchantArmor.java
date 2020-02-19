2f5c36ed6cc347cf6d3b8c4bbf73097169436b65
superworldsun <superworldsun@gmail.com>
2020-02-19 03:37:06 -0500

    Update NonEcnahtArmor
    removed book enchant to false

--- src/main/java/superworldsun/superslegend/items/NonEnchantArmor.java
+++ src/main/java/superworldsun/superslegend/items/NonEnchantArmor.java
@@ -18,7 +18,7 @@ public class NonEnchantArmor extends ArmorItem {
 	@Override
     public boolean isBookEnchantable(ItemStack stack, ItemStack book) {
         // TODO Auto-generated method stub
-        return !super.isBookEnchantable(stack, book);
+        return false;
 	}
 	
 	
