512fb53ba533ab005af4566f8032dcc60ce7ab61
superworldsun <superworldsun@gmail.com>
2020-02-19 03:33:01 -0500

    Update GoronMask
    add ability to remove wither

--- src/main/java/superworldsun/superslegend/items/MaskGoronmask.java
+++ src/main/java/superworldsun/superslegend/items/MaskGoronmask.java
@@ -3,14 +3,17 @@ package superworldsun.superslegend.items;
 import java.util.List;
 
 import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.*;
+import net.minecraft.potion.Effect;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
 import superworldsun.superslegend.SupersLegend;
 import superworldsun.superslegend.lists.ArmourMaterialList;
+import superworldsun.superslegend.lists.ItemList;
 
 
 public class MaskGoronmask extends NonEnchantArmor {
@@ -24,6 +27,26 @@ public class MaskGoronmask extends NonEnchantArmor {
 	public void addInformation(ItemStack stack, World world, List<ITextComponent> list, ITooltipFlag flag)
 	{
 		super.addInformation(stack, world, list, flag);				
-		list.add(new StringTextComponent(TextFormatting.DARK_GRAY + "The face of a Goron"));
+		list.add(new StringTextComponent(TextFormatting.DARK_RED + "The face of a Goron"));
+		list.add(new StringTextComponent(TextFormatting.DARK_GRAY + "Your skin is like stone and cannot stay withered"));
 	}
+    
+    
+    @Override
+    public void onArmorTick(ItemStack stack, World world, PlayerEntity player) 
+    {
+    	
+    	
+    	
+        if (!world.isRemote){
+                boolean isHelmeton = player.getItemStackFromSlot(EquipmentSlotType.HEAD).getItem().equals(ItemList.mask_goronmask);
+                if(isHelmeton) 
+                	
+                	player.removePotionEffect(Effect.get(20));
+                
+                }
+            }
+        
+    
+    
 }
