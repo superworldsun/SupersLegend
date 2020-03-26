1680dd452756c37786491fa7136641913fdf2c7c
superworldsun <superworldsun@gmail.com>
2020-03-26 04:40:29 -0400

    Update Boss drops to silver rupee

--- src/main/java/superworldsun/superslegend/CustomLootMobs/CustomLootWither.java
+++ src/main/java/superworldsun/superslegend/CustomLootMobs/CustomLootWither.java
@@ -17,7 +17,7 @@ public class CustomLootWither
 
         if(event.getEntityLiving() instanceof WitherEntity) {
             if(random.nextInt(1) == 0)
-                event.getEntityLiving().entityDropItem(new ItemStack(ItemList.orange_rupee,1));
+                event.getEntityLiving().entityDropItem(new ItemStack(ItemList.silver_rupee,2));
             
             if(random.nextInt(1) == 0)
                 event.getEntityLiving().entityDropItem(new ItemStack(ItemList.triforce_power_shard,1));
