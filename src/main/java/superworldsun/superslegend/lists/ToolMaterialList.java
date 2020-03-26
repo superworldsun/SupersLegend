ebe043201f166bf4a76f5938dddc8b231f66cfa6
superworldsun <superworldsun@gmail.com>
2020-03-26 04:33:45 -0400

    Update ToolMaterialList.java

--- src/main/java/superworldsun/superslegend/lists/ToolMaterialList.java
+++ src/main/java/superworldsun/superslegend/lists/ToolMaterialList.java
@@ -15,28 +15,37 @@ public enum ToolMaterialList implements IItemTier
 		rupee_sword			(2, -2f, 64, 0, 0, ItemList.rupee_sword),
 		blue_rupee_sword	(3, -2f, 122, 1, 0, ItemList.blue_rupee_sword),
 		red_rupee_sword		(4, -2f, 220, 2, 0, ItemList.red_rupee_sword),
-		orange_rupee_sword	(5, -2f, 1244, 3, 0, ItemList.orange_rupee_sword),
+		silver_rupee_sword	(5, -2f, 822, 3, 0, ItemList.silver_rupee_sword),
+		gold_rupee_sword	(5, -2f, 1658, 3, 0, ItemList.gold_rupee_sword),
 		
 		//Tools
 		rupee_pickaxe		(1, 2f, 64, 0, 0, ItemList.rupee_pickaxe),
 		blue_rupee_pickaxe	(2, 3f, 122, 1, 0, ItemList.blue_rupee_pickaxe),
 		red_rupee_pickaxe	(2, 4f, 220, 2, 0, ItemList.red_rupee_pickaxe),
-		orange_rupee_pickaxe(3, 5f, 1244, 3, 0, ItemList.orange_rupee_pickaxe),
+		silver_rupee_pickaxe(3, 5f, 822, 3, 0, ItemList.silver_rupee_pickaxe),
+		gold_rupee_pickaxe	(3, 5f, 1658, 3, 0, ItemList.gold_rupee_pickaxe),
+
 		
 		rupee_axe			(1, 2f, 64, 0, 0, ItemList.rupee_axe),
 		blue_rupee_axe		(2, 3f, 122, 1, 0, ItemList.blue_rupee_axe),
 		red_rupee_axe		(2, 4f, 220, 2, 0, ItemList.red_rupee_axe),
-		orange_rupee_axe	(3, 5f, 1244, 3, 0, ItemList.orange_rupee_axe),
+		silver_rupee_axe	(3, 5f, 822, 3, 0, ItemList.silver_rupee_axe),
+		gold_rupee_axe		(3, 5f, 1658, 3, 0, ItemList.gold_rupee_axe),
+
 		
 		rupee_shovel			(1, 2f, 64, 0, 0, ItemList.rupee_shovel),
 		blue_rupee_shovel		(2, 3f, 122, 1, 0, ItemList.blue_rupee_shovel),
 		red_rupee_shovel		(2, 4f, 220, 2, 0, ItemList.red_rupee_shovel),
-		orange_rupee_shovel		(3, 5f, 1244, 3, 0, ItemList.orange_rupee_shovel),
+		silver_rupee_shovel		(3, 5f, 822, 3, 0, ItemList.silver_rupee_shovel),
+		gold_rupee_shovel		(3, 5f, 1658, 3, 0, ItemList.gold_rupee_shovel),
+
 		
 		rupee_hoe		(1, 2f, 84, 0, 0, ItemList.rupee_hoe),
 		blue_rupee_hoe	(2, 3f, 164, 1, 0, ItemList.blue_rupee_hoe),
 		red_rupee_hoe	(2, 4f, 310, 2, 0, ItemList.red_rupee_hoe),
-		orange_rupee_hoe(3, 5f, 1300, 3, 0, ItemList.orange_rupee_hoe);
+		silver_rupee_hoe(3, 5f, 822, 3, 0, ItemList.silver_rupee_hoe),
+		gold_rupee_hoe	(3, 5f, 1733, 3, 0, ItemList.gold_rupee_hoe);
+
 	
 	private float attackDamage, efficiency;
 	private int durability, harvestLevel, enchantability;
