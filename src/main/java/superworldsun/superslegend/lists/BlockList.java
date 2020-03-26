2689416dbfe29c5f204e92e42599b8e1e7594c08
superworldsun <superworldsun@gmail.com>
2020-03-26 04:35:01 -0400

    Update BlockList.java

--- src/main/java/superworldsun/superslegend/lists/BlockList.java
+++ src/main/java/superworldsun/superslegend/lists/BlockList.java
@@ -7,7 +7,8 @@ public class BlockList
 	public static Block rupee_block;
 	public static Block blue_rupee_block;
 	public static Block red_rupee_block;
-	public static Block orange_rupee_block;
+	public static Block silver_rupee_block;
+	public static Block gold_rupee_block;
 	public static Block spikes_block;
 	public static Block gossip_stone_block;
 	public static Block bush_block;
@@ -16,6 +17,7 @@ public class BlockList
 	public static Block torch_tower_block_top;
 	public static Block deku_flower_block;
 	public static Block pot_block;
+	public static Block jar_block;
 	public static Block grate_block;
 	public static Block grass_patch_block;
 	public static Block master_ore_block;
