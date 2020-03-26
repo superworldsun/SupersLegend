acff7ba77ad60ff4e88704dd5a042553478ecca9
superworldsun <superworldsun@gmail.com>
2020-03-26 03:14:58 -0400

    Create SoundInit.java

---
+++ src/main/java/superworldsun/superslegend/init/SoundInit.java
@@ -0,0 +1,20 @@
+package superworldsun.superslegend.init;
+
+import net.minecraft.util.SoundEvent;
+
+public class SoundInit
+{
+	
+	//songs
+    public static SoundEvent JAWA;
+    public static SoundEvent SUNS_SONG;
+    
+    
+    //items
+    public static SoundEvent ZELDA_ERROR;
+    public static SoundEvent NAYRUS_LOVE_CAST;
+    public static SoundEvent DASH;
+    public static SoundEvent MAGIC_CAPE_OFF;
+    public static SoundEvent MAGIC_CAPE_ON;
+    
+}
