ab35f175d39bfbaa698fa46c2c4d348ccf88c8c3
superworldsun <superworldsun@gmail.com>
2020-03-26 04:11:50 -0400

    Update Config.java

--- src/main/java/superworldsun/superslegend/config/Config.java
+++ src/main/java/superworldsun/superslegend/config/Config.java
@@ -1,22 +1,49 @@
 package superworldsun.superslegend.config;
 
-/*import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.common.ForgeConfigSpec.Builder;
+
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.config.ModConfig.ConfigReloading;
+
+import static net.minecraftforge.fml.Logging.CORE;
+import static net.minecraftforge.fml.loading.LogMarkers.FORGEMOD;
 
 @Mod.EventBusSubscriber
 public class Config 
 {
-	private static final ForgeConfigSpec.Builder SERVER_BUILDER = new ForgeConfigSpec.Builder();
-	public static final ForgeConfigSpec SERVER;
+	private static final Builder COMMON_BUILDER = new Builder();
+    private static final Builder CLIENT_BUILDER = new Builder();
+
+
+
+    public static final ForgeConfigSpec COMMON_CONFIG = COMMON_BUILDER.build();
+    public static final ForgeConfigSpec CLIENT_CONFIG = CLIENT_BUILDER.build();
 	
-	private static final ForgeConfigSpec.Builder CLIENT_BUILDER = new ForgeConfigSpec.Builder();
-	public static final ForgeConfigSpec CLIENT;
-	
-	static
-	{
-		OregenConfig.init(SERVER_BUILDER, CLIENT_BUILDER);
-		
-		SERVER = SERVER_BUILDER.build();
-		CLIENT = CLIENT_BUILDER.build();
-	}
-}*/
+	public static final class CategoryDeveloper {
+
+        public static ForgeConfigSpec.BooleanValue enableLogger;
+
+        private CategoryDeveloper(){
+            COMMON_BUILDER.comment("Developer Settings").push("DEVELOPER");
+
+            enableLogger = COMMON_BUILDER
+                    .comment("Enable / Disable LogManager logging (DEV)")
+                    .translation("codenamerevy.config.logging")
+                    .define("logger", false);
+            COMMON_BUILDER.pop();
+        }
+    }
+
+    @SubscribeEvent
+    public static void onLoad(final ModConfig.Loading configEvent) {
+        Ref.LOGGER.debug(FORGEMOD, "Loaded {} config file {}", Ref.MODID, configEvent.getConfig().getFileName());
+    }
+
+    @SubscribeEvent
+    public static void onFileChange(final ConfigReloading configEvent) {
+        Ref.LOGGER.fatal(CORE, "{} config just got changed on the file system!", Ref.MODID);
+    }
+}
