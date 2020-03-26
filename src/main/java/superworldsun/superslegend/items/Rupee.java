edb3c7b97dbcad839ec0b703583f8c5724843455
superworldsun <superworldsun@gmail.com>
2020-03-26 04:40:46 -0400

    Update Rupee item

---
+++ src/main/java/superworldsun/superslegend/items/Rupee.java
@@ -0,0 +1,57 @@
+package superworldsun.superslegend.items;
+
+import java.util.List;
+
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Hand;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.world.World;
+import superworldsun.superslegend.SupersLegend;
+import superworldsun.superslegend.init.SoundInit;
+import superworldsun.superslegend.lists.ItemList;
+
+public class Rupee extends Item{
+
+	public Rupee(Properties properties)
+	{
+		super(properties);
+	}
+	
+	public ActionResult<ItemStack> onItemRightClick(World world, PlayerEntity player, Hand hand)
+	 {
+		ItemStack stack = player.getHeldItem(hand);
+		
+		if(stack.getCount() >= 5)
+		 {
+			
+			 stack.shrink(5);
+			
+			 player.addItemStackToInventory(new ItemStack(ItemList.blue_rupee));
+			 
+			 BlockPos currentPos = player.getPosition();
+			 world.playSound(null, currentPos.getX(), currentPos.getY(), currentPos.getZ(), SoundInit.JAWA, SoundCategory.PLAYERS, 1f, 1f);
+		 }
+		        
+	return new ActionResult<>(ActionResultType.PASS, player.getHeldItem(hand));
+		
+	}
+	
+	
+	@Override
+	public void addInformation(ItemStack stack, World world, List<ITextComponent> list, ITooltipFlag flag)
+	{
+		super.addInformation(stack, world, list, flag);				
+		list.add(new StringTextComponent(TextFormatting.GREEN + "1 rupee"));
+	}   
+} 
