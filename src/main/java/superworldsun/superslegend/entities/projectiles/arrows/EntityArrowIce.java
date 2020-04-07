5e91244f4fadb8283c15a611ec23ee0e6a5835aa
superworldsun <superworldsun@gmail.com>
2020-04-07 04:41:57 -0400

    Update EntityArrowIce.java

--- src/main/java/superworldsun/superslegend/entities/projectiles/arrows/EntityArrowIce.java
+++ src/main/java/superworldsun/superslegend/entities/projectiles/arrows/EntityArrowIce.java
@@ -1,12 +1,16 @@
 package superworldsun.superslegend.entities.projectiles.arrows;
 
+import net.minecraft.block.Blocks;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.projectile.ArrowEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import superworldsun.superslegend.init.SoundInit;
 import superworldsun.superslegend.lists.ItemList;
 
 public class EntityArrowIce extends ArrowEntity
@@ -33,6 +37,35 @@ public class EntityArrowIce extends ArrowEntity
         entity.setLastAttackedEntity(null);
         entity.addPotionEffect(new EffectInstance(Effects.SLOWNESS, 200, 255, false, false));
         entity.addPotionEffect(new EffectInstance(Effects.BLINDNESS, 200, 255, false, false));
+        
+        BlockPos currentPos = entity.getPosition();
+        entity.world.playSound(null, currentPos.getX(), currentPos.getY(), currentPos.getZ(), SoundInit.ARROW_HIT_ICE, SoundCategory.PLAYERS, 1f, 1f);
+    }
+    
+    
+    @Override
+    public void tick() {
+        super.tick();
+        if(this.inGround){
+            if(!world.isRaining() && !world.isThundering()){
+                world.setBlockState(this.getPosition(), Blocks.SNOW.getDefaultState(), 11);
+                
+                BlockPos currentPos = this.getPosition();
+                this.world.playSound(null, currentPos.getX(), currentPos.getY(), currentPos.getZ(), SoundInit.ARROW_HIT_ICE, SoundCategory.PLAYERS, 1f, 1f);
+                
+                this.remove();
+            }
+
+        }
+        if(this.isInWater())
+        {
+        	world.setBlockState(this.getPosition(), Blocks.ICE.getDefaultState(), 11);
+        	
+        	BlockPos currentPos = this.getPosition();
+            this.world.playSound(null, currentPos.getX(), currentPos.getY(), currentPos.getZ(), SoundInit.ARROW_HIT_ICE, SoundCategory.PLAYERS, 1f, 1f);
+        	
+        	this.remove();
+        }
     }
 
 
