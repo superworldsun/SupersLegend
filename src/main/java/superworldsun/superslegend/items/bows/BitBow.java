fec85c0c92db0466037d8095b77325f2f22650ec
superworldsun <superworldsun@gmail.com>
2020-04-07 04:42:14 -0400

    Update BitBow.java

--- src/main/java/superworldsun/superslegend/items/bows/BitBow.java
+++ src/main/java/superworldsun/superslegend/items/bows/BitBow.java
@@ -1,32 +1,19 @@
 package superworldsun.superslegend.items.bows;
 
 import java.util.List;
-import java.util.Random;
-
 import net.minecraft.client.util.ITooltipFlag;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.item.EnderPearlEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.ArrowEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
-import net.minecraft.particles.ParticleTypes;
-import net.minecraft.potion.Effect;
-import net.minecraft.potion.EffectInstance;
 import net.minecraft.util.ActionResult;
-import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.SoundEvents;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TextFormatting;
-import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
-import superworldsun.superslegend.init.SoundInit;
 import superworldsun.superslegend.lists.ItemList;
 
 public class BitBow extends Item
@@ -40,8 +27,9 @@ public class BitBow extends Item
 	
 	public ActionResult<ItemStack> onItemRightClick(World world, PlayerEntity player, Hand hand)
 	 {
-		 @SuppressWarnings("unused")
 		ItemStack stack = player.getHeldItem(hand);
+		
+		 if (player.inventory.hasItemStack(new ItemStack(ItemList.rupee)))
 		 {
 			 ArrowEntity ent = new ArrowEntity(world, player); ent.shoot(player, player.rotationPitch, player.rotationYaw, 0.0F, 1.5F, 1.0F);
 			 
@@ -49,20 +37,30 @@ public class BitBow extends Item
 			 
 			 world.addEntity(ent);
 			 
-			 
+			 for (int i = 0; i < player.inventory.getSizeInventory(); i++) 
+         	{
+       		  ItemStack itemStack = player.inventory.getStackInSlot(i);
+       		  if (itemStack .getItem() == ItemList.rupee) 
+       		  {
+       		    itemStack .shrink(stack.getMaxStackSize());
+       		  }
+         	}
 			 
 			 player.getCooldownTracker().setCooldown(this, 30);
 			 
 			
 			 return super.onItemRightClick(world, player, hand);
 		}
+		
+		
+		return null;
 	 }
 	
 	@Override
 	public void addInformation(ItemStack stack, World world, List<ITextComponent> list, ITooltipFlag flag)
 	{
 		super.addInformation(stack, world, list, flag);				
-		list.add(new StringTextComponent(TextFormatting.GRAY + "Uses Stamina on use"));
+		list.add(new StringTextComponent(TextFormatting.YELLOW + "Uses Green Rupee as ammo"));
 	}  
 	
 }
