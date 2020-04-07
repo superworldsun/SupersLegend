3edad045d7539f34130275660f6d9c2ba2ad8d7d
superworldsun <superworldsun@gmail.com>
2020-04-07 04:41:53 -0400

    Update EntityArrowFire.java

--- src/main/java/superworldsun/superslegend/entities/projectiles/arrows/EntityArrowFire.java
+++ src/main/java/superworldsun/superslegend/entities/projectiles/arrows/EntityArrowFire.java
@@ -5,7 +5,10 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.projectile.ArrowEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import superworldsun.superslegend.init.SoundInit;
 import superworldsun.superslegend.lists.ItemList;
 
 public class EntityArrowFire extends ArrowEntity
@@ -31,6 +34,9 @@ public class EntityArrowFire extends ArrowEntity
         super.arrowHit(entity);
         if(!world.isRaining() && !world.isThundering()){
             entity.setFire(6);
+            
+            BlockPos currentPos = entity.getPosition();
+            entity.world.playSound(null, currentPos.getX(), currentPos.getY(), currentPos.getZ(), SoundInit.ARROW_HIT_FIRE, SoundCategory.PLAYERS, 1f, 1f);
         }
     }
 
@@ -38,12 +44,26 @@ public class EntityArrowFire extends ArrowEntity
     public void tick() {
         super.tick();
         if(this.inGround){
-            if(!world.isRaining() && !world.isThundering()){
+            if(!world.isRaining() && !world.isThundering() && !this.isInWater()){
+            	
                 world.setBlockState(this.getPosition(), Blocks.FIRE.getDefaultState(), 11);
+                
+                BlockPos currentPos = this.getPosition();
+                this.world.playSound(null, currentPos.getX(), currentPos.getY(), currentPos.getZ(), SoundInit.ARROW_HIT_FIRE, SoundCategory.PLAYERS, 1f, 1f);
+                
                 this.remove();
             }
 
         }
+        if (this.isAirBorne) 
+        {
+        	
+        	this.setFire(99);
+        	
+        }
+        if(this.isInWater())
+        {
+        	this.remove();
+        }
     }
-
 }
