678d5a30fcdaf1e9280f1c50d9eb4b6b585c1692
superworldsun <superworldsun@gmail.com>
2020-04-07 04:42:19 -0400

    Update ItemCustomShield.java

--- src/main/java/superworldsun/superslegend/items/ItemCustomShield.java
+++ src/main/java/superworldsun/superslegend/items/ItemCustomShield.java
@@ -1,9 +1,14 @@
 package superworldsun.superslegend.items;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ShieldItem;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.World;
 
 public class ItemCustomShield extends ShieldItem
 {
@@ -19,10 +24,52 @@ public class ItemCustomShield extends ShieldItem
 	
 	
 	
+	public void inventoryTick(ItemStack stack, World world, Entity entity, int itemSlot, boolean isSelected)
+	{		
+		if(entity instanceof PlayerEntity && !world.isRemote)
+		{
+			PlayerEntity player = (PlayerEntity)entity;
+			ItemStack equipped = player.getHeldItemMainhand();
+			if(!world.isRemote)
+			{
+				if(stack == equipped)
+		        {
+					if(player.isBurning())
+					{
+						stack.setDamage(337);
+						
+						player.sendStatusMessage(new TranslationTextComponent(TextFormatting.RED + "Your Shield has been Burnt"), true);
+					}
+		        }
+			}	
+
+		}
+		if(entity instanceof PlayerEntity && !world.isRemote)
+		{
+			PlayerEntity player = (PlayerEntity)entity;
+			ItemStack equipped = player.getHeldItemOffhand();
+			if(!world.isRemote)
+			{
+				if(stack == equipped)
+		        {
+					if(player.isBurning())
+					{
+						stack.setDamage(337);
+						
+						player.sendStatusMessage(new TranslationTextComponent(TextFormatting.RED + "Your Shield has been Burnt"), true);
+					}
+		        }
+			}
+		}
+	}
+	
+	
 	      @Override
 	      public boolean isBookEnchantable(ItemStack stack, ItemStack book) {
 	          // TODO Auto-generated method stub
 	    	  return false;	
 	
 	}
+	      
+	      
 }
