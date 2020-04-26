70f55b9e17e00d58d1425e9e7e24b03c1a3c9b63
superworldsun <superworldsun@gmail.com>
2020-04-26 03:27:29 -0400

    Update ItemCustomShield.java

--- src/main/java/superworldsun/superslegend/items/ItemCustomShield.java
+++ src/main/java/superworldsun/superslegend/items/ItemCustomShield.java
@@ -6,6 +6,9 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ShieldItem;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvents;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
@@ -26,37 +29,41 @@ public class ItemCustomShield extends ShieldItem
 	
 	public void inventoryTick(ItemStack stack, World world, Entity entity, int itemSlot, boolean isSelected)
 	{		
-		if(entity instanceof PlayerEntity && !world.isRemote)
+		if(entity instanceof PlayerEntity)
 		{
 			PlayerEntity player = (PlayerEntity)entity;
 			ItemStack equipped = player.getHeldItemMainhand();
-			if(!world.isRemote)
 			{
 				if(stack == equipped)
 		        {
 					if(player.isBurning())
 					{
-						stack.setDamage(337);
+						stack.shrink(1);
 						
-						player.sendStatusMessage(new TranslationTextComponent(TextFormatting.RED + "Your Shield has been Burnt"), true);
+						BlockPos currentPos = player.getPosition();
+						 world.playSound(null, currentPos.getX(), currentPos.getY(), currentPos.getZ(), SoundEvents.ITEM_SHIELD_BREAK, SoundCategory.PLAYERS, 1f, 1f);
+						 
+						player.sendStatusMessage(new TranslationTextComponent(TextFormatting.RED + "Your shield is gone!"), true);
 					}
 		        }
 			}	
 
 		}
-		if(entity instanceof PlayerEntity && !world.isRemote)
+		if(entity instanceof PlayerEntity)
 		{
 			PlayerEntity player = (PlayerEntity)entity;
 			ItemStack equipped = player.getHeldItemOffhand();
-			if(!world.isRemote)
 			{
 				if(stack == equipped)
 		        {
 					if(player.isBurning())
 					{
-						stack.setDamage(337);
+						stack.shrink(1);
 						
-						player.sendStatusMessage(new TranslationTextComponent(TextFormatting.RED + "Your Shield has been Burnt"), true);
+						BlockPos currentPos = player.getPosition();
+						 world.playSound(null, currentPos.getX(), currentPos.getY(), currentPos.getZ(), SoundEvents.ITEM_SHIELD_BREAK, SoundCategory.PLAYERS, 1f, 1f);
+						 
+						player.sendStatusMessage(new TranslationTextComponent(TextFormatting.RED + "Your shield is gone!"), true);
 					}
 		        }
 			}
