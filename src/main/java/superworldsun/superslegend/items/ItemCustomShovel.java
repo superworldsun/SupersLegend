26b6766c61968ed2b4093f2b0a063ff8cfb510d5
superworldsun <superworldsun@gmail.com>
2020-04-27 02:22:09 -0400

    Create ItemCustomShovel.java

---
+++ src/main/java/superworldsun/superslegend/items/ItemCustomShovel.java
@@ -0,0 +1,21 @@
+package superworldsun.superslegend.items;
+
+import net.minecraft.item.IItemTier;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.PickaxeItem;
+import net.minecraft.item.ShovelItem;
+
+public class ItemCustomShovel extends ShovelItem
+{
+
+	public ItemCustomShovel(IItemTier tier, int attackDamageIn, float attackSpeedIn, Properties builder) 
+	{
+		super(tier, attackDamageIn, attackSpeedIn, builder);
+	}
+	
+	@Override
+    public boolean isBookEnchantable(ItemStack stack, ItemStack book) {
+        // TODO Auto-generated method stub
+		return false;
+	}
+}
