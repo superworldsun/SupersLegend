400026b7490d1e26293a02954ea1c3dc0235f567
superworldsun <superworldsun@gmail.com>
2020-09-29 02:03:09 -0400

    Update MoonPearl.java

--- src/main/java/superworldsun/superslegend/items/MoonPearl.java
+++ src/main/java/superworldsun/superslegend/items/MoonPearl.java
@@ -20,67 +20,58 @@ import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
 import net.minecraft.world.dimension.DimensionType;
 
-/*public class MoonPearl extends Item
-{
+public class MoonPearl extends Item {
 
-	public MoonPearl(Properties properties)
-	{
-		super(properties);
-	}
-	
-	@Override
-	public ActionResult<ItemStack> onItemRightClick(World world, PlayerEntity entity, Hand hand)
-	{
-		ItemStack stack = entity.getHeldItem(hand);
-		
-		int currentDim = entity.dimension.getId();
-		
-		if(!world.isRemote)
-		{
-			if(!(currentDim == 1))
-			{
-				Random rand = entity.world.rand;
-		        for (int i = 0; i < 45; i++)
-		        {
-		        	entity.world.addParticle(ParticleTypes.DRAGON_BREATH,
-		        			entity.posX + (rand.nextBoolean() ? -1 : 1) * Math.pow(rand.nextFloat(), 2) * 2,
-		        			entity.posY + rand.nextFloat() * 3 - 2,
-		        			entity.posZ + (rand.nextBoolean() ? -1 : 1) * Math.pow(rand.nextFloat(), 2) * 2,
-		                    0, 0.105D, 0);
-		        }
-		        
-		        BlockPos currentPos = entity.getPosition();
-				 world.playSound(null, currentPos.getX(), currentPos.getY(), currentPos.getZ(), SoundEvents.ITEM_CHORUS_FRUIT_TELEPORT, SoundCategory.PLAYERS, 1f, 1f);
-				entity.changeDimension(DimensionType.THE_NETHER);
-		    	return ActionResult.newResult(ActionResultType.SUCCESS, stack);
-			}
-			else if(((currentDim == 1)) && (entity.isCrouching()))
-			{
-				Random rand = entity.world.rand;
-		        for (int i = 0; i < 45; i++)
-		        {
-		        	entity.world.addParticle(ParticleTypes.DRAGON_BREATH,
-		        			entity.posX + (rand.nextBoolean() ? -1 : 1) * Math.pow(rand.nextFloat(), 2) * 2,
-		        			entity.posY + rand.nextFloat() * 3 - 2,
-		        			entity.posZ + (rand.nextBoolean() ? -1 : 1) * Math.pow(rand.nextFloat(), 2) * 2,
-		                    0, 0.105D, 0);
-		        }
-		        
-		        BlockPos currentPos = entity.getPosition();
-				 world.playSound(null, currentPos.getX(), currentPos.getY(), currentPos.getZ(), SoundEvents.ITEM_CHORUS_FRUIT_TELEPORT, SoundCategory.PLAYERS, 1f, 1f);
-				entity.changeDimension(DimensionType.OVERWORLD);
-		    	return ActionResult.newResult(ActionResultType.SUCCESS, stack);
-			}
-		}
-	    
-	    return ActionResult.newResult(ActionResultType.FAIL, stack);		 
-	}
-	
+    public MoonPearl(Properties properties) {
+        super(properties);
+    }
+
     @Override
-	public void addInformation(ItemStack stack, World world, List<ITextComponent> list, ITooltipFlag flag)
-	{
-		super.addInformation(stack, world, list, flag);				
-		list.add(new StringTextComponent(TextFormatting.RED + "Teleports the player to the Nether"));
-		list.add(new StringTextComponent(TextFormatting.GREEN + "Right-click to use"));
-		list.add(new StringTextComponent(TextFormatting.GREEN + "When used in the Nether it will Return player to Portal"));
-	} */
+    public ActionResult<ItemStack> onItemRightClick(World world, PlayerEntity entity, Hand hand) {
+        ItemStack stack = entity.getHeldItem(hand);
+
+        int currentDim = entity.dimension.getId();
+
+        if (!world.isRemote) {
+            if (!(currentDim == 1)) {
+                Random rand = entity.world.rand;
+                for (int i = 0; i < 45; i++) {
+                    entity.world.addParticle(ParticleTypes.DRAGON_BREATH,
+                            entity.prevPosX + (rand.nextBoolean() ? -1 : 1) * Math.pow(rand.nextFloat(), 2) * 2,
+                            entity.prevPosY + rand.nextFloat() * 3 - 2,
+                            entity.prevPosZ + (rand.nextBoolean() ? -1 : 1) * Math.pow(rand.nextFloat(), 2) * 2,
+                            0, 0.105D, 0);
+                }
+
+                BlockPos currentPos = entity.getPosition();
+                world.playSound(null, currentPos.getX(), currentPos.getY(), currentPos.getZ(), SoundEvents.ITEM_CHORUS_FRUIT_TELEPORT, SoundCategory.PLAYERS, 1f, 1f);
+                entity.changeDimension(DimensionType.THE_NETHER);
+                return ActionResult.resultSuccess(stack);
+            } else if (((currentDim == 1)) && (entity.isCrouching())) {
+                Random rand = entity.world.rand;
+                for (int i = 0; i < 45; i++) {
+                    entity.world.addParticle(ParticleTypes.DRAGON_BREATH,
+                            entity.prevPosX + (rand.nextBoolean() ? -1 : 1) * Math.pow(rand.nextFloat(), 2) * 2,
+                            entity.prevPosY + rand.nextFloat() * 3 - 2,
+                            entity.prevPosZ + (rand.nextBoolean() ? -1 : 1) * Math.pow(rand.nextFloat(), 2) * 2,
+                            0, 0.105D, 0);
+                }
+
+                BlockPos currentPos = entity.getPosition();
+                world.playSound(null, currentPos.getX(), currentPos.getY(), currentPos.getZ(), SoundEvents.ITEM_CHORUS_FRUIT_TELEPORT, SoundCategory.PLAYERS, 1f, 1f);
+                entity.changeDimension(DimensionType.OVERWORLD);
+                return ActionResult.resultSuccess(stack);
+            }
+        }
+
+        return ActionResult.resultFail(stack);
+    }
+
+    @Override
+    public void addInformation(ItemStack stack, World world, List<ITextComponent> list, ITooltipFlag flag) {
+        super.addInformation(stack, world, list, flag);
+        list.add(new StringTextComponent(TextFormatting.RED + "Teleports the player to the Nether"));
+        list.add(new StringTextComponent(TextFormatting.GREEN + "Right-click to use"));
+        list.add(new StringTextComponent(TextFormatting.GREEN + "When used in the Nether it will Return player to Portal"));
+    }
+}
