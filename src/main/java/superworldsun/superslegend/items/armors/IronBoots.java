5c9a13588290dbbb9cf343e36313fe2f51eb87cc
Riley Brown <30506033+X-Niter@users.noreply.github.com>
2021-01-14 05:11:39 -0600

    IronBoots Effects tweaks

--- src/main/java/superworldsun/superslegend/items/armors/IronBoots.java
+++ src/main/java/superworldsun/superslegend/items/armors/IronBoots.java
@@ -6,10 +6,13 @@ import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.*;
 import net.minecraft.potion.Effect;
 import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.EffectType;
+import net.minecraft.potion.Effects;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
+import net.minecraftforge.common.ForgeMod;
 import superworldsun.superslegend.SupersLegend;
 import superworldsun.superslegend.items.NonEnchantArmor;
 import superworldsun.superslegend.lists.ArmourMaterialList;
@@ -67,27 +70,18 @@ public class IronBoots extends NonEnchantArmor {
             		player.addPotionEffect(new EffectInstance(PotionList.iron_boots_effect, 8, 0, false, false));
 
             	}
-            	else
+            	else if (!player.isInWater())
             	{
             		
             		//Vec3d v = player.getMotion();
 					//player.setMotion(v.x, v.y * -1.0D, v.z);
             		
-            		player.addPotionEffect(new EffectInstance(Effect.get(2), 10, 1, false, false));
+            		player.addPotionEffect(new EffectInstance(Effects.SLOWNESS, 10, 1, false, false));
             		
             		player.removePotionEffect(PotionList.iron_boots_effect);
             		//player.removePotionEffect(Effect.get(30));
             	}
                 
             	}
-    	
-
     }
-    
-    //player.getMotion().getX() != 0 && player.getMotion().getY() != 0
-    
-    //@Override
-   // public boolean hasEffect(ItemStack stack) {
-        //return false;
-   // }
 }
