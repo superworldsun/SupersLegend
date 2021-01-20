4323345c27467522880622f3e9414db16cc245ec
superworldsun <superworldsun@gmail.com>
2021-01-20 00:39:03 -0500

    Fixed BitBow crash

--- src/main/java/superworldsun/superslegend/items/bows/BitBow.java
+++ src/main/java/superworldsun/superslegend/items/bows/BitBow.java
@@ -6,14 +6,12 @@ import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.Enchantments;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.entity.projectile.AbstractArrowEntity;
 import net.minecraft.entity.projectile.ArrowEntity;
 import net.minecraft.item.*;
 import net.minecraft.stats.Stats;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
-import net.minecraft.util.SoundCategory;
-import net.minecraft.util.SoundEvents;
+import net.minecraft.util.*;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TextFormatting;
@@ -29,40 +27,43 @@ public class BitBow extends BowItem
 		super(properties);
 	}
 
-    /*public ActionResult<ItemStack> onItemRightClick(World world, PlayerEntity player, Hand hand)
-    {
-        ItemStack stack = player.getHeldItem(hand);
+	@Override
+	public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn)
+	{
 
-        if (player.inventory.hasItemStack(new ItemStack(ItemList.rupee)))
-        {
-            ArrowEntity ent = new ArrowEntity(world, player); ent.shoot(player, player.rotationPitch, player.rotationYaw, 0.0F, 1.5F, 1.0F);
+		if (!worldIn.isRemote && playerIn.inventory.hasItemStack(new ItemStack(ItemList.rupee)))
+		{
+			playerIn.getCooldownTracker().setCooldown(this, 20);
 
-			 world.addEntity(ent);
+			ArrowItem itemarrow = (ArrowItem)Items.ARROW;
+			AbstractArrowEntity entityarrow = itemarrow.createArrow(worldIn, new ItemStack(Items.ARROW), playerIn);
+			float arrowVelocity = 2.0F;
+			entityarrow.func_234612_a_(playerIn, playerIn.rotationPitch, playerIn.rotationYaw, 0.0F, arrowVelocity, 1.0F);
+			entityarrow.setDamage(1);
+			worldIn.addEntity(entityarrow);
+			entityarrow.pickupStatus = AbstractArrowEntity.PickupStatus.DISALLOWED;
+
+			for (int i = 0; i < playerIn.inventory.getSizeInventory(); i++)
+			{
+				ItemStack itemStack = playerIn.inventory.getStackInSlot(i);
+				if (itemStack.getItem() == ItemList.rupee)
+				{
+					itemStack.shrink(1);
+				}
+			}
 
 
-            for (int i = 0; i < player.inventory.getSizeInventory(); i++)
-            {
-                ItemStack itemStack = player.inventory.getStackInSlot(i);
-                if (itemStack .getItem() == ItemList.rupee)
-                {
-                    itemStack .shrink(stack.getMaxStackSize());
-                }
-            }
-
-            player.getCooldownTracker().setCooldown(this, 30);
 
 
-            return super.onItemRightClick(world, player, hand);
-        }else{
-            return null;
-        }
-    }
+		}
+		return new ActionResult<ItemStack>(ActionResultType.PASS, playerIn.getHeldItem(handIn));
+	}
 
 	@Override
 	public void addInformation(ItemStack stack, World world, List<ITextComponent> list, ITooltipFlag flag)
 	{
 		super.addInformation(stack, world, list, flag);				
 		list.add(new StringTextComponent(TextFormatting.YELLOW + "Uses Green Rupee as ammo"));
-	}*/
+	}
 	
 }
