5498343b8de211f1a6081c011b2e1d4752e81a4d
Riley Brown <30506033+X-Niter@users.noreply.github.com>
2021-01-20 22:44:24 -0600

    Turning off MoonPearl sense I can't find a way to put the player in the dimension safely.

--- src/main/java/superworldsun/superslegend/items/MoonPearl.java
+++ src/main/java/superworldsun/superslegend/items/MoonPearl.java
@@ -1,23 +1,33 @@
 package superworldsun.superslegend.items;
 
 import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUseContext;
 import net.minecraft.particles.ParticleTypes;
 import net.minecraft.util.*;
 import net.minecraft.util.concurrent.TickDelayedTask;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TextFormatting;
+import net.minecraft.world.DimensionType;
+import net.minecraft.world.Teleporter;
 import net.minecraft.world.World;
 import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.common.util.ITeleporter;
+import net.minecraftforge.event.world.BlockEvent;
+import net.minecraftforge.event.world.WorldEvent;
 
+import java.net.ServerSocket;
 import java.util.List;
+import java.util.Objects;
 import java.util.Random;
+import java.util.function.Function;
 
 public class MoonPearl extends Item {
 
@@ -25,7 +35,13 @@ public class MoonPearl extends Item {
         super(properties.maxStackSize(16));
     }
 
-    public ActionResult<ItemStack> onItemRightClick(World world, PlayerEntity playerIn, Hand hand) {
+    // Huge issue with player teleporting across dimensions.
+    // We can get them in the dimensions, but were failing to position the player in a safe place.
+
+
+
+
+    /*public ActionResult<ItemStack> onItemRightClick(World world, PlayerEntity playerIn, Hand hand) {
 
         ItemStack stack = playerIn.getHeldItem(hand);
 
@@ -43,25 +59,24 @@ public class MoonPearl extends Item {
                 BlockPos currentPos = playerIn.getPosition();
                 world.playSound(null, currentPos.getX(), currentPos.getY(), currentPos.getZ(), SoundEvents.ITEM_CHORUS_FRUIT_TELEPORT, SoundCategory.PLAYERS, 1f, 1f);
 
+
                 //Dimension changing setup
                 ServerPlayerEntity player = (ServerPlayerEntity) playerIn;
+
                 //BlockPos destinationPos = player.getPosition();
-                BlockPos destinationPos = new BlockPos(0, 70, 0);
-                final RegistryKey<World> THE_NETHER = World.THE_NETHER;
-                ServerWorld targetWorld = world.getServer().getWorld(THE_NETHER);
-                targetWorld.getServer().enqueue(new TickDelayedTask(0, () -> {
-                    targetWorld.getChunk(destinationPos.getX() >> 5, destinationPos.getZ() >> 5);
+                BlockPos destinationPos = new BlockPos(currentPos.getX(), currentPos.getY(), currentPos.getZ());
+
+                ServerWorld targetWorld = world.getServer().getWorld(world.THE_NETHER);
+                targetWorld.getServer().enqueue(new TickDelayedTask(1, () -> {
+                    targetWorld.getChunk(destinationPos.getX(), destinationPos.getZ());
                     //Teleporting the player to the Nether and updating position
-                    playerIn.changeDimension(targetWorld, new ITeleporter() {});
-                    //player.teleport(targetWorld, destinationPos.getX(), destinationPos.getY(), destinationPos.getZ(), player.rotationYaw, player.prevRotationPitch);
-                    player.fallDistance = 0;
+                    player.teleport(targetWorld, destinationPos.getX(), destinationPos.getY(), destinationPos.getZ(), player.rotationYaw, player.prevRotationPitch);
                 }));
 
                 //playerIn.changeDimension(targetWorld, new ITeleporter() {}).setPositionAndUpdate(targetVec.getX(), targetVec.getY(), targetVec.getZ());
                 return new ActionResult<>(ActionResultType.SUCCESS, playerIn.getHeldItem(hand));
 
-            }
-            else if(playerIn instanceof ServerPlayerEntity && world.getDimensionKey().equals(World.THE_NETHER)) {
+            } else if(playerIn instanceof ServerPlayerEntity && world.getDimensionKey().equals(World.THE_NETHER)) {
                 Random rand = playerIn.world.rand;
                 //Particle Effects when using item
                 for (int i = 0; i < 45; i++) {
@@ -85,7 +100,6 @@ public class MoonPearl extends Item {
                     targetWorld.getChunk(destinationPos.getX(), destinationPos.getZ());
                     //Teleporting the player to the Nether and updating position
                     player.teleport(targetWorld, destinationPos.getX(), destinationPos.getY(), destinationPos.getZ(), player.rotationYaw, player.prevRotationPitch);
-                    player.fallDistance = 0;
                 }));
 
                 //playerIn.changeDimension(world.getServer().getWorld(World.OVERWORLD), new ITeleporter() {});
@@ -96,7 +110,7 @@ public class MoonPearl extends Item {
         }
 
         return new ActionResult<>(ActionResultType.SUCCESS, playerIn.getHeldItem(hand));
-    }
+    }*/
 
 
     @Override
