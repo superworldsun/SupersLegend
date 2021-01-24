2c082747e5d0a84b4c94b854a14bb32c902429b3
superworldsun <superworldsun@gmail.com>
2021-01-24 05:21:46 -0500

    Add Barbarian Armor Set

---
+++ src/main/java/superworldsun/superslegend/items/armors/ArmorBarbarianEffects.java
@@ -0,0 +1,53 @@
+package superworldsun.superslegend.items.armors;
+
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.Effect;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.world.World;
+import superworldsun.superslegend.SupersLegend;
+import superworldsun.superslegend.items.NonEnchantArmor;
+import superworldsun.superslegend.lists.ArmourMaterialList;
+import superworldsun.superslegend.lists.ItemList;
+
+
+public class ArmorBarbarianEffects extends NonEnchantArmor
+{
+    public ArmorBarbarianEffects(String name, EquipmentSlotType slot)
+    
+    {
+        super(ArmourMaterialList.barbarian, slot, new Properties().group(SupersLegend.supers_legend));
+        setRegistryName(SupersLegend.modid, name);
+    }
+        
+    public void addInformation(ItemStack stack, World world, java.util.List<ITextComponent> list, ITooltipFlag flag)
+	{
+		super.addInformation(stack, world, list, flag);				
+		list.add(new StringTextComponent(TextFormatting.RED + "Armor once worn by warriors from the Faron region"));
+		list.add(new StringTextComponent(TextFormatting.GREEN + "Wearing full set grants strength"));
+	}
+
+    @Override
+    public void onArmorTick(ItemStack stack, World world, PlayerEntity player) 
+    {
+    	
+    	
+    	
+        if (!world.isRemote){
+        		boolean isHelmetOn = player.getItemStackFromSlot(EquipmentSlotType.HEAD).getItem().equals(ItemList.flamebreaker_helmet);
+                boolean isChestplateOn = player.getItemStackFromSlot(EquipmentSlotType.CHEST).getItem().equals(ItemList.flamebreaker_tunic);
+                boolean isLeggingsOn = player.getItemStackFromSlot(EquipmentSlotType.LEGS).getItem().equals(ItemList.flamebreaker_leggings);
+                boolean isBootsOn = player.getItemStackFromSlot(EquipmentSlotType.FEET).getItem().equals(ItemList.flamebreaker_boots);
+                if(isHelmetOn&isChestplateOn&isLeggingsOn&isBootsOn)
+                	{
+                		player.addPotionEffect(new EffectInstance(Effect.get(12), 10, 0, false, false, false));
+                	}
+                	
+                }
+    }
+}
