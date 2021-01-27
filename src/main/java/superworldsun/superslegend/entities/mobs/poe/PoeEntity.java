43607196459161db6f864b25bd86915faa28c1cb
zOnlyKroks <64548817+zOnlyKroks@users.noreply.github.com>
2021-01-27 10:55:22 +0100

    Poe Entity Ai works kinda

--- src/main/java/superworldsun/superslegend/entities/mobs/poe/PoeEntity.java
+++ src/main/java/superworldsun/superslegend/entities/mobs/poe/PoeEntity.java
@@ -1,25 +1,63 @@
 package superworldsun.superslegend.entities.mobs.poe;
 
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.MobEntity;
+import net.minecraft.entity.*;
+import net.minecraft.entity.ai.RandomPositionGenerator;
 import net.minecraft.entity.ai.attributes.AttributeModifierMap;
 import net.minecraft.entity.ai.attributes.Attributes;
+import net.minecraft.entity.ai.controller.FlyingMovementController;
+import net.minecraft.entity.ai.controller.MovementController;
+import net.minecraft.entity.ai.goal.Goal;
+import net.minecraft.entity.ai.goal.NearestAttackableTargetGoal;
+import net.minecraft.entity.ai.goal.ZombieAttackGoal;
 import net.minecraft.entity.monster.MonsterEntity;
+import net.minecraft.entity.monster.PhantomEntity;
+import net.minecraft.entity.monster.VexEntity;
 import net.minecraft.entity.passive.IFlyingAnimal;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.pathfinding.FlyingPathNavigator;
+import net.minecraft.pathfinding.PathNavigator;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.world.World;
+import superworldsun.superslegend.entities.ai.PoeAttackGoal;
+
+import java.util.Comparator;
+import java.util.List;
+
 
 public class PoeEntity extends MonsterEntity implements IFlyingAnimal {
 
-
     public PoeEntity(EntityType<? extends MonsterEntity> type, World worldIn) {
         super(type, worldIn);
+        this.moveController = new FlyingMovementController(this, 10, false);
+    }
+
+
+
+    @Override
+    protected void registerGoals() {
+        this.goalSelector.addGoal(1, new PoeAttackGoal(this, 0.8D, true));
+        this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, PlayerEntity.class, true));
+        this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, LivingEntity.class, true));
     }
 
     public static AttributeModifierMap.MutableAttribute prepareAttributes() {
         return MonsterEntity.func_234295_eP_()
                 .createMutableAttribute(Attributes.MAX_HEALTH, 20.0D)
                 .createMutableAttribute(Attributes.FLYING_SPEED, 0.4F)
-                .createMutableAttribute(Attributes.ATTACK_DAMAGE, 0.5D)
-                .createMutableAttribute(Attributes.MOVEMENT_SPEED, 0.2F);
+                .createMutableAttribute(Attributes.ATTACK_DAMAGE, 2.0D);
+    }
+
+    protected PathNavigator createNavigator(World worldIn) {
+        FlyingPathNavigator flyingpathnavigator = new FlyingPathNavigator(this, worldIn);
+        flyingpathnavigator.setCanOpenDoors(false);
+        flyingpathnavigator.setCanSwim(true);
+        flyingpathnavigator.setCanEnterDoors(true);
+        return flyingpathnavigator;
+    }
+
+    public boolean onLivingFall(float distance, float damageMultiplier) {
+        return false;
     }
 }
