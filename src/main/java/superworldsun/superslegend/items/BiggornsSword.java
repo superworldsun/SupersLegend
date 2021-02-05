6c853def639c809e3bffc223f20ba219ffdf7b95
superworldsun <superworldsun@gmail.com>
2021-02-05 16:15:41 -0500

    Add BiggoronsSword

---
+++ src/main/java/superworldsun/superslegend/items/BiggornsSword.java
@@ -0,0 +1,54 @@
+package superworldsun.superslegend.items;
+
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.IItemTier;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ShieldItem;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvents;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.World;
+
+import java.util.List;
+
+public class BiggornsSword extends ItemCustomSword
+{
+	public BiggornsSword(IItemTier tier, int attackDamageIn, float attackSpeedIn, Properties builder)
+	{
+		super(tier, attackDamageIn, attackSpeedIn, builder);
+	}
+
+	@Override
+	public void addInformation(ItemStack stack, World world, List<ITextComponent> list, ITooltipFlag flag)
+	{
+		super.addInformation(stack, world, list, flag);
+		list.add(new StringTextComponent(TextFormatting.GRAY + "A Large Sword that requires two hands to wield"));
+	}
+
+	public void inventoryTick(ItemStack stack, World world, Entity entity, int itemSlot, boolean isSelected)
+	{		
+		if(entity instanceof PlayerEntity)
+		{
+			PlayerEntity player = (PlayerEntity)entity;
+			ItemStack equipped = player.getHeldItemMainhand();
+			{
+				if(stack == equipped)
+					{
+						if(player.hasItemInSlot(EquipmentSlotType.OFFHAND))
+						{
+							player.entityDropItem(player.getHeldItemOffhand());
+							player.setItemStackToSlot(EquipmentSlotType.OFFHAND, ItemStack.EMPTY);
+						}
+					}
+			}
+		}
+	}
+}
