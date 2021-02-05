cbd14d3105e83c122f465bc14ee6ffc875ef139f
superworldsun <superworldsun@gmail.com>
2021-02-05 16:18:19 -0500

    Update EntityInit

--- src/main/java/superworldsun/superslegend/init/EntityInit.java
+++ src/main/java/superworldsun/superslegend/init/EntityInit.java
@@ -5,6 +5,8 @@ import net.minecraft.entity.EntityClassification;
 import net.minecraft.entity.EntitySpawnPlacementRegistry;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.projectile.SnowballEntity;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.gen.Heightmap;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.RegistryObject;
@@ -15,6 +17,7 @@ import org.lwjgl.system.CallbackI;
 import superworldsun.superslegend.SupersLegend;
 import superworldsun.superslegend.entities.mobs.fairy.FairyEntity;
 import superworldsun.superslegend.entities.mobs.poe.PoeEntity;
+import superworldsun.superslegend.entities.projectiles.items.EntityBomb;
 
 import java.util.Random;
 
@@ -23,9 +26,15 @@ public class EntityInit {
 
 	public static final DeferredRegister<EntityType<?>> ENTITY_TYPES = DeferredRegister.create(ForgeRegistries.ENTITIES, SupersLegend.modid);
 
-	public static final RegistryObject<EntityType<FairyEntity>> FAIRYENTITY = ENTITY_TYPES.register("fairyentity", () -> EntityType.Builder.create(FairyEntity::new, EntityClassification.CREATURE).size(.5f, .5f).setShouldReceiveVelocityUpdates(false).build("fairyentity"));
-	public static final RegistryObject<EntityType<PoeEntity>> POEENTITY = ENTITY_TYPES.register("poeentity", () -> EntityType.Builder.create(PoeEntity::new, EntityClassification.MONSTER).size(.5f, .5f).setShouldReceiveVelocityUpdates(false).build("poeentity"));
+	//public static final RegistryObject<EntityType<FairyEntity>> FAIRYENTITY = ENTITY_TYPES.register("fairyentity", () -> EntityType.Builder.create(FairyEntity::new, EntityClassification.CREATURE).size(.5f, .5f).setShouldReceiveVelocityUpdates(false).build("fairyentity"));
+	//public static final RegistryObject<EntityType<PoeEntity>> POEENTITY = ENTITY_TYPES.register("poeentity", () -> EntityType.Builder.create(PoeEntity::new, EntityClassification.MONSTER).size(.5f, .5f).setShouldReceiveVelocityUpdates(false).build("poeentity"));
 
+	public static final RegistryObject<EntityType<FairyEntity>> FAIRYENTITY = ENTITY_TYPES.register("fairyentity", () -> EntityType.Builder.<FairyEntity>create(FairyEntity::new, EntityClassification.CREATURE).size(0.9f, 1.3f).build(new ResourceLocation(SupersLegend.modid, "fairyentity").toString()));
+	public static final RegistryObject<EntityType<PoeEntity>> POEENTITY = ENTITY_TYPES.register("poeentity", () -> EntityType.Builder.<PoeEntity>create(PoeEntity::new, EntityClassification.MONSTER).size(.5f, .5f).build(new ResourceLocation(SupersLegend.modid, "poeentity").toString()));
+
+	//public static final RegistryObject<EntityType<EntityBomb>> BOMBENTITY = ENTITY_TYPES.register("bombentity", () -> EntityType.Builder.create(BombEntity, EntityClassification.MISC).size(.5f, .5f).setShouldReceiveVelocityUpdates(false).build("bombentity"));
+	//private static Object BombEntity;
+	//public static final RegistryObject<EntityType<EntityBomb>> BOMBENTITY = ENTITY_TYPES.register("bomb", () -> EntityType.Builder.create(EntityBomb::new, EntityClassification.MISC).size(0.25F, 0.25F).setShouldReceiveVelocityUpdates(false).build("bombentity"));
 
 
 	public static boolean rollSpawn(int rolls, Random random, SpawnReason reason){
