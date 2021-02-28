0d1ae7584fd17e308d1ba9e6ed809d46a35598f8
superworldsun <superworldsun@gmail.com>
2021-02-28 03:37:19 -0500

    Add Boomerang throw sound to item

--- src/main/java/superworldsun/superslegend/items/BoomerangItem.java
+++ src/main/java/superworldsun/superslegend/items/BoomerangItem.java
@@ -6,10 +6,13 @@ import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import superworldsun.superslegend.config.SupersLegendConfig;
 import superworldsun.superslegend.entities.projectiles.items.boomerang.RegularBoomerang;
 import superworldsun.superslegend.entities.projectiles.items.boomerang.BoomerangEntity;
+import superworldsun.superslegend.init.SoundInit;
 
 public class BoomerangItem extends Item {
 
@@ -26,6 +29,9 @@ public class BoomerangItem extends Item {
     public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn) {
         if (playerIn.getHeldItem(handIn).getDamage() == 0) {
             BoomerangEntity boom = new RegularBoomerang(worldIn, playerIn, playerIn.getHeldItem(handIn), handIn);
+
+            BlockPos currentPos = playerIn.getPosition();
+            worldIn.playSound(null, currentPos.getX(), currentPos.getY(), currentPos.getZ(), SoundInit.BOOMERANG_THROW, SoundCategory.PLAYERS, 0.6f, 1.0f);
 
             worldIn.addEntity(boom);
             playerIn.setHeldItem(handIn, ItemStack.EMPTY);
