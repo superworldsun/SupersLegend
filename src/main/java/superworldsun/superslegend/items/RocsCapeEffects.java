e9569a5387a308a7059bfe58a84ffbebaf823783
superworldsun <superworldsun@gmail.com>
2021-02-28 03:43:32 -0500

    Change RocsCape to 3D model

--- src/main/java/superworldsun/superslegend/items/RocsCapeEffects.java
+++ src/main/java/superworldsun/superslegend/items/RocsCapeEffects.java
@@ -2,7 +2,9 @@ package superworldsun.superslegend.items;
 
 import java.util.List;
 
+import net.minecraft.client.renderer.entity.model.BipedModel;
 import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.*;
@@ -12,9 +14,14 @@ import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import superworldsun.superslegend.SupersLegend;
 import superworldsun.superslegend.lists.ArmourMaterialList;
 import superworldsun.superslegend.lists.ItemList;
+import superworldsun.superslegend.models.masks.ModelBunnyhoodMask;
+import superworldsun.superslegend.models.masks.ModelRocsCape;
+
 public class RocsCapeEffects extends NonEnchantArmor {
     public RocsCapeEffects(String name, EquipmentSlotType slot) 
     
@@ -22,12 +29,18 @@ public class RocsCapeEffects extends NonEnchantArmor {
         super(ArmourMaterialList.rocscape, slot, new Properties().group(SupersLegend.supers_legend));
         setRegistryName(SupersLegend.modid, name);
     }
+
+	@SuppressWarnings("unchecked")
+	@OnlyIn(Dist.CLIENT)
+	public <A extends BipedModel<?>> A getArmorModel(LivingEntity entityLiving, ItemStack itemStack, EquipmentSlotType armorSlot, A _default)
+	{
+		return (A) new ModelRocsCape(0);
+	}
+
+
     @Override
     public void onArmorTick(ItemStack stack, World world, PlayerEntity player) 
     {
-    	
-    	
-    	
     	if (!world.isRemote){
     		boolean isChestplateOn = player.getItemStackFromSlot(EquipmentSlotType.CHEST).getItem().equals(ItemList.rocs_cape);
             if(isChestplateOn)
