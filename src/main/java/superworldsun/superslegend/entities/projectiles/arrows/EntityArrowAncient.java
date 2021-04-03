31d99231ea21fe2c562a9a5b1b0de1a21a174a7a
superworldsun <superworldsun@gmail.com>
2021-04-03 03:37:50 -0400

    Update AncientArrow Entity, now deals 30 damage

--- src/main/java/superworldsun/superslegend/entities/projectiles/arrows/EntityArrowAncient.java
+++ src/main/java/superworldsun/superslegend/entities/projectiles/arrows/EntityArrowAncient.java
@@ -1,23 +1,33 @@
 package superworldsun.superslegend.entities.projectiles.arrows;
 
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.projectile.AbstractArrowEntity;
 import net.minecraft.entity.projectile.ArrowEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.network.IPacket;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.network.NetworkHooks;
+import superworldsun.superslegend.init.EntityInit;
 import superworldsun.superslegend.init.SoundInit;
 import superworldsun.superslegend.lists.ItemList;
 
-public class EntityArrowAncient extends ArrowEntity
+public class EntityArrowAncient extends AbstractArrowEntity
 {
 
-    public EntityArrowAncient(World worldIn, double x, double y, double z){
-        super(worldIn, x, y, z);
+    public EntityArrowAncient(EntityType<? extends EntityArrowAncient> type, World world) {
+        super(type, world);
     }
 
-    public EntityArrowAncient(World worldIn, LivingEntity shooter){
-        super(worldIn, shooter);
+    public EntityArrowAncient(World worldIn, LivingEntity shooter) {
+        super(EntityInit.ANCIENT_ARROW.get(), shooter, worldIn);
+        this.setDamage(this.getDamage() + 30.0F);
+    }
+
+    public EntityArrowAncient(World worldIn, double x, double y, double z) {
+        super(EntityInit.ANCIENT_ARROW.get(), x, y, z, worldIn);
     }
 
     @Override
@@ -26,26 +36,21 @@ public class EntityArrowAncient extends ArrowEntity
     }
 
     @Override
+    public IPacket<?> createSpawnPacket() {
+        return NetworkHooks.getEntitySpawningPacket(this);
+    }
+
+    @Override
     protected void arrowHit(LivingEntity entity) {
-
-        int i = this.getColor();
-
-        @SuppressWarnings("unused")
-		double d0 = (double)(i >> 16 & 255) / 255.0D;
-        @SuppressWarnings("unused")
-		double d1 = (double)(i >> 8 & 255) / 255.0D;
-        @SuppressWarnings("unused")
-		double d2 = (double)(i >> 0 & 255) / 255.0D;
-
         super.arrowHit(entity);
         if(entity.isNonBoss()){
         	BlockPos currentPos = entity.getPosition();
             entity.world.playSound(null, currentPos.getX(), currentPos.getY(), currentPos.getZ(), SoundInit.ARROW_HIT_ANCIENT, SoundCategory.PLAYERS, 1f, 1f);
-            entity.setHealth(0);
+            //entity.setHealth(0);
         }else {
         	BlockPos currentPos = entity.getPosition();
             entity.world.playSound(null, currentPos.getX(), currentPos.getY(), currentPos.getZ(), SoundInit.ARROW_HIT_ANCIENT, SoundCategory.PLAYERS, 1f, 1f);
-            entity.setHealth(entity.getHealth()-45);
+            //entity.setHealth(entity.getHealth()-45);
         }
     }
 }
