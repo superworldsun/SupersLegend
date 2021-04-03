865bf47b6459eee4b63388dda288c9e79220ff61
superworldsun <superworldsun@gmail.com>
2021-04-03 03:43:10 -0400

    Update ShockArrow Entity, now plays particles

--- src/main/java/superworldsun/superslegend/entities/projectiles/arrows/EntityArrowShock.java
+++ src/main/java/superworldsun/superslegend/entities/projectiles/arrows/EntityArrowShock.java
@@ -1,46 +1,57 @@
 package superworldsun.superslegend.entities.projectiles.arrows;
 
-import net.minecraft.block.Blocks;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.effect.LightningBoltEntity;
-import net.minecraft.entity.projectile.ArrowEntity;
+import net.minecraft.entity.projectile.AbstractArrowEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.network.IPacket;
+import net.minecraft.particles.ParticleTypes;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
-import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import superworldsun.superslegend.SupersLegend;
-import superworldsun.superslegend.entities.mobs.fairy.FairyEntity;
+import net.minecraftforge.fml.network.NetworkHooks;
+import superworldsun.superslegend.init.EntityInit;
 import superworldsun.superslegend.init.SoundInit;
 import superworldsun.superslegend.lists.ItemList;
 
-import javax.annotation.Nonnull;
-import javax.annotation.ParametersAreNonnullByDefault;
+public class EntityArrowShock extends AbstractArrowEntity {
 
-
-// TODO Remove Fire from lightning strike
-public class EntityArrowShock extends ArrowEntity
-{
-
-    public static final ResourceLocation ARROW = new ResourceLocation(SupersLegend.modid, "textures/entity/arrows/shock_arrow.png");
-
-    @ParametersAreNonnullByDefault
-    public ResourceLocation getEntityTexture(EntityArrowShock entity) {
-        return ARROW;
+    public EntityArrowShock(EntityType<? extends EntityArrowShock> type, World world) {
+        super(type, world);
     }
 
-    public EntityArrowShock(World worldIn, LivingEntity shooter){
-        super(worldIn, shooter);
+    public EntityArrowShock(World worldIn, LivingEntity shooter) {
+        super(EntityInit.SHOCK_ARROW.get(), shooter, worldIn);
+        this.setDamage(this.getDamage() + 2.0F);
     }
 
-    @Nonnull
+    public EntityArrowShock(World worldIn, double x, double y, double z) {
+        super(EntityInit.SHOCK_ARROW.get(), x, y, z, worldIn);
+    }
+
+    @Override
     protected ItemStack getArrowStack() {
         return new ItemStack(ItemList.shock_arrow);
     }
+
+    @Override
+    public IPacket<?> createSpawnPacket() {
+        return NetworkHooks.getEntitySpawningPacket(this);
+    }
+
+
+    @Override
+    public void tick() {
+        super.tick();
+
+        if (!this.inGround) {
+            this.world.addParticle(ParticleTypes.BUBBLE_POP, this.getPosX(), this.getPosY(), this.getPosZ(), 0.0D, 0.0D,
+                    0.0D);
+        }
+    }
+
 
     protected void arrowHit(LivingEntity entity) {
         BlockPos currentPos = entity.getPosition();
