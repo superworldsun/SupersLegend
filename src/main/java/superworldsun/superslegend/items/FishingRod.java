43f26c04a44c8709330b5f1c5d0d7eaca45eda29
superworldsun <superworldsun@gmail.com>
2021-04-18 16:06:31 -0400

    Add Fishing Rod item

---
+++ src/main/java/superworldsun/superslegend/items/FishingRod.java
@@ -0,0 +1,37 @@
+package superworldsun.superslegend.items;
+
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.EnderChestInventory;
+import net.minecraft.inventory.container.ChestContainer;
+import net.minecraft.inventory.container.SimpleNamedContainerProvider;
+import net.minecraft.item.FishingRodItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.*;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.World;
+
+import java.util.List;
+
+public class FishingRod extends FishingRodItem
+{
+	public FishingRod(Properties builder) {
+		super(builder);
+	}
+
+	public boolean isBookEnchantable(ItemStack stack, ItemStack book) {
+		return false;
+	}
+
+	@Override
+	public void addInformation(ItemStack stack, World world, List<ITextComponent> list, ITooltipFlag flag)
+	{
+		super.addInformation(stack, world, list, flag);				
+		list.add(new StringTextComponent(TextFormatting.ITALIC + "A classic Hylian fishing rod"));
+	}
+}
