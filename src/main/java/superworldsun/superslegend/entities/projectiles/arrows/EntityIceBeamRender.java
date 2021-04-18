0816801206c4a9d6fdbd4a441cac4f487946a019
superworldsun <superworldsun@gmail.com>
2021-04-18 17:42:30 -0400

    Add IceBeam Render

---
+++ src/main/java/superworldsun/superslegend/entities/projectiles/arrows/EntityIceBeamRender.java
@@ -0,0 +1,26 @@
+package superworldsun.superslegend.entities.projectiles.arrows;
+
+import net.minecraft.client.renderer.entity.ArrowRenderer;
+import net.minecraft.client.renderer.entity.EntityRenderer;
+import net.minecraft.client.renderer.entity.EntityRendererManager;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import superworldsun.superslegend.SupersLegend;
+
+//@OnlyIn(Dist.CLIENT)
+//public class EntityIceBeamRender extends EntityRenderer<EntityIceBeam> {
+
+    /*protected static final ResourceLocation TEXTURE = new ResourceLocation(SupersLegend.modid, "textures/entity/projectiles/arrows/ice_beam.png");
+
+    public EntityIceBeamRender(EntityRendererManager renderManagerIn) {
+        super(renderManagerIn);
+    }
+
+    @Override
+    public ResourceLocation getEntityTexture(EntityIceBeam entity) {
+        return TEXTURE;
+    }*/
+
+
+//}
