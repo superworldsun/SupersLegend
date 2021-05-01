a6dbf4d8822e279bafab7c590f785a2e60e763ba
superworldsun <superworldsun@gmail.com>
2021-05-01 02:37:34 -0400

    Updated Item List

--- src/main/java/superworldsun/superslegend/lists/BlockList.java
+++ src/main/java/superworldsun/superslegend/lists/BlockList.java
@@ -10,6 +10,8 @@ public class BlockList
 	public static Block silver_rupee_block;
 	public static Block gold_rupee_block;
 	public static Block spikes_block;
+	public static Block pedestal;
+	public static Block fan_block;
 	public static Block gossip_stone_block;
 	public static Block bush_block;
 	public static Block chain_link_fence_block;
@@ -20,14 +22,24 @@ public class BlockList
 	public static Block jar_block;
 	public static Block grate_block;
 	public static Block grass_patch_block;
+	public static Block silver_ore;
 	public static Block master_ore_block;
+	public static Block silver_ore_block;
 	public static Block shadow_block;
 	public static Block false_shadow_block;
 	public static Block hidden_shadow_block;
 	public static Block tombstone_block;
 	public static Block stone_path_block;
 	public static Block stone_tile_block;
-	public static Block bomb_block;
+	public static Block dins_flame;
+	public static Block farores_flame;
+	public static Block nayrus_flame;
+	public static Block lantern_light_block;
+
+	public static Block deku_sapling;
+	public static Block deku_wood;
+	public static Block deku_leaves;
+	public static Block deku_log;
 	
 	public static Block poison;
 }
