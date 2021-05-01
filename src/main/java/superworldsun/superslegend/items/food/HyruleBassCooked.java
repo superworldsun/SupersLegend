3b6168df145f8e659cbe6781081a8bd8e9944eb1
superworldsun <superworldsun@gmail.com>
2021-05-01 00:54:02 -0400

    Add Cooked Hyrule Bass Item

---
+++ src/main/java/superworldsun/superslegend/items/food/HyruleBassCooked.java
@@ -0,0 +1,25 @@
+package superworldsun.superslegend.items.food;
+
+import net.minecraft.item.Food;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.UseAction;
+
+public class HyruleBassCooked extends Item {
+
+
+    public HyruleBassCooked(Properties properties) {
+        super(properties.food(new Food.Builder().saturation(0.8f).hunger(7).meat().build()));
+    }
+
+    @Override
+    public UseAction getUseAction(ItemStack stack) {
+        return UseAction.EAT;
+    }
+
+    @Override
+    public int getUseDuration(ItemStack p_77626_1_) {
+        return 32;
+    }
+
+}
