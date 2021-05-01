734d0032377767c809dc367f9f3f80388c46b4f7
superworldsun <superworldsun@gmail.com>
2021-05-01 02:48:59 -0400

    Add DinsFire Entity (wip)

---
+++ src/main/java/superworldsun/superslegend/entities/projectiles/items/dinsfire/EntityDinsFireRender.java
@@ -0,0 +1,26 @@
+package superworldsun.superslegend.entities.projectiles.items.dinsfire;
+
+import net.minecraft.client.renderer.entity.ArrowRenderer;
+import net.minecraft.client.renderer.entity.EntityRenderer;
+import net.minecraft.client.renderer.entity.EntityRendererManager;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import superworldsun.superslegend.SupersLegend;
+import superworldsun.superslegend.entities.projectiles.beam.EntityFireBeam;
+
+@OnlyIn(Dist.CLIENT)
+public class EntityDinsFireRender extends EntityRenderer<EntityDinsFire> {
+
+    protected static final ResourceLocation TEXTURE = new ResourceLocation(SupersLegend.modid, "");
+
+    public EntityDinsFireRender(EntityRendererManager renderManagerIn) {
+        super(renderManagerIn);
+    }
+
+    @Override
+    public ResourceLocation getEntityTexture(EntityDinsFire entity) {
+        return TEXTURE;
+    }
+
+}
