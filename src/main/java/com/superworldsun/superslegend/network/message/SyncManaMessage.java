389358be7330061d8a30fb466e7a0c1da2f19166
superworldsun <superworldsun@gmail.com>
2021-07-19 23:10:08 -0400

    Add ManaSyncMessage

---
+++ src/main/java/com/superworldsun/superslegend/network/message/SyncManaMessage.java
@@ -0,0 +1,49 @@
+package com.superworldsun.superslegend.network.message;
+
+import java.util.function.Supplier;
+
+import com.superworldsun.superslegend.mana.ManaProvider;
+import com.superworldsun.superslegend.network.ClientPacketHandlerClass;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.network.PacketBuffer;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.network.NetworkEvent;
+
+public class SyncManaMessage
+{
+	public CompoundNBT nbt;
+	
+	public SyncManaMessage(PlayerEntity player)
+	{
+		nbt = (CompoundNBT) ManaProvider.MANA_CAPABILITY.writeNBT(ManaProvider.get(player), null);
+	}
+	
+	private SyncManaMessage()
+	{
+	}
+	
+	public static SyncManaMessage decode(PacketBuffer buf)
+	{
+		SyncManaMessage result = new SyncManaMessage();
+		result.nbt = buf.readAnySizeNbt();
+		return result;
+	}
+	
+	public void encode(PacketBuffer buf)
+	{
+		buf.writeNbt(nbt);
+	}
+	
+	public static class Handler
+	{
+		public static void onMessageReceived(final SyncManaMessage message, Supplier<NetworkEvent.Context> ctxSupplier)
+		{
+			NetworkEvent.Context ctx = ctxSupplier.get();
+			ctx.setPacketHandled(true);
+			ctx.enqueueWork(() -> DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> ClientPacketHandlerClass.handlePacket(message, ctx)));
+		}
+	}
+}
