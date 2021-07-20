de9de8d3572ffe8a0a53bae565c1e084a2119300
Daripher <saelver.serbek@gmail.com>
2021-07-20 17:29:42 +0500

    Removed mana cost from hover boots

--- src/main/java/com/superworldsun/superslegend/items/armors/HoverBoots.java
+++ src/main/java/com/superworldsun/superslegend/items/armors/HoverBoots.java
@@ -2,7 +2,6 @@ package com.superworldsun.superslegend.items.armors;
 
 import com.superworldsun.superslegend.SupersLegendMain;
 import com.superworldsun.superslegend.items.custom.NonEnchantArmor;
-import com.superworldsun.superslegend.mana.ManaProvider;
 import com.superworldsun.superslegend.registries.ArmourInit;
 import com.superworldsun.superslegend.registries.ItemInit;
 import com.superworldsun.superslegend.util.IHoveringEntity;
@@ -45,19 +44,11 @@ public class HoverBoots extends NonEnchantArmor
 		}
 		
 		IHoveringEntity eplayer = (IHoveringEntity) event.player;
-		float manaCost = 0.01F;
 		boolean hasHoverBoots = event.player.getItemBySlot(EquipmentSlotType.FEET).getItem() == ItemInit.HOVER_BOOTS.get();
-		// We can use it in creative even without mana
-		boolean hasMana = ManaProvider.get(event.player).getMana() >= manaCost || event.player.abilities.instabuild;
 		
 		// 40 ticks are 2 seconds
-		if (hasHoverBoots && hasMana && eplayer.increaseHoverTime() < 40)
+		if (hasHoverBoots && eplayer.increaseHoverTime() < 40)
 		{
-			if (!event.player.abilities.instabuild)
-			{
-				ManaProvider.get(event.player).spendMana(manaCost);
-			}
-			
 			double motionY = event.player.getDeltaMovement().y;
 			
 			if (motionY < 0 && event.player.getY() <= eplayer.getHoverHeight() && !event.player.isOnGround())
