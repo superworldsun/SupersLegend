b8d03d55799e97db9a0e7595b46bcd2f9e86b958
superworldsun <superworldsun@gmail.com>
2021-07-22 02:50:08 -0400

    Add BiggornSword from port

---
+++ src/main/java/com/superworldsun/superslegend/items/weapons/BiggornsSword.java
@@ -0,0 +1,57 @@
+package com.superworldsun.superslegend.items.weapons;
+
+import com.superworldsun.superslegend.items.custom.ItemCustomSword;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.IItemTier;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ShieldItem;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvents;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.World;
+
+import java.util.List;
+
+import net.minecraft.item.Item.Properties;
+
+public class BiggornsSword extends ItemCustomSword
+{
+	public BiggornsSword(IItemTier tier, int attackDamageIn, float attackSpeedIn, Properties builder)
+	{
+		super(tier, attackDamageIn, attackSpeedIn, builder);
+	}
+
+	@Override
+	public void appendHoverText(ItemStack stack, World world, List<ITextComponent> list, ITooltipFlag flag)
+	{
+		super.appendHoverText(stack, world, list, flag);
+		list.add(new StringTextComponent(TextFormatting.GRAY + "A Large Sword that requires two hands to wield"));
+	}
+
+	public void inventoryTick(ItemStack stack, World world, Entity entity, int itemSlot, boolean isSelected)
+	{		
+		if(entity instanceof PlayerEntity)
+		{
+			PlayerEntity player = (PlayerEntity)entity;
+			ItemStack equipped = player.getMainHandItem();
+			{
+				if(stack == equipped)
+					{
+						if(player.hasItemInSlot(EquipmentSlotType.OFFHAND))
+						{
+							player.spawnAtLocation(player.getOffhandItem());
+							player.setItemSlot(EquipmentSlotType.OFFHAND, ItemStack.EMPTY);
+						}
+					}
+			}
+		}
+	}
+}
