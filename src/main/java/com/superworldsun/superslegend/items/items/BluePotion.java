146acc1ac96299f362e4714fb6bb6bbaa86ead88
superworldsun <superworldsun@gmail.com>
2021-07-22 02:42:53 -0400

    Update Blue Potion to now restore magic

--- src/main/java/com/superworldsun/superslegend/items/items/BluePotion.java
+++ src/main/java/com/superworldsun/superslegend/items/items/BluePotion.java
@@ -1,12 +1,20 @@
 package com.superworldsun.superslegend.items.items;
 
+import com.superworldsun.superslegend.mana.IMana;
+import com.superworldsun.superslegend.mana.ManaProvider;
+import net.minecraft.advancements.CriteriaTriggers;
 import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.item.UseAction;
 import net.minecraft.potion.Effect;
 import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
+import net.minecraft.stats.Stats;
 import net.minecraft.util.*;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.ITextComponent;
@@ -28,7 +36,72 @@ public class BluePotion extends Item
 		super(properties);
 	}
 
-	@Nonnull
+	@Override
+	public ItemStack finishUsingItem(ItemStack stack, World world, LivingEntity entity)
+	{
+		PlayerEntity player = entity instanceof PlayerEntity ? (PlayerEntity) entity : null;
+
+		if (player instanceof ServerPlayerEntity)
+		{
+			CriteriaTriggers.CONSUME_ITEM.trigger((ServerPlayerEntity) player, stack);
+		}
+
+		if (player != null)
+		{
+			IMana mana = ManaProvider.get(player);
+			// Restore to maximum
+			mana.restoreMana(mana.getMaxMana());
+			player.addEffect(new EffectInstance(Objects.requireNonNull(Effects.REGENERATION), 60, 4, false, false));
+			player.awardStat(Stats.ITEM_USED.get(this));
+
+			if (!player.abilities.instabuild)
+			{
+				stack.shrink(1);
+			}
+		}
+
+		if (player == null || !player.abilities.instabuild)
+		{
+			if (stack.isEmpty())
+			{
+				return new ItemStack(Items.GLASS_BOTTLE);
+			}
+
+			if (player != null)
+			{
+				player.inventory.add(new ItemStack(Items.GLASS_BOTTLE));
+			}
+		}
+
+		return stack;
+	}
+
+	@Override
+	public int getUseDuration(ItemStack stack)
+	{
+		return 32;
+	}
+
+	@Override
+	public UseAction getUseAnimation(ItemStack stack)
+	{
+		return UseAction.DRINK;
+	}
+
+	@Override
+	public ActionResult<ItemStack> use(World world, PlayerEntity player, Hand hand)
+	{
+		ItemStack stack = player.getItemInHand(hand);
+
+		if (ManaProvider.get(player).getMana() == ManaProvider.get(player).getMaxMana() && !player.isHurt())
+		{
+			return ActionResult.fail(stack);
+		}
+
+		return DrinkHelper.useDrink(world, player, hand);
+	}
+
+	/*@Nonnull
 	public ActionResult<ItemStack> use(World world, PlayerEntity player,@Nonnull Hand hand)
 	 {
 		ItemStack stack = player.getItemInHand(hand);
@@ -81,7 +154,7 @@ public class BluePotion extends Item
 		 }
 	 
 		 return new ActionResult<>(ActionResultType.PASS, player.getItemInHand(hand)); 
-	 }
+	 }*/
 
 	public void appendHoverText(@Nonnull ItemStack stack, World world,@Nonnull List<ITextComponent> list,@Nonnull ITooltipFlag flag)
 	{
