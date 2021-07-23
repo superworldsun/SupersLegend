2722d255b83421115bd37bbd859ea33fd13c6117
Daripher <saelver.serbek@gmail.com>
2021-07-23 20:59:22 +0500

    Made magic fire arrow melt ice

--- src/main/java/com/superworldsun/superslegend/entities/projectiles/arrows/MagicFireArrowEntity.java
+++ src/main/java/com/superworldsun/superslegend/entities/projectiles/arrows/MagicFireArrowEntity.java
@@ -1,13 +1,22 @@
 package com.superworldsun.superslegend.entities.projectiles.arrows;
 
+import java.util.List;
+
 import com.superworldsun.superslegend.registries.EntityTypeInit;
 import com.superworldsun.superslegend.registries.ItemInit;
+import com.superworldsun.superslegend.registries.SoundInit;
+import com.superworldsun.superslegend.util.BuildingHelper;
 
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.material.Material;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.network.IPacket;
 import net.minecraft.util.DamageSource;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.util.math.EntityRayTraceResult;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.network.NetworkHooks;
@@ -57,5 +66,23 @@ public class MagicFireArrowEntity extends FireArrowEntity
 		// an arrow. To deal damage 2 times in a row, we have to reset it.
 		rayTraceResult.getEntity().invulnerableTime = 0;
 		rayTraceResult.getEntity().hurt(DamageSource.MAGIC, 5.0F);
+	}
+	
+	@Override
+	protected void onHitBlock(BlockRayTraceResult rayTraceResult)
+	{
+		BlockState blockHit = level.getBlockState(rayTraceResult.getBlockPos());
+		
+		if (blockHit.getMaterial() == Material.WATER)
+		{
+			List<BlockPos> platformShape = BuildingHelper.createRoundPlatformShape(rayTraceResult.getBlockPos(), 3);
+			// We want to replace only ice
+			platformShape.removeIf(pos -> level.getBlockState(pos).getMaterial() != Material.ICE && level.getBlockState(pos).getMaterial() != Material.ICE_SOLID);
+			platformShape.forEach(pos -> level.setBlockAndUpdate(pos, Blocks.AIR.defaultBlockState()));
+			remove();
+		}
+		
+		playSound(SoundInit.ARROW_HIT_FIRE.get(), 1f, 1f);
+		super.onHitBlock(rayTraceResult);
 	}
 }
