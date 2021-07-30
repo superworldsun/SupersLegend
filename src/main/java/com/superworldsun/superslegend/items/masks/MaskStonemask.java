f212e09044fb9668550733c47f30837df3d2440b
superworldsun <superworldsun@gmail.com>
2021-07-30 02:47:22 -0400

    Add StoneMask from port

---
+++ src/main/java/com/superworldsun/superslegend/items/masks/MaskStonemask.java
@@ -0,0 +1,52 @@
+package com.superworldsun.superslegend.items.masks;
+
+import com.superworldsun.superslegend.items.custom.NonEnchantArmor;
+import com.superworldsun.superslegend.registries.ArmourInit;
+import com.superworldsun.superslegend.registries.ItemInit;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.monster.EndermanEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.Effect;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.world.World;
+
+public class MaskStonemask extends NonEnchantArmor
+{
+	public MaskStonemask(Properties properties)
+	{
+		super(ArmourInit.stonemask, EquipmentSlotType.HEAD, properties);
+	}
+
+	@Override
+	public boolean isEnderMask(ItemStack stack, PlayerEntity player, EndermanEntity endermanEntity) {
+		return true;
+	}
+
+	@Override
+	public boolean makesPiglinsNeutral(ItemStack stack, LivingEntity wearer) {
+		return true;
+	}
+
+	@Override
+	public void onArmorTick(ItemStack stack, World world, PlayerEntity player)
+	{
+		if (!world.isClientSide){
+			boolean isHelmeton = player.getItemBySlot(EquipmentSlotType.HEAD).getItem().equals(ItemInit.MASK_STONEMASK);
+			if(isHelmeton) player.addEffect(new EffectInstance(Effects.INVISIBILITY, 10, 0, false, false, false));
+		}
+	}
+
+	public void appendHoverText(ItemStack stack, World world, java.util.List<ITextComponent> list, ITooltipFlag flag)
+	{
+		super.appendHoverText(stack, world, list, flag);
+		list.add(new StringTextComponent(TextFormatting.GRAY + "You wont be noticed, more than usual"));
+		list.add(new StringTextComponent(TextFormatting.DARK_GRAY + "Grants invisibility"));
+	}
+}
