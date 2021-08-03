ac357692187a7262349987ab52aa2373a6f83299
Daripher <saelver.serbek@gmail.com>
2021-08-03 14:36:03 +0500

    Update GossipStoneBlock.java

--- src/main/java/com/superworldsun/superslegend/blocks/GossipStoneBlock.java
+++ src/main/java/com/superworldsun/superslegend/blocks/GossipStoneBlock.java
@@ -1,25 +1,17 @@
 package com.superworldsun.superslegend.blocks;
 
-
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.IBlockReader;
 
-import net.minecraft.block.AbstractBlock.Properties;
-
 public class GossipStoneBlock extends AbstractGossipStoneBlock
 {
-
-    public GossipStoneBlock(Properties builder)
-    {
-        super(builder);
-    }
-
-    public TileEntity createNewTileEntity(IBlockReader worldIn) {
-        
-        //Eventually where you would reference your tile arrows class (unless you dont need one)
-        return null;
-        
-    }
-
-}
+	public GossipStoneBlock(Properties builder)
+	{
+		super(builder);
+	}
 	
+	public TileEntity createNewTileEntity(IBlockReader worldIn)
+	{
+		return null;
+	}
+}
