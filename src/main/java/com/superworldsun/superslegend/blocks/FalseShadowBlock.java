4ec233bdfed17f0b1ace602cf5c9bae05116d5ef
Daripher <saelver.serbek@gmail.com>
2021-08-13 18:55:42 +0500

    Created a functional shadow blocks

--- src/main/java/com/superworldsun/superslegend/blocks/FalseShadowBlock.java
+++ src/main/java/com/superworldsun/superslegend/blocks/FalseShadowBlock.java
@@ -1,31 +1,68 @@
 package com.superworldsun.superslegend.blocks;
 
+import com.superworldsun.superslegend.SupersLegendMain;
+import com.superworldsun.superslegend.blocks.tile.FalseShadowTileEntity;
+import com.superworldsun.superslegend.registries.ItemInit;
+import com.superworldsun.superslegend.registries.PropertiesInit;
+
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockRenderType;
 import net.minecraft.block.BlockState;
+import net.minecraft.client.Minecraft;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.client.event.DrawHighlightEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 
+@EventBusSubscriber(bus = Bus.FORGE, modid = SupersLegendMain.MOD_ID, value = Dist.CLIENT)
 public class FalseShadowBlock extends Block
-
 {
-	protected static final VoxelShape SHAPE = Block.box(0.0D, 0.0D, 0.0D, 0.0D, 0.0D, 0.0D);
-
-	public FalseShadowBlock(Properties properties) {
-		super(properties);
+	public FalseShadowBlock()
+	{
+		super(PropertiesInit.WRECKAGE);
 	}
-
-	public VoxelShape getCollisionShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
-		return SHAPE;
+	
+	@Override
+	public BlockRenderType getRenderShape(BlockState state)
+	{
+		return BlockRenderType.ENTITYBLOCK_ANIMATED;
 	}
-
-
-	public BlockState getPlant(IBlockReader world, BlockPos pos) {
-		return defaultBlockState();
+	
+	@Override
+	public boolean hasTileEntity(BlockState state)
+	{
+		return true;
 	}
-
-	   /*public BlockRenderLayer getRenderLayer() {
-		    return BlockRenderLayer.CUTOUT;
-	   }*/
+	
+	@Override
+	public TileEntity createTileEntity(BlockState state, IBlockReader world)
+	{
+		return new FalseShadowTileEntity();
+	}
+	
+	@Override
+	public VoxelShape getOcclusionShape(BlockState state, IBlockReader world, BlockPos pos)
+	{
+		return VoxelShapes.empty();
+	}
+	
+	@OnlyIn(value = Dist.CLIENT)
+	@SubscribeEvent
+	public static void onDrawBlockHighlight(DrawHighlightEvent.HighlightBlock event)
+	{
+		Minecraft client = Minecraft.getInstance();
+		
+		// Do not render if player is using lens
+		if (client.player.isUsingItem() && client.player.getUseItem().getItem() == ItemInit.LENS_OF_TRUTH.get())
+		{
+			event.setCanceled(true);
+		}
+	}
 }
