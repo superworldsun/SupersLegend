0553921892d0681c4fee77363f7ce48e4bef0f30
superworldsun <superworldsun@gmail.com>
2021-08-18 23:12:14 -0400

    Add Wallets

--- src/main/java/com/superworldsun/superslegend/client/screen/BagScreen.java
+++ src/main/java/com/superworldsun/superslegend/client/screen/BagScreen.java
@@ -2,21 +2,23 @@ package com.superworldsun.superslegend.client.screen;
 
 import com.superworldsun.superslegend.SupersLegendMain;
 
+import com.superworldsun.superslegend.container.BagContainer;
+import com.superworldsun.superslegend.container.LetterContainer;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.container.Container;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.ITextComponent;
 
-public class BagScreen<T extends Container> extends SimpleContainerScreen<T>
+public class BagScreen extends SimpleContainerScreen<BagContainer>
 {
-	public BagScreen(T container, PlayerInventory playerInventory, ITextComponent title)
+	public BagScreen(BagContainer container, PlayerInventory playerInventory, ITextComponent title)
 	{
 		super(container, playerInventory, title);
 	}
-	
+
 	@Override
 	protected ResourceLocation getBackgroundTexture()
 	{
-		return new ResourceLocation(SupersLegendMain.MOD_ID, "textures/gui/bag.png");
+		return new ResourceLocation(SupersLegendMain.MOD_ID, "textures/gui/letter.png");
 	}
 }
