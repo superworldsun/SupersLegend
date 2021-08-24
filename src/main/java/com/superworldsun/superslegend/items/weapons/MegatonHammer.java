41f614fd4753f422c99c1f019c6281a9cfb62be7
superworldsun <superworldsun@gmail.com>
2021-08-24 00:27:36 -0400

    Add MegatonHammer Class

---
+++ src/main/java/com/superworldsun/superslegend/items/weapons/MegatonHammer.java
@@ -0,0 +1,89 @@
+package com.superworldsun.superslegend.items.weapons;
+
+import com.google.common.collect.ImmutableMultimap;
+import com.google.common.collect.Multimap;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.material.Material;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.enchantment.IVanishable;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.ai.attributes.Attribute;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.entity.ai.attributes.Attributes;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.IItemTier;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.TieredItem;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.world.World;
+
+import java.util.List;
+
+public class MegatonHammer extends TieredItem implements IVanishable {
+	private final float attackDamage;
+	private final Multimap<Attribute, AttributeModifier> defaultModifiers;
+
+	public MegatonHammer(IItemTier p_i48460_1_, int p_i48460_2_, float p_i48460_3_, Properties p_i48460_4_) {
+		super(p_i48460_1_, p_i48460_4_);
+		this.attackDamage = (float)p_i48460_2_ + p_i48460_1_.getAttackDamageBonus();
+		ImmutableMultimap.Builder<Attribute, AttributeModifier> builder = ImmutableMultimap.builder();
+		builder.put(Attributes.ATTACK_DAMAGE, new AttributeModifier(BASE_ATTACK_DAMAGE_UUID, "Weapon modifier", (double)this.attackDamage, AttributeModifier.Operation.ADDITION));
+		builder.put(Attributes.ATTACK_SPEED, new AttributeModifier(BASE_ATTACK_SPEED_UUID, "Weapon modifier", (double)p_i48460_3_, AttributeModifier.Operation.ADDITION));
+		this.defaultModifiers = builder.build();
+	}
+
+	public float getDamage() {
+		return this.attackDamage;
+	}
+
+	public boolean canAttackBlock(BlockState p_195938_1_, World p_195938_2_, BlockPos p_195938_3_, PlayerEntity p_195938_4_) {
+		return !p_195938_4_.isCreative();
+	}
+
+	public float getDestroySpeed(ItemStack p_150893_1_, BlockState p_150893_2_) {
+		if (p_150893_2_.is(Blocks.COBWEB)) {
+			return 15.0F;
+		} else {
+			Material material = p_150893_2_.getMaterial();
+			return material != Material.PLANT && material != Material.REPLACEABLE_PLANT && material != Material.CORAL && !p_150893_2_.is(BlockTags.LEAVES) && material != Material.VEGETABLE ? 1.0F : 1.5F;
+		}
+	}
+
+	public boolean hurtEnemy(ItemStack p_77644_1_, LivingEntity p_77644_2_, LivingEntity p_77644_3_) {
+		p_77644_1_.hurtAndBreak(1, p_77644_3_, (p_220045_0_) -> {
+			p_220045_0_.broadcastBreakEvent(EquipmentSlotType.MAINHAND);
+		});
+		return true;
+	}
+
+	public boolean mineBlock(ItemStack p_179218_1_, World p_179218_2_, BlockState p_179218_3_, BlockPos p_179218_4_, LivingEntity p_179218_5_) {
+		if (p_179218_3_.getDestroySpeed(p_179218_2_, p_179218_4_) != 0.0F) {
+			p_179218_1_.hurtAndBreak(2, p_179218_5_, (p_220044_0_) -> {
+				p_220044_0_.broadcastBreakEvent(EquipmentSlotType.MAINHAND);
+			});
+		}
+
+		return true;
+	}
+
+	public boolean isCorrectToolForDrops(BlockState p_150897_1_) {
+		return p_150897_1_.is(Blocks.COBWEB);
+	}
+
+	public Multimap<Attribute, AttributeModifier> getDefaultAttributeModifiers(EquipmentSlotType p_111205_1_) {
+		return p_111205_1_ == EquipmentSlotType.MAINHAND ? this.defaultModifiers : super.getDefaultAttributeModifiers(p_111205_1_);
+	}
+
+	@Override
+	public void appendHoverText(ItemStack stack, World world, List<ITextComponent> list, ITooltipFlag flag)
+	{
+		super.appendHoverText(stack, world, list, flag);
+		list.add(new StringTextComponent(TextFormatting.WHITE + "A large hammer from the goron tribe"));
+	}
+}
