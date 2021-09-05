54bae08d9ec586ae247698053de7d308314cea49
Daripher <saelver.serbek@gmail.com>
2021-09-05 06:30:03 +0500

    Update IEntityResizer.java
    Added description

--- src/main/java/com/superworldsun/superslegend/interfaces/IEntityResizer.java
+++ src/main/java/com/superworldsun/superslegend/interfaces/IEntityResizer.java
@@ -1,7 +1,14 @@
 package com.superworldsun.superslegend.interfaces;
 
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ArmorItem;
 
+/**
+ * Can be used with instances of {@link ArmorItem} to change player's size
+ * 
+ * @author Daripher
+ *
+ */
 public interface IEntityResizer
 {
 	float getScale(PlayerEntity player);
