d09b41cf1e32d6b53f1efe71693298f4037b1ef8
superworldsun <superworldsun@gmail.com>
2021-11-09 20:49:06 -0500

    Added registry to BiomeInit

--- src/main/java/com/superworldsun/superslegend/registries/BiomeInit.java
+++ src/main/java/com/superworldsun/superslegend/registries/BiomeInit.java
@@ -1,21 +1,39 @@
 package com.superworldsun.superslegend.registries;
 
 import com.superworldsun.superslegend.SupersLegendMain;
-import com.superworldsun.superslegend.util.BiomeBuilder;
-import net.minecraft.item.Item;
+import net.minecraft.util.RegistryKey;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.registry.Registry;
 import net.minecraft.world.biome.Biome;
-import net.minecraft.world.gen.surfacebuilders.ConfiguredSurfaceBuilders;
+import net.minecraft.world.biome.BiomeMaker;
+import net.minecraftforge.common.BiomeManager;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
+import java.util.function.Supplier;
+
 public class BiomeInit {
 
-    public static final DeferredRegister<Item> BIOMES = DeferredRegister.create(ForgeRegistries.ITEMS,
-            SupersLegendMain.MOD_ID);
+    public static final DeferredRegister<Biome> BIOMES = DeferredRegister.create(ForgeRegistries.BIOMES, SupersLegendMain.MOD_ID);
 
-    /*public static final RegistryObject<Biome> LOSTWOODS = BIOMES.register("lostwoods", () -> BiomeBuilder.makeLostWoodsBiome(
-            BiomeBuilder.getSurfaceBuilder(ConfiguredSurfaceBuilders.LOSTWOODS),
-            0.18f, 0.1f, true)
-    );*/
+    static {
+        createBiome("dark_world_forest", BiomeMaker::theVoidBiome);
+    }
+
+    //register the biome and add it to the biome manager
+    public static RegistryKey<Biome> DARK_WORLD_FOREST = registryKey("dark_world_forest");
+
+
+    public static RegistryKey<Biome> registryKey(String name) {
+        return RegistryKey.create(Registry.BIOME_REGISTRY, new ResourceLocation(SupersLegendMain.MOD_ID, name));
+    }
+
+    public static RegistryObject<Biome> createBiome(String name, Supplier<Biome> biome) {
+        return BIOMES.register(name, biome);
+    }
+
+    public static void registerBiomes() {
+        BiomeManager.addBiome(BiomeManager.BiomeType.COOL, new BiomeManager.BiomeEntry(DARK_WORLD_FOREST, 5));
+    }
 }
