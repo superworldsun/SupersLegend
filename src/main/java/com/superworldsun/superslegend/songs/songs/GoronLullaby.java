f68b531d9d2c0e2ef414bfdece477758b717be9f
Daripher <saelver.serbek@gmail.com>
2021-12-05 16:32:54 +0500

    Added functional to goron lullaby song

--- src/main/java/com/superworldsun/superslegend/songs/songs/GoronLullaby.java
+++ src/main/java/com/superworldsun/superslegend/songs/songs/GoronLullaby.java
@@ -2,6 +2,7 @@ package com.superworldsun.superslegend.songs.songs;
 
 import com.superworldsun.superslegend.songs.OcarinaSong;
 
+import net.minecraft.entity.IAngerable;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.world.World;
 
@@ -15,6 +16,13 @@ public class GoronLullaby extends OcarinaSong
 	@Override
 	public void onSongPlayed(PlayerEntity player, World level)
 	{
-		
+		int radius = 5;
+		level.getEntities(player, player.getBoundingBox().inflate(radius)).forEach(entity ->
+		{
+			if (entity instanceof IAngerable && ((IAngerable) entity).isAngry())
+			{
+				((IAngerable) entity).stopBeingAngry();
+			}
+		});
 	}
 }
