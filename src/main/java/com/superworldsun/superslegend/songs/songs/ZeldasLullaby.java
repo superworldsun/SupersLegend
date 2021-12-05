6f5cdb0ed1636a90017a5d5b1b6907c0307ed14d
Daripher <saelver.serbek@gmail.com>
2021-12-05 13:29:02 +0500

    Added functional to zelda's lullaby song

--- src/main/java/com/superworldsun/superslegend/songs/songs/ZeldasLullaby.java
+++ src/main/java/com/superworldsun/superslegend/songs/songs/ZeldasLullaby.java
@@ -1,8 +1,12 @@
 package com.superworldsun.superslegend.songs.songs;
 
+import com.superworldsun.superslegend.blocks.RoyalTileBlock;
 import com.superworldsun.superslegend.songs.OcarinaSong;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 public class ZeldasLullaby extends OcarinaSong
@@ -15,6 +19,18 @@ public class ZeldasLullaby extends OcarinaSong
 	@Override
 	public void onSongPlayed(PlayerEntity player, World level)
 	{
-		
+		int radius = 5;
+		BlockPos start = player.blockPosition().offset(-radius, -radius, -radius);
+		BlockPos end = player.blockPosition().offset(radius, radius, radius);
+		BlockPos.betweenClosed(start, end).forEach(pos ->
+		{
+			BlockState blockState = level.getBlockState(pos);
+			Block block = blockState.getBlock();
+			
+			if (block instanceof RoyalTileBlock)
+			{
+				((RoyalTileBlock) block).activate(level, blockState, pos);
+			}
+		});
 	}
 }
