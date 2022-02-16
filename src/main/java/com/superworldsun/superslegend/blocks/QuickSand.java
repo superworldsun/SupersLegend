89b006033f69be2a712175f3949c8b0dc9c876fc
superworldsun <superworldsun@gmail.com>
2022-02-16 00:58:12 -0500

    Updated QuickSand (Fixed Damage Issue)

--- src/main/java/com/superworldsun/superslegend/blocks/QuickSand.java
+++ src/main/java/com/superworldsun/superslegend/blocks/QuickSand.java
@@ -3,6 +3,7 @@ package com.superworldsun.superslegend.blocks;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.FallingBlock;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.vector.Vector3d;
@@ -14,12 +15,11 @@ public class QuickSand extends FallingBlock {
     }
 
 
-    //TODO Entities dont take damage, when inside the block.
     public void entityInside(BlockState state, World worldIn, BlockPos pos, Entity entityIn) {
         entityIn.makeStuckInBlock(state, new Vector3d(0.75D, (double)0.10F, 0.75D));
-        if(entityIn.isInWall())
+        if(entityIn instanceof LivingEntity && entityIn.isAlive() && entityIn.getEyeY() < (double)(pos.getY() + 1))
         {
-            entityIn.hurt(DamageSource.IN_WALL,4f);
+            entityIn.hurt(DamageSource.IN_WALL,1.0f);
         }
     }
 
