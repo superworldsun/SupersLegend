481e14825b2e92c5dab31423fd4ca9c3a3707f45
Cozary <ayrecio99@gmail.com>
2022-02-27 23:48:56 +0100

    Changed the Collision of Wooden Peg Block. Now you can't jump over them.

--- src/main/java/com/superworldsun/superslegend/blocks/WoodenPegBlock.java
+++ src/main/java/com/superworldsun/superslegend/blocks/WoodenPegBlock.java
@@ -5,24 +5,29 @@ import com.superworldsun.superslegend.registries.ItemInit;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.pathfinding.PathType;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.SoundEvents;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
 
-public class WoodenPegBlock extends Block
-
-{
+public class WoodenPegBlock extends Block {
 
 	protected static final VoxelShape SHAPE = Block.box(4.0D, 0.0D, 4.0D, 12.0D, 13.0D, 12.0D);
+	protected static final VoxelShape HITBOX_SHAPE = Block.box(4.0D, 0.0D, 4.0D, 12.0D, 24.0D, 12.0D);
 
 	public WoodenPegBlock(Properties properties) {
 		super(properties);
 	}
 
+	@Override
+	public VoxelShape getCollisionShape(BlockState p_220071_1_, IBlockReader p_220071_2_, BlockPos p_220071_3_, ISelectionContext p_220071_4_) {
+		return HITBOX_SHAPE;
+	}
 
 	@Override
 	public void attack(BlockState state, World world, BlockPos pos, PlayerEntity playerEntity)
@@ -48,7 +53,12 @@ public class WoodenPegBlock extends Block
 		}
 	}*/
 
+	@Override
 	public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
 		return SHAPE;
+	}
+	@Override
+	public boolean isPathfindable(BlockState p_196266_1_, IBlockReader p_196266_2_, BlockPos p_196266_3_, PathType p_196266_4_) {
+		return false;
 	}
 }
