730385ddd63c3a0214ba35484d92503589222290
superworldsun <superworldsun@gmail.com>
2022-04-25 01:18:57 -0400

    Updated HammeredPegs (updated change, was causing crashes)

--- src/main/java/com/superworldsun/superslegend/registries/BlockInitNotInTab.java
+++ src/main/java/com/superworldsun/superslegend/registries/BlockInitNotInTab.java
@@ -16,9 +16,9 @@ public class BlockInitNotInTab
 	public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, SupersLegendMain.MOD_ID);
 
     //Blocks
-    public static final RegistryObject<Block> HAMMERED_WOODEN_PEG_BLOCK = BLOCKS.register("hammered_wooden_peg_block", () -> new HammeredWoodenPegBlock(PropertiesInit.HAMMERED_WOODEN_PEG));
-    public static final RegistryObject<Block> HAMMERED_RUSTED_PEG_BLOCK = BLOCKS.register("hammered_rusted_peg_block", () -> new HammeredRustedPegBlock(PropertiesInit.HAMMERED_RUSTED_PEG));
-    public static final RegistryObject<Block> HAMMERED_SPIKED_PEG_BLOCK = BLOCKS.register("hammered_spiked_peg_block", () -> new HammeredSpikedPegBlock(PropertiesInit.HAMMERED_SPIKED_PEG));
+    //public static final RegistryObject<Block> HAMMERED_WOODEN_PEG_BLOCK = BLOCKS.register("hammered_wooden_peg_block", () -> new HammeredWoodenPegBlock(PropertiesInit.HAMMERED_WOODEN_PEG));
+    //public static final RegistryObject<Block> HAMMERED_RUSTED_PEG_BLOCK = BLOCKS.register("hammered_rusted_peg_block", () -> new HammeredRustedPegBlock(PropertiesInit.HAMMERED_RUSTED_PEG));
+    //public static final RegistryObject<Block> HAMMERED_SPIKED_PEG_BLOCK = BLOCKS.register("hammered_spiked_peg_block", () -> new HammeredSpikedPegBlock(PropertiesInit.HAMMERED_SPIKED_PEG));
 
     //Liquids
     //public static final RegistryObject<FluidBlock> LIQUID_POISON = BLOCKS.register("liquid_poison", () -> new FluidBlock(FluidInit.POISON_SOURCE));
