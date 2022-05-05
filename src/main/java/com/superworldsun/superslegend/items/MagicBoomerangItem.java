3a4198e11e777d6d4140d432baf949ea8118c226
Daripher <saelver.serbek@gmail.com>
2022-05-05 15:04:05 +0500

    Doubled the speed of magic boomerang

--- src/main/java/com/superworldsun/superslegend/items/MagicBoomerangItem.java
+++ src/main/java/com/superworldsun/superslegend/items/MagicBoomerangItem.java
@@ -12,26 +12,28 @@ import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
-public class MagicBoomerangItem extends Item {
-
-
-    public MagicBoomerangItem(Properties p_i48487_1_) {
-        super(p_i48487_1_);
-    }
-
-    @Override
-    public ActionResult<ItemStack> use(World world, PlayerEntity player, Hand hand) {
-        if (player.getItemInHand(hand).getDamageValue() == 0) {
-            MagicBoomerangEntity boomerangEntity = new MagicBoomerang(world, player, player.getItemInHand(hand), hand);
-
-            BlockPos currentPos = player.blockPosition();
-            world.playSound(null, currentPos.getX(), currentPos.getY(), currentPos.getZ(), SoundInit.BOOMERANG_THROW.get(), SoundCategory.PLAYERS, 0.6f, 1.0f);
-
-            world.addFreshEntity(boomerangEntity);
-            player.setItemInHand(hand, ItemStack.EMPTY);
-        }
-        return ActionResult.success(player.getItemInHand(hand));
-    }
+public class MagicBoomerangItem extends Item
+{	
+	public MagicBoomerangItem(Properties p_i48487_1_)
+	{
+		super(p_i48487_1_);
+	}
+	
+	@Override
+	public ActionResult<ItemStack> use(World world, PlayerEntity player, Hand hand)
+	{
+		if (player.getItemInHand(hand).getDamageValue() == 0)
+		{
+			MagicBoomerangEntity boomerangEntity = new MagicBoomerang(world, player, player.getItemInHand(hand), hand);			
+			BlockPos currentPos = player.blockPosition();
+			world.playSound(null, currentPos.getX(), currentPos.getY(), currentPos.getZ(), SoundInit.BOOMERANG_THROW.get(), SoundCategory.PLAYERS, 0.6f, 1.0f);
+			double speedMultiplier = 2D;
+			boomerangEntity.setDeltaMovement(boomerangEntity.getDeltaMovement().multiply(speedMultiplier, speedMultiplier, speedMultiplier));
+			world.addFreshEntity(boomerangEntity);
+			player.setItemInHand(hand, ItemStack.EMPTY);
+		}
+		return ActionResult.success(player.getItemInHand(hand));
+	}
 
     /*@Override
     public ActionResult<ItemStack> use(World world, PlayerEntity player, Hand hand) {
