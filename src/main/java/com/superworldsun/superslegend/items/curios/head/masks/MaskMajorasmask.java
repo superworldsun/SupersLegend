cd10f3f8ab3089ffdb44fbeaddf5b10e1ac0cf8d
superworldsun <superworldsun@gmail.com>
2022-06-02 17:57:29 -0400

    Add Model for MajorasMask

--- src/main/java/com/superworldsun/superslegend/items/curios/head/masks/MaskMajorasmask.java
+++ src/main/java/com/superworldsun/superslegend/items/curios/head/masks/MaskMajorasmask.java
@@ -1,6 +1,15 @@
 package com.superworldsun.superslegend.items.curios.head.masks;
 
+import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.vertex.IVertexBuilder;
+import com.superworldsun.superslegend.SupersLegendMain;
+import com.superworldsun.superslegend.client.model.armor.AllNightMaskModel;
+import com.superworldsun.superslegend.client.model.armor.MajorasMaskModel;
+import com.superworldsun.superslegend.client.model.armor.MaskOfScentsModel;
 import com.superworldsun.superslegend.registries.ItemInit;
+import net.minecraft.client.renderer.IRenderTypeBuffer;
+import net.minecraft.client.renderer.ItemRenderer;
+import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
@@ -8,32 +17,28 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.potion.Effect;
 import net.minecraft.potion.EffectInstance;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import org.apache.commons.lang3.tuple.ImmutableTriple;
 import top.theillusivec4.curios.api.CuriosApi;
+import top.theillusivec4.curios.api.type.capability.ICurio;
 import top.theillusivec4.curios.api.type.capability.ICurioItem;
 
-public class MaskMajorasmask extends Item implements ICurioItem {
+public class MaskMajorasmask extends Item implements ICurioItem
+{
+    @OnlyIn(Dist.CLIENT)
+    private Object model;
+    // put your texture here
+    private static final ResourceLocation TEXTURE = new ResourceLocation(SupersLegendMain.MOD_ID, "textures/models/armor/majoras_mask.png");
 
     public MaskMajorasmask(Properties properties) {
         super(properties);
     }
-    
-    /*@SuppressWarnings("unchecked")
-    @Override
-    @OnlyIn(Dist.CLIENT)
-    public <A extends BipedModel<?>> A getArmorModel(LivingEntity entityLiving, ItemStack itemStack, EquipmentSlotType armorSlot, A _default) {
-    	ModelMajorasMask model = new ModelMajorasMask();
-
-        model.isChild = _default.isChild;
-        model.isSneak = _default.isSneak;
-        model.isSitting = _default.isSitting;
-
-        return (A) model;
-    }*/
 
     public void appendHoverText(ItemStack stack, World world, java.util.List<ITextComponent> list, ITooltipFlag flag) {
         super.appendHoverText(stack, world, list, flag);
@@ -59,5 +64,27 @@ public class MaskMajorasmask extends Item implements ICurioItem {
 
             }
         }
+    }
+
+    @Override
+    public boolean canRender(String identifier, int index, LivingEntity livingEntity, ItemStack stack)
+    {
+        return true;
+    }
+
+    @Override
+    public void render(String identifier, int index, MatrixStack matrixStack, IRenderTypeBuffer renderTypeBuffer, int light, LivingEntity livingEntity, float limbSwing, float limbSwingAmount,
+                       float partialTicks, float ageInTicks, float netHeadYaw, float headPitch, ItemStack stack)
+    {
+        // put your model here
+        if (!(this.model instanceof MajorasMaskModel))
+        {
+            model = new MajorasMaskModel<>();
+        }
+
+        MajorasMaskModel<?> maskModel = (MajorasMaskModel<?>) this.model;
+        ICurio.RenderHelper.followHeadRotations(livingEntity, maskModel.base);
+        IVertexBuilder vertexBuilder = ItemRenderer.getArmorFoilBuffer(renderTypeBuffer, maskModel.renderType(TEXTURE), false, stack.hasFoil());
+        maskModel.renderToBuffer(matrixStack, vertexBuilder, light, OverlayTexture.NO_OVERLAY, 1.0F, 1.0F, 1.0F, 1.0F);
     }
 }
