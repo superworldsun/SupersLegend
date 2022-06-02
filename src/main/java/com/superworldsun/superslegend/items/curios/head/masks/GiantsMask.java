e72b2eb01b293d2d0a25ae375a20efaee122357d
superworldsun <superworldsun@gmail.com>
2022-06-02 17:46:59 -0400

    Add Model for GiantsMask

--- src/main/java/com/superworldsun/superslegend/items/curios/head/masks/GiantsMask.java
+++ src/main/java/com/superworldsun/superslegend/items/curios/head/masks/GiantsMask.java
@@ -1,18 +1,35 @@
 package com.superworldsun.superslegend.items.curios.head.masks;
 
+import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.vertex.IVertexBuilder;
 import com.superworldsun.superslegend.SupersLegendMain;
+import com.superworldsun.superslegend.client.model.armor.AllNightMaskModel;
+import com.superworldsun.superslegend.client.model.armor.GiantsMaskModel;
 import com.superworldsun.superslegend.interfaces.IEntityResizer;
 import com.superworldsun.superslegend.mana.ManaProvider;
+import net.minecraft.client.renderer.IRenderTypeBuffer;
+import net.minecraft.client.renderer.ItemRenderer;
+import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import top.theillusivec4.curios.api.type.capability.ICurio;
 import top.theillusivec4.curios.api.type.capability.ICurioItem;
 
 @EventBusSubscriber(bus = Bus.FORGE, modid = SupersLegendMain.MOD_ID)
-public class GiantsMask extends Item implements IEntityResizer, ICurioItem {
+public class GiantsMask extends Item implements IEntityResizer, ICurioItem
+{
+    @OnlyIn(Dist.CLIENT)
+    private Object model;
+    // put your texture here
+    private static final ResourceLocation TEXTURE = new ResourceLocation(SupersLegendMain.MOD_ID, "textures/models/armor/giants_mask.png");
+
     public GiantsMask(Properties properties) {
         super(properties);
     }
@@ -31,5 +48,27 @@ public class GiantsMask extends Item implements IEntityResizer, ICurioItem {
         float manaCost = 0.01F;
         boolean hasMana = ManaProvider.get(player).getMana() >= manaCost || player.abilities.instabuild;
         return hasMana ? 4.0F : 1.0F;
+    }
+
+    @Override
+    public boolean canRender(String identifier, int index, LivingEntity livingEntity, ItemStack stack)
+    {
+        return true;
+    }
+
+    @Override
+    public void render(String identifier, int index, MatrixStack matrixStack, IRenderTypeBuffer renderTypeBuffer, int light, LivingEntity livingEntity, float limbSwing, float limbSwingAmount,
+                       float partialTicks, float ageInTicks, float netHeadYaw, float headPitch, ItemStack stack)
+    {
+        // put your model here
+        if (!(this.model instanceof GiantsMaskModel))
+        {
+            model = new GiantsMaskModel<>();
+        }
+
+        GiantsMaskModel<?> maskModel = (GiantsMaskModel<?>) this.model;
+        ICurio.RenderHelper.followHeadRotations(livingEntity, maskModel.base);
+        IVertexBuilder vertexBuilder = ItemRenderer.getArmorFoilBuffer(renderTypeBuffer, maskModel.renderType(TEXTURE), false, stack.hasFoil());
+        maskModel.renderToBuffer(matrixStack, vertexBuilder, light, OverlayTexture.NO_OVERLAY, 1.0F, 1.0F, 1.0F, 1.0F);
     }
 }
