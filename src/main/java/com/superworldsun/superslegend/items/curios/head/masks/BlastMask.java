86dee596dac36aa729f94eff628ce12713b3e0b5
superworldsun <superworldsun@gmail.com>
2022-06-02 17:33:02 -0400

    Add Custom Model for BlastMask

--- src/main/java/com/superworldsun/superslegend/items/curios/head/masks/BlastMask.java
+++ src/main/java/com/superworldsun/superslegend/items/curios/head/masks/BlastMask.java
@@ -1,22 +1,41 @@
 package com.superworldsun.superslegend.items.curios.head.masks;
 
+import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.vertex.IVertexBuilder;
+import com.superworldsun.superslegend.SupersLegendMain;
+import com.superworldsun.superslegend.client.model.armor.AllNightMaskModel;
+import com.superworldsun.superslegend.client.model.armor.BlastMaskModel;
 import com.superworldsun.superslegend.cooldowns.CooldownsProvider;
 import com.superworldsun.superslegend.interfaces.IMaskAbility;
 import com.superworldsun.superslegend.registries.ItemInit;
+import net.minecraft.client.renderer.IRenderTypeBuffer;
+import net.minecraft.client.renderer.ItemRenderer;
+import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.DamageSource;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.Explosion.Mode;
 import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import top.theillusivec4.curios.api.type.capability.ICurio;
 import top.theillusivec4.curios.api.type.capability.ICurioItem;
 
 public class BlastMask extends Item implements IMaskAbility, ICurioItem {
+    @OnlyIn(Dist.CLIENT)
+    private Object model;
+    // put your texture here
+    private static final ResourceLocation TEXTURE = new ResourceLocation(SupersLegendMain.MOD_ID, "textures/models/armor/blast_mask.png");
+
+
     public BlastMask(Properties properties) {
         super(properties);
     }
@@ -39,5 +58,27 @@ public class BlastMask extends Item implements IMaskAbility, ICurioItem {
         // 200 ticks are 10 seconds
         CooldownsProvider.get(player).setCooldown(ItemInit.MASK_BLASTMASK.get(), 200);
         IMaskAbility.super.startUsingAbility(player);
+    }
+
+    @Override
+    public boolean canRender(String identifier, int index, LivingEntity livingEntity, ItemStack stack)
+    {
+        return true;
+    }
+
+    @Override
+    public void render(String identifier, int index, MatrixStack matrixStack, IRenderTypeBuffer renderTypeBuffer, int light, LivingEntity livingEntity, float limbSwing, float limbSwingAmount,
+                       float partialTicks, float ageInTicks, float netHeadYaw, float headPitch, ItemStack stack)
+    {
+        // put your model here
+        if (!(this.model instanceof AllNightMaskModel))
+        {
+            model = new BlastMaskModel<>();
+        }
+
+        BlastMaskModel<?> maskModel = (BlastMaskModel<?>) this.model;
+        ICurio.RenderHelper.followHeadRotations(livingEntity, maskModel.base);
+        IVertexBuilder vertexBuilder = ItemRenderer.getArmorFoilBuffer(renderTypeBuffer, maskModel.renderType(TEXTURE), false, stack.hasFoil());
+        maskModel.renderToBuffer(matrixStack, vertexBuilder, light, OverlayTexture.NO_OVERLAY, 1.0F, 1.0F, 1.0F, 1.0F);
     }
 }
