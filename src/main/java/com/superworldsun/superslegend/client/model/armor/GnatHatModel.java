4c33506c0ee1a067448ee2a1716af5c365d386cc
superworldsun <superworldsun@gmail.com>
2022-06-02 17:44:24 -0400

    Add Model for GnatHat

--- src/main/java/com/superworldsun/superslegend/client/model/armor/GnatHatModel.java
+++ src/main/java/com/superworldsun/superslegend/client/model/armor/GnatHatModel.java
@@ -2,75 +2,71 @@ package com.superworldsun.superslegend.client.model.armor;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
 import com.mojang.blaze3d.vertex.IVertexBuilder;
-
-import net.minecraft.client.renderer.entity.model.BipedModel;
+import net.minecraft.client.renderer.entity.model.EntityModel;
 import net.minecraft.client.renderer.model.ModelRenderer;
 import net.minecraft.entity.LivingEntity;
 
-public class GnatHatModel extends BipedModel<LivingEntity>
+public class GnatHatModel<T extends LivingEntity> extends EntityModel<T>
 {
+	public final ModelRenderer base;
 	private final ModelRenderer cube_r1;
 	private final ModelRenderer cube_r2;
 	private final ModelRenderer cube_r3;
 	private final ModelRenderer cube_r4;
 	private final ModelRenderer cube_r5;
 	private final ModelRenderer cube_r6;
-	
+
 	public GnatHatModel()
 	{
-		super(0F);
-		
 		texWidth = 64;
 		texHeight = 64;
-		
-		head = new ModelRenderer(this);
-		head.setPos(0.0F, 0.0F, 0.0F);
-		head.texOffs(0, 16).addBox(-4.0F, -8.0F, -4.0F, 8.0F, 2.0F, 8.0F, 0.2F, false);
-		head.texOffs(0, 36).addBox(-0.5F, -8.3F, -4.2F, 1.0F, 2.0F, 1.0F, 0.2F, false);
-		
+
+		base = new ModelRenderer(this);
+		base.setPos(0.0F, 24.0F, 0.0F);
+		base.texOffs(0, 16).addBox(-4.0F, -8.0F, -4.0F, 8.0F, 2.0F, 8.0F, 0.2F, false);
+		base.texOffs(0, 36).addBox(-0.5F, -8.3F, -4.2F, 1.0F, 2.0F, 1.0F, 0.2F, false);
+
 		cube_r1 = new ModelRenderer(this);
 		cube_r1.setPos(0.0F, -2.3F, 0.5F);
-		head.addChild(cube_r1);
+		base.addChild(cube_r1);
 		setRotationAngle(cube_r1, -1.5272F, 0.0F, 0.0F);
 		cube_r1.texOffs(21, 33).addBox(-1.5F, -8.1F, -4.9F, 3.0F, 1.0F, 3.0F, 0.0F, false);
-		
+
 		cube_r2 = new ModelRenderer(this);
 		cube_r2.setPos(0.0F, -2.3F, 0.5F);
-		head.addChild(cube_r2);
+		base.addChild(cube_r2);
 		setRotationAngle(cube_r2, -1.2654F, 0.0F, 0.0F);
 		cube_r2.texOffs(24, 19).addBox(-2.0F, -9.4F, -4.6F, 4.0F, 2.0F, 3.0F, 0.0F, false);
-		
+
 		cube_r3 = new ModelRenderer(this);
 		cube_r3.setPos(0.0F, -2.3F, 0.5F);
-		head.addChild(cube_r3);
+		base.addChild(cube_r3);
 		setRotationAngle(cube_r3, -0.7854F, 0.0F, 0.0F);
 		cube_r3.texOffs(24, 26).addBox(-2.5F, -10.6F, -3.3F, 5.0F, 3.0F, 4.0F, 0.0F, false);
-		
+
 		cube_r4 = new ModelRenderer(this);
 		cube_r4.setPos(0.0F, -2.3F, 0.5F);
-		head.addChild(cube_r4);
+		base.addChild(cube_r4);
 		setRotationAngle(cube_r4, -0.3927F, 0.0F, 0.0F);
 		cube_r4.texOffs(0, 26).addBox(-3.0F, -11.4F, -4.7F, 6.0F, 4.0F, 6.0F, 0.0F, false);
-		
+
 		cube_r5 = new ModelRenderer(this);
 		cube_r5.setPos(0.0F, 0.0F, 0.0F);
-		head.addChild(cube_r5);
+		base.addChild(cube_r5);
 		setRotationAngle(cube_r5, -0.2618F, 0.0F, 0.0F);
-		cube_r5.texOffs(24, 0).addBox(-3.001F, -12.5F, -4.3F, 6.0F, 3.0F, 5.0F, 0.0F, false);
-		
+		cube_r5.texOffs(24, 0).addBox(-2.99F, -12.5F, -4.3F, 6.0F, 3.0F, 5.0F, 0.0F, false);
+
 		cube_r6 = new ModelRenderer(this);
 		cube_r6.setPos(0.0F, 0.0F, 0.0F);
-		head.addChild(cube_r6);
+		base.addChild(cube_r6);
 		setRotationAngle(cube_r6, -0.0873F, 0.0F, 0.0F);
 		cube_r6.texOffs(25, 9).addBox(-4.0F, -10.6F, -4.0F, 8.0F, 3.0F, 7.0F, 0.0F, false);
 	}
 	
 	@Override
-	public void renderToBuffer(MatrixStack p_225598_1_, IVertexBuilder p_225598_2_, int p_225598_3_, int p_225598_4_, float p_225598_5_, float p_225598_6_,
-			float p_225598_7_, float p_225598_8_)
+	public void renderToBuffer(MatrixStack p_225598_1_, IVertexBuilder p_225598_2_, int p_225598_3_, int p_225598_4_, float p_225598_5_, float p_225598_6_, float p_225598_7_, float p_225598_8_)
 	{
-		hat.visible = false;
-		super.renderToBuffer(p_225598_1_, p_225598_2_, p_225598_3_, p_225598_4_, p_225598_5_, p_225598_6_, p_225598_7_, p_225598_8_);
+		base.render(p_225598_1_, p_225598_2_, p_225598_3_, p_225598_4_, p_225598_5_, p_225598_6_, p_225598_7_, p_225598_8_);
 	}
 	
 	public void setRotationAngle(ModelRenderer modelRenderer, float x, float y, float z)
@@ -78,5 +74,10 @@ public class GnatHatModel extends BipedModel<LivingEntity>
 		modelRenderer.xRot = x;
 		modelRenderer.yRot = y;
 		modelRenderer.zRot = z;
+	}
+	
+	@Override
+	public void setupAnim(T p_225597_1_, float p_225597_2_, float p_225597_3_, float p_225597_4_, float p_225597_5_, float p_225597_6_)
+	{
 	}
 }
--- src/main/java/com/superworldsun/superslegend/items/curios/head/masks/GnatHat.java
+++ src/main/java/com/superworldsun/superslegend/items/curios/head/masks/GnatHat.java
@@ -1,20 +1,39 @@
 package com.superworldsun.superslegend.items.curios.head.masks;
 
+import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.vertex.IVertexBuilder;
 import com.superworldsun.superslegend.SupersLegendMain;
+import com.superworldsun.superslegend.client.model.armor.AllNightMaskModel;
+import com.superworldsun.superslegend.client.model.armor.GnatHatModel;
 import com.superworldsun.superslegend.interfaces.IEntityResizer;
+import net.minecraft.client.renderer.IRenderTypeBuffer;
+import net.minecraft.client.renderer.ItemRenderer;
 import net.minecraft.client.renderer.entity.model.BipedModel;
+import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import top.theillusivec4.curios.api.type.capability.ICurio;
 import top.theillusivec4.curios.api.type.capability.ICurioItem;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @EventBusSubscriber(bus = Bus.FORGE, modid = SupersLegendMain.MOD_ID)
-public class GnatHat extends Item implements IEntityResizer, ICurioItem {
+public class GnatHat extends Item implements IEntityResizer, ICurioItem
+{
+    @OnlyIn(Dist.CLIENT)
+    private Object model;
+    // put your texture here
+    private static final ResourceLocation TEXTURE = new ResourceLocation(SupersLegendMain.MOD_ID, "textures/models/armor/gnat_hat.png");
+
     private static final Map<EquipmentSlotType, BipedModel<?>> MODELS_CACHE = new HashMap<>();
 
     public GnatHat(Properties properties) {
@@ -46,5 +65,27 @@ public class GnatHat extends Item implements IEntityResizer, ICurioItem {
     @Override
     public float getScale(PlayerEntity player) {
         return 0.2F;
+    }
+
+    @Override
+    public boolean canRender(String identifier, int index, LivingEntity livingEntity, ItemStack stack)
+    {
+        return true;
+    }
+
+    @Override
+    public void render(String identifier, int index, MatrixStack matrixStack, IRenderTypeBuffer renderTypeBuffer, int light, LivingEntity livingEntity, float limbSwing, float limbSwingAmount,
+                       float partialTicks, float ageInTicks, float netHeadYaw, float headPitch, ItemStack stack)
+    {
+        // put your model here
+        if (!(this.model instanceof GnatHatModel))
+        {
+            model = new GnatHatModel<>();
+        }
+
+        GnatHatModel<?> maskModel = (GnatHatModel<?>) this.model;
+        ICurio.RenderHelper.followHeadRotations(livingEntity, maskModel.base);
+        IVertexBuilder vertexBuilder = ItemRenderer.getArmorFoilBuffer(renderTypeBuffer, maskModel.renderType(TEXTURE), false, stack.hasFoil());
+        maskModel.renderToBuffer(matrixStack, vertexBuilder, light, OverlayTexture.NO_OVERLAY, 1.0F, 1.0F, 1.0F, 1.0F);
     }
 }
