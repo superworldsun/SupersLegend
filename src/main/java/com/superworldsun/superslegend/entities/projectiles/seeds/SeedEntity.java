241d7644173f76a0ee1b9016c1a41fc39cf74a2d
Daripher <saelver.serbek@gmail.com>
2022-06-10 18:28:57 +0500

    Base class for seed entities

---
+++ src/main/java/com/superworldsun/superslegend/entities/projectiles/seeds/SeedEntity.java
@@ -0,0 +1,69 @@
+package com.superworldsun.superslegend.entities.projectiles.seeds;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.projectile.AbstractArrowEntity;
+import net.minecraft.network.IPacket;
+import net.minecraft.util.SoundEvent;
+import net.minecraft.util.SoundEvents;
+import net.minecraft.util.math.EntityRayTraceResult;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.network.NetworkHooks;
+
+public abstract class SeedEntity extends AbstractArrowEntity
+{
+	public SeedEntity(EntityType<? extends SeedEntity> type, World world)
+	{
+		super(type, world);
+	}
+	
+	public SeedEntity(EntityType<? extends SeedEntity> entityType, LivingEntity shooter, World worldIn)
+	{
+		super(entityType, shooter, worldIn);
+	}
+	
+	@Override
+	public void onAddedToWorld()
+	{
+		super.onAddedToWorld();
+		setBaseDamage(2.0D);
+	}
+	
+	protected SoundEvent getDefaultHitGroundSoundEvent()
+	{
+		return SoundEvents.BAMBOO_BREAK;
+	}
+	
+	@Override
+	public IPacket<?> getAddEntityPacket()
+	{
+		return NetworkHooks.getEntitySpawningPacket(this);
+	}
+	
+	@Override
+	public void tick()
+	{
+		super.tick();
+		
+		if (inGround)
+		{
+			remove();
+		}
+	}
+	
+	@Override
+	protected void onHitEntity(EntityRayTraceResult result)
+	{
+		super.onHitEntity(result);
+		
+		if (result.getEntity() instanceof LivingEntity)
+		{
+			LivingEntity target = (LivingEntity) result.getEntity();
+			
+			if (!level.isClientSide && getPierceLevel() <= 0)
+			{
+				target.setArrowCount(target.getArrowCount() - 1);
+			}
+		}
+	}
+}
