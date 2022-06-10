645851c2c94536aa7bf0e310b4dacfcdb74c1419
Daripher <saelver.serbek@gmail.com>
2022-06-11 00:35:11 +0500

    Letters can now contain any items

--- src/main/java/com/superworldsun/superslegend/items/items/Letter.java
+++ src/main/java/com/superworldsun/superslegend/items/items/Letter.java
@@ -3,9 +3,7 @@ package com.superworldsun.superslegend.items.items;
 import com.superworldsun.superslegend.SupersLegendMain;
 import com.superworldsun.superslegend.items.LetterItem;
 
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 
 public class Letter extends LetterItem
 {
@@ -17,7 +15,6 @@ public class Letter extends LetterItem
 	@Override
 	public boolean canHoldItem(ItemStack stack)
 	{
-		Item item = stack.getItem();
-		return item == Items.WRITTEN_BOOK;
+		return true;
 	}
 }
