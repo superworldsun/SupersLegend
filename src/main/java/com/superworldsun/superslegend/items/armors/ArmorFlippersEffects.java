7c2345c27b09ebe0642796672b020f7604d7c276
superworldsun <superworldsun@gmail.com>
2022-07-29 17:08:09 -0400

    Updated ZorasFLippers (Updated swiming methods for speed underwater)

--- src/main/java/com/superworldsun/superslegend/items/armors/ArmorFlippersEffects.java
+++ src/main/java/com/superworldsun/superslegend/items/armors/ArmorFlippersEffects.java
@@ -4,6 +4,7 @@ import com.superworldsun.superslegend.SupersLegendMain;
 import com.superworldsun.superslegend.client.model.armor.KokiriArmorModel;
 import com.superworldsun.superslegend.client.model.armor.KokiriBootsModel;
 import com.superworldsun.superslegend.client.model.armor.ZorasFlippersModel;
+import com.superworldsun.superslegend.interfaces.IJumpingEntity;
 import com.superworldsun.superslegend.items.custom.NonEnchantArmor;
 import com.superworldsun.superslegend.registries.ArmourInit;
 import com.superworldsun.superslegend.registries.ItemInit;
@@ -11,23 +12,35 @@ import net.minecraft.client.renderer.entity.model.BipedModel;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.ai.attributes.Attribute;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.entity.ai.attributes.Attributes;
+import net.minecraft.entity.ai.attributes.ModifiableAttributeInstance;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.*;
 import net.minecraft.potion.EffectInstance;
+import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.common.ForgeMod;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE, modid = SupersLegendMain.MOD_ID)
 public class ArmorFlippersEffects extends NonEnchantArmor
 {
     private static final Map<EquipmentSlotType, BipedModel<?>> MODELS_CACHE = new HashMap<>();
+    private static final UUID ZORA_FLIPPERS_MODIFIER_ID = UUID.fromString("0fcc5d27-433f-4e9f-ac0e-0488a54b01ac");
 
     public ArmorFlippersEffects(EquipmentSlotType slot, Properties properties) {
         super(ArmourInit.FLIPPERS, slot, properties);
@@ -65,23 +78,68 @@ public class ArmorFlippersEffects extends NonEnchantArmor
             return SupersLegendMain.MOD_ID + ":textures/models/armor/zoras_flippers.png";
     }
 
-    @Override
-    public void onArmorTick(ItemStack stack, World world, PlayerEntity player) 
+    @SubscribeEvent
+    public static void onPlayerTick(TickEvent.PlayerTickEvent event)
     {
-        if (!world.isClientSide){
-                boolean isBootsOn = player.getItemBySlot(EquipmentSlotType.FEET).getItem().equals(ItemInit.ZORA_FLIPPERS);
-                if(isBootsOn)
-                	{
-                	if(player.isInWater()&&player.isSprinting()) 
-                	{
-                	    //todo add potion back
-                		//player.addEffect(new EffectInstance(PotionList.zoras_grace_effect, 8, 0, false, false));
-                	}
-                	if(!player.isSprinting()) 
-                	{
-                		//player.removeEffect(PotionList.zoras_grace_effect);
-                	}
+        // Prevent applying changes 2 times per tick
+        if (event.phase == TickEvent.Phase.START)
+        {
+            return;
+        }
+
+        // Only if we have flippers
+        if (event.player.getItemBySlot(EquipmentSlotType.FEET).getItem() == ItemInit.ZORA_FLIPPERS.get())
+        {
+            if (event.player.isInWater())
+            {
+
+                if(event.player.isInWater()&&event.player.isSprinting())
+                {
+                    // +50%
+                    addOrReplaceModifier(event.player, ForgeMod.SWIM_SPEED.get(), ZORA_FLIPPERS_MODIFIER_ID, 0.5F, AttributeModifier.Operation.MULTIPLY_TOTAL);
                 }
+                else
+                {
+                    removeModifier(event.player, ForgeMod.SWIM_SPEED.get(), ZORA_FLIPPERS_MODIFIER_ID);
+                }
+            }
+        }
+        else
+        {
+            removeModifier(event.player, ForgeMod.SWIM_SPEED.get(), ZORA_FLIPPERS_MODIFIER_ID);
         }
     }
+
+    private static void removeModifier(PlayerEntity player, Attribute attribute, UUID id)
+    {
+        ModifiableAttributeInstance attributeInstance = player.getAttribute(attribute);
+        AttributeModifier modifier = attributeInstance.getModifier(id);
+
+        if (modifier != null)
+        {
+            attributeInstance.removeModifier(modifier);
+        }
+    }
+
+    private static void addOrReplaceModifier(PlayerEntity player, Attribute attribute, UUID id, float amount, AttributeModifier.Operation operation)
+    {
+        ModifiableAttributeInstance attributeInstance = player.getAttribute(attribute);
+        AttributeModifier modifier = attributeInstance.getModifier(id);
+
+        if (modifier != null && modifier.getAmount() != amount)
+        {
+            attributeInstance.removeModifier(modifier);
+            modifier = new AttributeModifier(id, id.toString(), amount, operation);
+        }
+        else if (modifier == null)
+        {
+            modifier = new AttributeModifier(id, id.toString(), amount, operation);
+        }
+
+        if (modifier != null && !attributeInstance.hasModifier(modifier))
+        {
+            attributeInstance.addPermanentModifier(modifier);
+        }
+    }
+    
 }
