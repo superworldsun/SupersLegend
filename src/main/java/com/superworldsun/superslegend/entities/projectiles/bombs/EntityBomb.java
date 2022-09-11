6c97bd716f52b2e5fb8b1ad327f35a375f97e0b4
Cozary <ayrecio99@gmail.com>
2022-09-11 02:21:33 +0200

    [AbstractEntityBomb] Remove bounce

--- src/main/java/com/superworldsun/superslegend/entities/projectiles/bombs/EntityBomb.java
+++ src/main/java/com/superworldsun/superslegend/entities/projectiles/bombs/EntityBomb.java
@@ -13,14 +13,13 @@ public class EntityBomb extends AbstractEntityBomb {
     private static final float SECONDS_TO_EXPLODE = 4.0f;
     private static final float SECONDS_TO_FLASH_RAPIDLY = 2.0f;
     private static final int EXPLOSION_POWER = 4;
-    private static final double BOUNCE_DAMPENING_FACTOR = 0.05;
 
     public EntityBomb(EntityType<EntityBomb> type, World world) {
-        super(type, world, SECONDS_TO_EXPLODE, SECONDS_TO_FLASH_RAPIDLY, EXPLOSION_POWER, BOUNCE_DAMPENING_FACTOR);
+        super(type, world, SECONDS_TO_EXPLODE, SECONDS_TO_FLASH_RAPIDLY, EXPLOSION_POWER);
     }
 
     public EntityBomb(LivingEntity shooter, World world) {
-        super(EntityTypeInit.BOMB.get(), shooter, world, SECONDS_TO_EXPLODE, SECONDS_TO_FLASH_RAPIDLY, EXPLOSION_POWER, BOUNCE_DAMPENING_FACTOR);
+        super(EntityTypeInit.BOMB.get(), shooter, world, SECONDS_TO_EXPLODE, SECONDS_TO_FLASH_RAPIDLY, EXPLOSION_POWER);
     }
 	
 	public static EntityType<EntityBomb> createEntityType()
