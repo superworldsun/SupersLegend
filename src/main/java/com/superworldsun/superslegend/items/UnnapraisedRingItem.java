de5552aa6ad9803ebe4f1846f127cf67fea83a85
superworldsun <superworldsun@gmail.com>
2022-10-15 04:31:28 -0400

    Add UnnapraisedRingItem

---
+++ src/main/java/com/superworldsun/superslegend/items/UnnapraisedRingItem.java
@@ -0,0 +1,42 @@
+package com.superworldsun.superslegend.items;
+
+import com.superworldsun.superslegend.container.BagContainer;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.inventory.container.Container;
+import net.minecraft.inventory.container.INamedContainerProvider;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvents;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.fml.network.NetworkHooks;
+
+import java.util.List;
+
+public class UnnapraisedRingItem extends Item
+{
+	public UnnapraisedRingItem(Properties p_i48487_1_) {
+		super(p_i48487_1_);
+	}
+
+	@OnlyIn(Dist.CLIENT)
+	@Override
+	public void appendHoverText(ItemStack stack, World world, List<ITextComponent> list, ITooltipFlag flag)
+	{
+		super.appendHoverText(stack, world, list, flag);
+		list.add(new StringTextComponent(TextFormatting.GRAY + "An Unapraised Ring, give it to a novice Tool Smith"));
+		list.add(new StringTextComponent(TextFormatting.GRAY + "to have it looked at"));
+	}
+
+}
