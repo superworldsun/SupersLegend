0b4268b79994abd0c4e5b6aefe42731c00eead57
superworldsun <superworldsun@gmail.com>
2022-10-15 04:46:37 -0400

    Updated GreenHolyRing, added correct code

--- src/main/java/com/superworldsun/superslegend/items/curios/rings/GreenHolyRing.java
+++ src/main/java/com/superworldsun/superslegend/items/curios/rings/GreenHolyRing.java
@@ -1,13 +1,63 @@
 package com.superworldsun.superslegend.items.curios.rings;
 
+import com.superworldsun.superslegend.SupersLegendMain;
 import com.superworldsun.superslegend.items.RingItem;
 
+import com.superworldsun.superslegend.registries.ItemInit;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.event.entity.living.LivingAttackEvent;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import org.apache.commons.lang3.tuple.ImmutableTriple;
+import top.theillusivec4.curios.api.CuriosApi;
 
+import java.util.List;
+
+@Mod.EventBusSubscriber(modid = SupersLegendMain.MOD_ID)
 public class GreenHolyRing extends RingItem
 {	
 	public GreenHolyRing()
 	{
 		super(new Item.Properties());
+	}
+
+	@SubscribeEvent
+	public static void onLivingAttack(LivingAttackEvent event)
+	{
+		// Check if it is the Player who takes damage.
+		if (event.getEntityLiving() instanceof PlayerEntity)
+		{
+			PlayerEntity player = (PlayerEntity) event.getEntityLiving();
+			// Get the Ring as an ItemStack
+			ItemStack stack = CuriosApi.getCuriosHelper().findEquippedCurio(ItemInit.GREEN_HOLY_RING.get(), player).map(ImmutableTriple::getRight).orElse(ItemStack.EMPTY);
+
+			// Check if player is wearing it.
+			if (!stack.isEmpty())
+			{
+				if (event.getSource() == DamageSource.LIGHTNING_BOLT)
+				{
+					event.setCanceled(true);
+				}
+			}
+		}
+	}
+
+	@OnlyIn(Dist.CLIENT)
+	@Override
+	public void appendHoverText(ItemStack stack, World world, List<ITextComponent> list, ITooltipFlag flag)
+	{
+		super.appendHoverText(stack, world, list, flag);
+		list.add(new StringTextComponent(TextFormatting.GREEN + "No damage from electricity"));
 	}
 }
