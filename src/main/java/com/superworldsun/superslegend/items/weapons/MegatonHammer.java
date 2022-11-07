9ff37c8aea83395f2458c90d3ec886563c1a737f
superworldsun <superworldsun@gmail.com>
2022-11-07 05:36:39 -0500

    Updated Hammers, made some of them require both hands

--- src/main/java/com/superworldsun/superslegend/items/weapons/MegatonHammer.java
+++ src/main/java/com/superworldsun/superslegend/items/weapons/MegatonHammer.java
@@ -6,6 +6,9 @@ import com.superworldsun.superslegend.SupersLegendMain;
 import com.superworldsun.superslegend.util.ItemToolTiers;
 
 import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.StringTextComponent;
@@ -22,13 +25,36 @@ public class MegatonHammer extends HammerItem
 	@Override
 	protected int getLeftClickCooldown()
 	{
-		return 40;
+		return 18;
 	}
-	
+
+	//TODO When breaking plants there is no particle effect or sound played
+	//TODO Add blocks to the Init, list isnt full
+	//TODO Add a sound for when the hammer hits a block & entity
+	public void inventoryTick(ItemStack stack, World world, Entity entity, int itemSlot, boolean isSelected)
+	{
+		if(entity instanceof PlayerEntity)
+		{
+			PlayerEntity player = (PlayerEntity)entity;
+			ItemStack equipped = player.getMainHandItem();
+			{
+				if(stack == equipped)
+				{
+					if(player.hasItemInSlot(EquipmentSlotType.OFFHAND))
+					{
+						player.spawnAtLocation(player.getOffhandItem());
+						player.setItemSlot(EquipmentSlotType.OFFHAND, ItemStack.EMPTY);
+					}
+				}
+			}
+		}
+	}
+
 	@Override
 	public void appendHoverText(ItemStack stack, World world, List<ITextComponent> list, ITooltipFlag flag)
 	{
 		super.appendHoverText(stack, world, list, flag);
-		list.add(new StringTextComponent(TextFormatting.WHITE + "A large hammer from the goron tribe"));
+		list.add(new StringTextComponent(TextFormatting.WHITE + "A Large hammer from the Goron tribe"));
+		list.add(new StringTextComponent(TextFormatting.GRAY + "Requires two hands to wield"));
 	}
 }
