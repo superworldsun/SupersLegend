18ab4b78bec7bde0ba3e947a827fa50d21f93bd1
superworldsun <superworldsun@gmail.com>
2022-12-01 21:56:26 -0500

    Add abandoned_mineshaft loot (currently is bugged and needs looked at)

--- src/main/java/com/superworldsun/superslegend/registries/LootInit.java
+++ src/main/java/com/superworldsun/superslegend/registries/LootInit.java
@@ -1,7 +1,7 @@
 package com.superworldsun.superslegend.registries;
 
 import com.superworldsun.superslegend.SupersLegendMain;
-import com.superworldsun.superslegend.loot.FishingLootModifier;
+import com.superworldsun.superslegend.loot.GlobalLootModifier;
 
 import net.minecraftforge.common.loot.GlobalLootModifierSerializer;
 import net.minecraftforge.fml.RegistryObject;
@@ -12,5 +12,7 @@ public class LootInit
 {
 	public static final DeferredRegister<GlobalLootModifierSerializer<?>> REGISTRY = DeferredRegister.create(ForgeRegistries.LOOT_MODIFIER_SERIALIZERS, SupersLegendMain.MOD_ID);
     
-	public static final RegistryObject<FishingLootModifier.Serializer> FISHING = REGISTRY.register("fishing", FishingLootModifier.Serializer::new);
+	public static final RegistryObject<GlobalLootModifier.Serializer> FISHING = REGISTRY.register("fishing", GlobalLootModifier.Serializer::new);
+	public static final RegistryObject<GlobalLootModifier.Serializer> CREEPER_MUSIC_DISC = REGISTRY.register("creeper_music_disc", GlobalLootModifier.Serializer::new);
+	public static final RegistryObject<GlobalLootModifier.Serializer> ABANDONED_MINESHAFT = REGISTRY.register("abandoned_mineshaft", GlobalLootModifier.Serializer::new);
 }
