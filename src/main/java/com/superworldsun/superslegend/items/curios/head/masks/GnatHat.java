c4ab5eb693cc247b7d9cc9db1e4e4c066b513478
superworldsun <superworldsun@gmail.com>
2022-12-23 06:26:51 -0500

    Add TODO for GnatHat

--- src/main/java/com/superworldsun/superslegend/items/curios/head/masks/GnatHat.java
+++ src/main/java/com/superworldsun/superslegend/items/curios/head/masks/GnatHat.java
@@ -3,7 +3,6 @@ package com.superworldsun.superslegend.items.curios.head.masks;
 import com.mojang.blaze3d.matrix.MatrixStack;
 import com.mojang.blaze3d.vertex.IVertexBuilder;
 import com.superworldsun.superslegend.SupersLegendMain;
-import com.superworldsun.superslegend.client.model.armor.AllNightMaskModel;
 import com.superworldsun.superslegend.client.model.armor.GnatHatModel;
 import com.superworldsun.superslegend.interfaces.IEntityResizer;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
@@ -40,27 +39,8 @@ public class GnatHat extends Item implements IEntityResizer, ICurioItem
         super(properties);
     }
 
-	/*
-	@OnlyIn(Dist.CLIENT)
-	@SuppressWarnings("unchecked")
-	@Override
-	public <M extends BipedModel<?>> M getArmorModel(LivingEntity entityLiving, ItemStack itemStack, EquipmentSlotType armorSlot, M _default)
-	{
-		if (!MODELS_CACHE.containsKey(armorSlot))
-		{
-			MODELS_CACHE.put(armorSlot, new GnatHatModel());
-		}
-
-		return (M) MODELS_CACHE.get(armorSlot);
-	}
-	
-	@OnlyIn(Dist.CLIENT)
-	@Override
-	public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type)
-	{
-		return SupersLegendMain.MOD_ID + ":textures/models/armor/gnat_hat.png";
-	}
-	*/
+    //TODO make it so the player cant see inside blocks when hugging walls,
+    // make the players range smaller, reduce damage they deal, increase damage they take
 
     @Override
     public float getScale(PlayerEntity player) {
