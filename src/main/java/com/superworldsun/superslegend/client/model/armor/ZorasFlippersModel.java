d45b4b7a8f4e5e18265e96895db7906481212b22
superworldsun <superworldsun@gmail.com>
2022-12-23 05:28:09 -0500

    Update ZorasFLippers,fixed model issue where model was fixed in place

--- src/main/java/com/superworldsun/superslegend/client/model/armor/ZorasFlippersModel.java
+++ src/main/java/com/superworldsun/superslegend/client/model/armor/ZorasFlippersModel.java
@@ -1,5 +1,6 @@
 package com.superworldsun.superslegend.client.model.armor;
 
+import com.google.common.collect.ImmutableList;
 import com.mojang.blaze3d.matrix.MatrixStack;
 import com.mojang.blaze3d.vertex.IVertexBuilder;
 import net.minecraft.client.renderer.entity.model.BipedModel;
@@ -7,41 +8,81 @@ import net.minecraft.client.renderer.model.ModelRenderer;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 
-public class ZorasFlippersModel extends BipedModel<LivingEntity>
-{
-	private final ModelRenderer left_leg;
-	private final ModelRenderer right_leg;
+public class ZorasFlippersModel extends BipedModel<LivingEntity> {
+	private final ModelRenderer LeftFoot_r1;
+	private final ModelRenderer RightFoot_r1;
 
-	public ZorasFlippersModel()
-	{
-		super(0F);
-		
+	//TODO low priority,model could use small adjustments with its placement as its off a bit.
+
+	public ZorasFlippersModel() {
+		super(0.0F);
+
 		texWidth = 64;
 		texHeight = 64;
 
-		left_leg = new ModelRenderer(this);
-		left_leg.setPos(-2.0F, 12.0F, 0.0F);
-		left_leg.texOffs(0, 9).addBox(1.05F, 11.99F, -6.9F, 6.0F, 0.0F, 9.0F, 0.0F, false);
-		left_leg.texOffs(21, 6).addBox(1.55F, 10.2F, -2.5F, 5.0F, 2.0F, 1.0F, 0.0F, false);
-		left_leg.texOffs(21, 3).addBox(1.55F, 10.2F, 1.5F, 5.0F, 2.0F, 1.0F, 0.0F, false);
-		left_leg.texOffs(0, 19).addBox(5.55F, 10.2F, -1.5F, 1.0F, 2.0F, 3.0F, 0.0F, false);
-		left_leg.texOffs(0, 10).addBox(1.55F, 10.2F, -1.5F, 1.0F, 2.0F, 3.0F, 0.0F, false);
-		left_leg.texOffs(9, 18).addBox(2.55F, 11.2F, -1.5F, 3.0F, 1.0F, 3.0F, 0.0F, false);
+		leftLeg = new ModelRenderer(this);
+		leftLeg.setPos(-1.3F, 11.7F, -2.3F);
+		leftLeg.texOffs(0, 9).addBox(-2.65F, 11.85F, -6.6F, 6.0F, 0.0F, 9.0F, 0.0F, false);
+		leftLeg.texOffs(21, 6).addBox(-2.15F, 10.0F, -2.2F, 5.0F, 2.0F, 1.0F, 0.0F, false);
+		leftLeg.texOffs(0, 19).addBox(1.85F, 10.0F, -1.2F, 1.0F, 2.0F, 3.0F, 0.0F, false);
+		leftLeg.texOffs(21, 3).addBox(-2.15F, 10.0F, 1.8F, 5.0F, 2.0F, 1.0F, 0.0F, false);
+		leftLeg.texOffs(0, 10).addBox(-2.15F, 10.0F, -1.2F, 1.0F, 2.0F, 3.0F, 0.0F, false);
+		leftLeg.texOffs(9, 18).addBox(-1.15F, 11.0F, -1.2F, 3.0F, 1.0F, 3.0F, 0.0F, false);
 
-		right_leg = new ModelRenderer(this);
-		right_leg.setPos(2.0F, 12.0F, 0.0F);
-		right_leg.texOffs(0, 15).addBox(-5.45F, 11.2F, -1.5F, 3.0F, 1.0F, 3.0F, 0.0F, false);
-		right_leg.texOffs(0, 0).addBox(-6.45F, 10.2F, -1.5F, 1.0F, 2.0F, 3.0F, 0.0F, false);
-		right_leg.texOffs(0, 5).addBox(-2.45F, 10.2F, -1.5F, 1.0F, 2.0F, 3.0F, 0.0F, false);
-		right_leg.texOffs(18, 18).addBox(-6.45F, 10.2F, 1.5F, 5.0F, 2.0F, 1.0F, 0.0F, false);
-		right_leg.texOffs(21, 0).addBox(-6.45F, 10.2F, -2.5F, 5.0F, 2.0F, 1.0F, 0.0F, false);
-		right_leg.texOffs(0, 0).addBox(-6.95F, 11.99F, -6.9F, 6.0F, 0.0F, 9.0F, 0.0F, false);
+		/*leftLeg.setPos(-1.3F, 11.7F, -2.3F);
+		leftLeg.texOffs(0, 9).addBox(-1.65F, 12.04F, -6.6F, 6.0F, 0.0F, 9.0F, 0.0F, false);
+		leftLeg.texOffs(21, 6).addBox(-1.15F, 10.0F, -2.2F, 5.0F, 2.0F, 1.0F, 0.0F, false);
+		leftLeg.texOffs(0, 19).addBox(2.85F, 10.0F, -1.2F, 1.0F, 2.0F, 3.0F, 0.0F, false);
+		leftLeg.texOffs(21, 3).addBox(-1.15F, 10.0F, 1.8F, 5.0F, 2.0F, 1.0F, 0.0F, false);
+		leftLeg.texOffs(0, 10).addBox(-1.15F, 10.0F, -1.2F, 1.0F, 2.0F, 3.0F, 0.0F, false);
+		leftLeg.texOffs(9, 18).addBox(-0.15F, 11.0F, -1.2F, 3.0F, 1.0F, 3.0F, 0.0F, false);*/
+
+		LeftFoot_r1 = new ModelRenderer(this);
+		leftLeg.addChild(LeftFoot_r1);
+		setRotationAngle(LeftFoot_r1, 0.2182F, 0.0F, 0.0F);
+
+		rightLeg = new ModelRenderer(this);
+		rightLeg.setPos(3.7F, 11.7F, -2.3F);
+		rightLeg.texOffs(0, 0).addBox(-3.50F, 11.85F, -6.6F, 6.0F, 0.0F, 9.0F, 0.0F, false);
+		rightLeg.texOffs(21, 0).addBox(-3.0F, 10.0F, -2.2F, 5.0F, 2.0F, 1.0F, 0.0F, false);
+		rightLeg.texOffs(18, 18).addBox(-3.0F, 10.0F, 1.8F, 5.0F, 2.0F, 1.0F, 0.0F, false);
+		rightLeg.texOffs(0, 5).addBox(1.0F, 10.0F, -1.2F, 1.0F, 2.0F, 3.0F, 0.0F, false);
+		rightLeg.texOffs(0, 0).addBox(-3.0F, 10.0F, -1.2F, 1.0F, 2.0F, 3.0F, 0.0F, false);
+		rightLeg.texOffs(0, 15).addBox(-2.0F, 11.0F, -1.2F, 3.0F, 1.0F, 3.0F, 0.0F, false);
+
+		//SECOND
+		/*rightLeg.setPos(3.7F, 11.7F, -2.3F);
+		rightLeg.texOffs(0, 0).addBox(-3.65F, 12.04F, -6.6F, 6.0F, 0.0F, 9.0F, 0.0F, false);
+		rightLeg.texOffs(21, 0).addBox(-3.15F, 10.0F, -2.2F, 5.0F, 2.0F, 1.0F, 0.0F, false);
+		rightLeg.texOffs(18, 18).addBox(-3.15F, 10.0F, 1.8F, 5.0F, 2.0F, 1.0F, 0.0F, false);
+		rightLeg.texOffs(0, 5).addBox(0.85F, 10.0F, -1.2F, 1.0F, 2.0F, 3.0F, 0.0F, false);
+		rightLeg.texOffs(0, 0).addBox(-3.15F, 10.0F, -1.2F, 1.0F, 2.0F, 3.0F, 0.0F, false);
+		rightLeg.texOffs(0, 15).addBox(-2.15F, 11.0F, -1.2F, 3.0F, 1.0F, 3.0F, 0.0F, false);*/
+
+		//FIRST
+		/*rightLeg.setPos(3.7F, 11.7F, -2.3F);
+		rightLeg.texOffs(0, 0).addBox(-5.65F, 12.04F, -6.6F, 6.0F, 0.0F, 9.0F, 0.0F, false);
+		rightLeg.texOffs(21, 0).addBox(-5.15F, 10.0F, -2.2F, 5.0F, 2.0F, 1.0F, 0.0F, false);
+		rightLeg.texOffs(18, 18).addBox(-5.15F, 10.0F, 1.8F, 5.0F, 2.0F, 1.0F, 0.0F, false);
+		rightLeg.texOffs(0, 5).addBox(-1.15F, 10.0F, -1.2F, 1.0F, 2.0F, 3.0F, 0.0F, false);
+		rightLeg.texOffs(0, 0).addBox(-5.15F, 10.0F, -1.2F, 1.0F, 2.0F, 3.0F, 0.0F, false);
+		rightLeg.texOffs(0, 15).addBox(-4.15F, 11.0F, -1.2F, 3.0F, 1.0F, 3.0F, 0.0F, false);*/
+
+		RightFoot_r1 = new ModelRenderer(this);
+		rightLeg.addChild(RightFoot_r1);
+		setRotationAngle(RightFoot_r1, 0.2182F, 0.0F, 0.0F);
+	}
+
+	@Override
+	protected Iterable<ModelRenderer> bodyParts()
+	{
+		return ImmutableList.of(leftLeg, rightLeg);
 	}
 
 	@Override
 	public void renderToBuffer(MatrixStack matrixStack, IVertexBuilder buffer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha){
-		left_leg.render(matrixStack, buffer, packedLight, packedOverlay, red, green, blue, alpha);
-		right_leg.render(matrixStack, buffer, packedLight, packedOverlay, red, green, blue, alpha);
+		leftLeg.render(matrixStack, buffer, packedLight, packedOverlay, red, green, blue, alpha);
+		rightLeg.render(matrixStack, buffer, packedLight, packedOverlay, red, green, blue, alpha);
 	}
 
 	public void setRotationAngle(ModelRenderer modelRenderer, float x, float y, float z)
--- src/main/java/com/superworldsun/superslegend/items/armors/ArmorFlippersEffects.java
+++ src/main/java/com/superworldsun/superslegend/items/armors/ArmorFlippersEffects.java
@@ -1,10 +1,7 @@
 package com.superworldsun.superslegend.items.armors;
 
 import com.superworldsun.superslegend.SupersLegendMain;
-import com.superworldsun.superslegend.client.model.armor.KokiriArmorModel;
-import com.superworldsun.superslegend.client.model.armor.KokiriBootsModel;
-import com.superworldsun.superslegend.client.model.armor.ZorasFlippersModel;
-import com.superworldsun.superslegend.interfaces.IJumpingEntity;
+import com.superworldsun.superslegend.client.model.armor.*;
 import com.superworldsun.superslegend.items.custom.NonEnchantArmor;
 import com.superworldsun.superslegend.registries.ArmourInit;
 import com.superworldsun.superslegend.registries.ItemInit;
@@ -14,13 +11,10 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.ai.attributes.Attribute;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
-import net.minecraft.entity.ai.attributes.Attributes;
 import net.minecraft.entity.ai.attributes.ModifiableAttributeInstance;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.*;
-import net.minecraft.potion.EffectInstance;
-import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
@@ -39,12 +33,32 @@ import java.util.UUID;
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE, modid = SupersLegendMain.MOD_ID)
 public class ArmorFlippersEffects extends NonEnchantArmor
 {
+
     private static final Map<EquipmentSlotType, BipedModel<?>> MODELS_CACHE = new HashMap<>();
     private static final UUID ZORA_FLIPPERS_MODIFIER_ID = UUID.fromString("0fcc5d27-433f-4e9f-ac0e-0488a54b01ac");
 
     public ArmorFlippersEffects(EquipmentSlotType slot, Properties properties) {
         super(ArmourInit.FLIPPERS, slot, properties);
     }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public <M extends BipedModel<?>> M getArmorModel(LivingEntity entityLiving, ItemStack itemStack, EquipmentSlotType armorSlot, M _default)
+    {
+        if (MODELS_CACHE.isEmpty())
+        {
+            MODELS_CACHE.put(EquipmentSlotType.FEET, new ZorasFlippersModel());
+        }
+
+        return (M) MODELS_CACHE.get(armorSlot);
+    }
+
+    @Override
+    public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type)
+    {
+        return SupersLegendMain.MOD_ID + ":textures/models/armor/zoras_flippers.png";
+    }
+
     @OnlyIn(Dist.CLIENT)
     @Override
     public void appendHoverText(ItemStack stack, World world, java.util.List<ITextComponent> list, ITooltipFlag flag)
@@ -52,32 +66,6 @@ public class ArmorFlippersEffects extends NonEnchantArmor
 		super.appendHoverText(stack, world, list, flag);				
 		list.add(new StringTextComponent(TextFormatting.AQUA + "Provides the ability to swim like a Zora"));
 	}
-
-    @OnlyIn(Dist.CLIENT)
-    @SuppressWarnings("unchecked")
-    @Override
-    public <M extends BipedModel<?>> M getArmorModel(LivingEntity entityLiving, ItemStack itemStack, EquipmentSlotType armorSlot, M _default)
-    {
-        if (!MODELS_CACHE.containsKey(armorSlot))
-        {
-            if (armorSlot == EquipmentSlotType.FEET)
-            {
-                MODELS_CACHE.put(armorSlot, new ZorasFlippersModel());
-            }
-        }
-
-        return (M) MODELS_CACHE.get(armorSlot);
-    }
-
-    @OnlyIn(Dist.CLIENT)
-    @Override
-    public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType armorSlot, String type)
-    {
-        if (armorSlot == EquipmentSlotType.FEET)
-            return SupersLegendMain.MOD_ID + ":textures/models/armor/zoras_flippers.png";
-        else
-            return SupersLegendMain.MOD_ID + ":textures/models/armor/zoras_flippers.png";
-    }
 
     @SubscribeEvent
     public static void onPlayerTick(TickEvent.PlayerTickEvent event)
