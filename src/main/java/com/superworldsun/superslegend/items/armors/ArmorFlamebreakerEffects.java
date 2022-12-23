104bde0c87109458625a7fcf4a1a1fde60f26d65
superworldsun <superworldsun@gmail.com>
2022-12-23 05:22:23 -0500

    Update FlameBreaker armor, now prevents fire damage with full set, no longer provides fire resist effect, now scales damage reduction with each armor piece

--- src/main/java/com/superworldsun/superslegend/items/armors/ArmorFlamebreakerEffects.java
+++ src/main/java/com/superworldsun/superslegend/items/armors/ArmorFlamebreakerEffects.java
@@ -1,5 +1,6 @@
 package com.superworldsun.superslegend.items.armors;
 
+import com.superworldsun.superslegend.SupersLegendMain;
 import com.superworldsun.superslegend.items.custom.NonEnchantArmor;
 import com.superworldsun.superslegend.registries.ArmourInit;
 import com.superworldsun.superslegend.registries.ItemInit;
@@ -9,11 +10,20 @@ import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.*;
 import net.minecraft.potion.Effect;
 import net.minecraft.potion.EffectInstance;
+import net.minecraft.util.DamageSource;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
+import net.minecraftforge.event.entity.living.LivingAttackEvent;
+import net.minecraftforge.event.entity.living.LivingDamageEvent;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import org.apache.commons.lang3.tuple.ImmutableTriple;
+import top.theillusivec4.curios.api.CuriosApi;
 
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE, modid = SupersLegendMain.MOD_ID)
 public class ArmorFlamebreakerEffects extends NonEnchantArmor
 {
 
@@ -26,8 +36,91 @@ public class ArmorFlamebreakerEffects extends NonEnchantArmor
 	{
 		super.appendHoverText(stack, world, list, flag);				
 		list.add(new StringTextComponent(TextFormatting.RED + "Armor of the Gorons"));
-		list.add(new StringTextComponent(TextFormatting.GREEN + "Wearing full set grants fire restiance"));
+		list.add(new StringTextComponent(TextFormatting.GREEN + "Wearing full set will make you fireproof"));
 	}
+
+    @SubscribeEvent
+    public static void onLivingHurt(LivingHurtEvent event)
+    {
+        if (!(event.getEntity() instanceof PlayerEntity))
+        {
+            return;
+        }
+        PlayerEntity player = (PlayerEntity) event.getEntityLiving();
+        int armorPartsEquipped = 0;
+
+        if (player.getItemBySlot(EquipmentSlotType.HEAD).getItem() == ItemInit.FLAMEBREAKER_HELMET.get())
+            armorPartsEquipped++;
+
+        if (player.getItemBySlot(EquipmentSlotType.CHEST).getItem() == ItemInit.FLAMEBREAKER_TUNIC.get())
+            armorPartsEquipped++;
+
+        if (player.getItemBySlot(EquipmentSlotType.LEGS).getItem() == ItemInit.FLAMEBREAKER_LEGGINGS.get())
+            armorPartsEquipped++;
+
+        if (player.getItemBySlot(EquipmentSlotType.FEET).getItem() == ItemInit.FLAMEBREAKER_BOOTS.get())
+            armorPartsEquipped++;
+
+        if (armorPartsEquipped == 1)
+        {
+            if (event.getSource() == DamageSource.LAVA || event.getSource() == DamageSource.IN_FIRE || event.getSource() == DamageSource.ON_FIRE)
+            {
+                event.setAmount(event.getAmount() / 1.25f);
+            }
+        }
+        else if (armorPartsEquipped == 2)
+        {
+            if (event.getSource() == DamageSource.LAVA || event.getSource() == DamageSource.IN_FIRE || event.getSource() == DamageSource.ON_FIRE)
+            {
+                event.setAmount(event.getAmount() / 1.5f);
+            }
+        }
+        else if (armorPartsEquipped == 3)
+        {
+            if (event.getSource() == DamageSource.LAVA || event.getSource() == DamageSource.IN_FIRE || event.getSource() == DamageSource.ON_FIRE)
+            {
+                event.setAmount(event.getAmount() / 1.75f);
+            }
+        }
+        else if (armorPartsEquipped == 4)
+        {
+            if (event.getSource() == DamageSource.LAVA)
+            {
+                event.setAmount(event.getAmount() / 2f);
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public static void onLivingAttack(LivingAttackEvent event)
+    {
+        if (!(event.getEntity() instanceof PlayerEntity))
+        {
+            return;
+        }
+        PlayerEntity player = (PlayerEntity) event.getEntityLiving();
+        int armorPartsEquipped = 0;
+
+        if (player.getItemBySlot(EquipmentSlotType.HEAD).getItem() == ItemInit.FLAMEBREAKER_HELMET.get())
+            armorPartsEquipped++;
+
+        if (player.getItemBySlot(EquipmentSlotType.CHEST).getItem() == ItemInit.FLAMEBREAKER_TUNIC.get())
+            armorPartsEquipped++;
+
+        if (player.getItemBySlot(EquipmentSlotType.LEGS).getItem() == ItemInit.FLAMEBREAKER_LEGGINGS.get())
+            armorPartsEquipped++;
+
+        if (player.getItemBySlot(EquipmentSlotType.FEET).getItem() == ItemInit.FLAMEBREAKER_BOOTS.get())
+            armorPartsEquipped++;
+
+        if (armorPartsEquipped == 4)
+        {
+            if (event.getSource() == DamageSource.HOT_FLOOR || event.getSource() == DamageSource.IN_FIRE || event.getSource() == DamageSource.ON_FIRE)
+            {
+                event.setCanceled(true);
+            }
+        }
+    }
 
     @Override
     public void onArmorTick(ItemStack stack, World world, PlayerEntity player) 
@@ -40,10 +133,8 @@ public class ArmorFlamebreakerEffects extends NonEnchantArmor
                 boolean isBootsOn = player.getItemBySlot(EquipmentSlotType.FEET).getItem().getItem() == ItemInit.FLAMEBREAKER_BOOTS.get();
                 if(isHelmetOn&isChestplateOn&isLeggingsOn&isBootsOn)
                 	{
-                		player.addEffect(new EffectInstance(Effect.byId(12), 10, 0, false, false, false));
                 		player.clearFire();
                 	}
-                	
         }
     }
 }
