7242eac6845c519f8810383d1457fcbfd6649979
superworldsun <superworldsun@gmail.com>
2022-12-25 06:45:11 -0500

    Add BlockItems

---
+++ src/main/java/com/superworldsun/superslegend/items/block/SpikedPegItem.java
@@ -0,0 +1,31 @@
+package com.superworldsun.superslegend.items.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.fml.RegistryObject;
+
+import java.util.List;
+
+public class SpikedPegItem extends ModBlockItem
+{
+	public SpikedPegItem(RegistryObject<Block> blockObject)
+	{
+		super(blockObject);
+	}
+
+	@OnlyIn(Dist.CLIENT)
+	@Override
+	public void appendHoverText(ItemStack stack, World world, List<ITextComponent> list, ITooltipFlag flag)
+	{
+		super.appendHoverText(stack, world, list, flag);
+		list.add(new StringTextComponent(TextFormatting.GRAY + "Using a heavy hammer will bury the peg"));
+		list.add(new StringTextComponent(TextFormatting.GRAY + "When buried you can mine easier"));
+	}
+}
