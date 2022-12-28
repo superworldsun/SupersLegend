250b6172356dd35ca9af8b8cb6e21d64df8b33bc
superworldsun <superworldsun@gmail.com>
2022-12-27 19:41:58 -0500

    Updated ArmorZoraArmorEffects, Player now swims faster in water when wearing full set of armor

--- src/main/java/com/superworldsun/superslegend/items/armors/ArmorZoraArmorEffects.java
+++ src/main/java/com/superworldsun/superslegend/items/armors/ArmorZoraArmorEffects.java
@@ -1,9 +1,13 @@
 package com.superworldsun.superslegend.items.armors;
 
+import com.superworldsun.superslegend.SupersLegendMain;
 import com.superworldsun.superslegend.items.custom.NonEnchantArmor;
 import com.superworldsun.superslegend.registries.ArmourInit;
 import com.superworldsun.superslegend.registries.ItemInit;
 import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.ai.attributes.Attribute;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.entity.ai.attributes.ModifiableAttributeInstance;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.*;
@@ -15,10 +19,18 @@ import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.common.ForgeMod;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 
+import java.util.UUID;
+
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE, modid = SupersLegendMain.MOD_ID)
 public class ArmorZoraArmorEffects extends NonEnchantArmor
 {
-
+	private static final UUID ZORA_ARMOR_MODIFIER_ID = UUID.fromString("9b86d513-457e-4231-8d90-bdd270ec6748");
 
 	public ArmorZoraArmorEffects(EquipmentSlotType slot, Properties properties) {
 		super(ArmourInit.ZORAARMOR, slot, properties);
@@ -51,17 +63,75 @@ public class ArmorZoraArmorEffects extends NonEnchantArmor
                 		//Todo add potion back
                 		//player.addEffect(new EffectInstance(PotionList.zoras_grace_effect, 10, 0, false, false, false));
                 	}
-                	if(player.isInWater() && player.isSprinting()) 
-                	{
-                		player.addEffect(new EffectInstance(Effect.byId(13), 10, 0, false, false, false));
-                		//player.addEffect(new EffectInstance(PotionList.zoras_grace_effect, 10, 0, false, false, false));
-                	}
-                	if(!player.isSprinting()) 
-                	{
-                		//player.removeEffect(PotionList.zoras_grace_effect);
-                	}
-                	
                 }
         }
     }
+
+	@SubscribeEvent
+	public static void onPlayerTick(TickEvent.PlayerTickEvent event)
+	{
+		// Prevent applying changes 2 times per tick
+		if (event.phase == TickEvent.Phase.START)
+		{
+			return;
+		}
+
+		// Only if we have flippers
+		boolean isHelmetOn = event.player.getItemBySlot(EquipmentSlotType.HEAD).getItem() == ItemInit.ZORA_ARMOR_CAP.get();
+		boolean isChestplateOn = event.player.getItemBySlot(EquipmentSlotType.CHEST).getItem() == ItemInit.ZORA_ARMOR_TUNIC.get();
+		boolean isLeggingsOn = event.player.getItemBySlot(EquipmentSlotType.LEGS).getItem() == ItemInit.ZORA_ARMOR_LEGGINGS.get();
+		boolean isBootsOn = event.player.getItemBySlot(EquipmentSlotType.FEET).getItem() == ItemInit.ZORA_ARMOR_BOOTS.get();
+		if(isHelmetOn&isChestplateOn&isLeggingsOn&isBootsOn)
+		{
+			if (event.player.isInWater())
+			{
+
+				if(event.player.isInWater()&&event.player.isSprinting())
+				{
+					// +50%
+					addOrReplaceModifier(event.player, ForgeMod.SWIM_SPEED.get(), ZORA_ARMOR_MODIFIER_ID, 0.5F, AttributeModifier.Operation.MULTIPLY_TOTAL);
+				}
+				else
+				{
+					removeModifier(event.player, ForgeMod.SWIM_SPEED.get(), ZORA_ARMOR_MODIFIER_ID);
+				}
+			}
+		}
+		else
+		{
+			removeModifier(event.player, ForgeMod.SWIM_SPEED.get(), ZORA_ARMOR_MODIFIER_ID);
+		}
+	}
+
+	private static void removeModifier(PlayerEntity player, Attribute attribute, UUID id)
+	{
+		ModifiableAttributeInstance attributeInstance = player.getAttribute(attribute);
+		AttributeModifier modifier = attributeInstance.getModifier(id);
+
+		if (modifier != null)
+		{
+			attributeInstance.removeModifier(modifier);
+		}
+	}
+
+	private static void addOrReplaceModifier(PlayerEntity player, Attribute attribute, UUID id, float amount, AttributeModifier.Operation operation)
+	{
+		ModifiableAttributeInstance attributeInstance = player.getAttribute(attribute);
+		AttributeModifier modifier = attributeInstance.getModifier(id);
+
+		if (modifier != null && modifier.getAmount() != amount)
+		{
+			attributeInstance.removeModifier(modifier);
+			modifier = new AttributeModifier(id, id.toString(), amount, operation);
+		}
+		else if (modifier == null)
+		{
+			modifier = new AttributeModifier(id, id.toString(), amount, operation);
+		}
+
+		if (modifier != null && !attributeInstance.hasModifier(modifier))
+		{
+			attributeInstance.addPermanentModifier(modifier);
+		}
+	}
 }
