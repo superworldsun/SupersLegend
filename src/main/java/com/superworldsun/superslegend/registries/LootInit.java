0701c556885ae47092dee35782f52dfe986d5745
superworldsun <superworldsun@gmail.com>
2022-12-27 19:57:29 -0500

    Update LootInit, import changed from files moved

--- src/main/java/com/superworldsun/superslegend/registries/LootInit.java
+++ src/main/java/com/superworldsun/superslegend/registries/LootInit.java
@@ -1,7 +1,8 @@
 package com.superworldsun.superslegend.registries;
 
 import com.superworldsun.superslegend.SupersLegendMain;
-import com.superworldsun.superslegend.loot.*;
+import com.superworldsun.superslegend.loot.AddLootModifier;
+import com.superworldsun.superslegend.loot.ReplaceLootModifier;
 
 import net.minecraftforge.common.loot.GlobalLootModifierSerializer;
 import net.minecraftforge.fml.RegistryObject;
