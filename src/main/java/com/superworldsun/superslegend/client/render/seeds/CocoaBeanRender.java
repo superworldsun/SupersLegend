4a29db0071eb3134f0771fd0ca51b7492214183b
Daripher <saelver.serbek@gmail.com>
2022-12-30 06:41:05 +0500

    Seeds renderers code formatting

--- src/main/java/com/superworldsun/superslegend/client/render/seeds/CocoaBeanRender.java
+++ src/main/java/com/superworldsun/superslegend/client/render/seeds/CocoaBeanRender.java
@@ -6,12 +6,10 @@ import com.superworldsun.superslegend.entities.projectiles.seeds.CocoaBeanEntity
 import net.minecraft.client.renderer.entity.EntityRendererManager;
 import net.minecraft.util.ResourceLocation;
 
-public class CocoaBeanRender extends SeedRenderer<CocoaBeanEntity>
-{
+public class CocoaBeanRender extends SeedRenderer<CocoaBeanEntity> {
 	private static final ResourceLocation TEXTURE = new ResourceLocation(SupersLegendMain.MOD_ID, "textures/entity/seeds/cocoa_bean.png");
-	
-	public CocoaBeanRender(EntityRendererManager renderManager)
-	{
+
+	public CocoaBeanRender(EntityRendererManager renderManager) {
 		super(renderManager, TEXTURE);
 	}
 }
--- src/main/java/com/superworldsun/superslegend/client/render/seeds/WheatSeedRender.java
+++ src/main/java/com/superworldsun/superslegend/client/render/seeds/WheatSeedRender.java
@@ -6,12 +6,10 @@ import com.superworldsun.superslegend.entities.projectiles.seeds.WheatSeedEntity
 import net.minecraft.client.renderer.entity.EntityRendererManager;
 import net.minecraft.util.ResourceLocation;
 
-public class WheatSeedRender extends SeedRenderer<WheatSeedEntity>
-{
+public class WheatSeedRender extends SeedRenderer<WheatSeedEntity> {
 	private static final ResourceLocation TEXTURE = new ResourceLocation(SupersLegendMain.MOD_ID, "textures/entity/seeds/wheat_seed.png");
-	
-	public WheatSeedRender(EntityRendererManager renderManager)
-	{
+
+	public WheatSeedRender(EntityRendererManager renderManager) {
 		super(renderManager, TEXTURE);
 	}
 }
