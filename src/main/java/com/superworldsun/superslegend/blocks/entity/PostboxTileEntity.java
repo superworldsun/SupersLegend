0a1d00fafa53c3fbe8a930fc64d416e3fabd6f8f
Daripher <saelver.serbek@gmail.com>
2023-01-04 22:53:15 +0500

    Postboxes can now be closed when using postman's hat

--- src/main/java/com/superworldsun/superslegend/blocks/entity/PostboxTileEntity.java
+++ src/main/java/com/superworldsun/superslegend/blocks/entity/PostboxTileEntity.java
@@ -3,43 +3,100 @@ package com.superworldsun.superslegend.blocks.entity;
 import com.superworldsun.superslegend.container.PostboxContainer;
 import com.superworldsun.superslegend.inventory.PostboxInventory;
 import com.superworldsun.superslegend.registries.BlockInit;
+import com.superworldsun.superslegend.registries.ItemInit;
 import com.superworldsun.superslegend.registries.TileEntityInit;
 
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.inventory.InventoryHelper;
 import net.minecraft.inventory.container.Container;
 import net.minecraft.inventory.container.INamedContainerProvider;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.tileentity.TileEntityType;
 import net.minecraft.tileentity.TileEntityType.Builder;
-import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.Hand;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvents;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.fml.network.NetworkHooks;
+import top.theillusivec4.curios.api.CuriosApi;
 
 public class PostboxTileEntity extends SyncableTileEntity implements INamedContainerProvider {
-	public final PostboxInventory inventory = new PostboxInventory(this);
+	private final PostboxInventory inventory = new PostboxInventory(this);
+	private boolean isLocked;
 
 	public PostboxTileEntity() {
 		super(TileEntityInit.POSTBOX.get());
 	}
 
-	@Override
-	public CompoundNBT save(CompoundNBT compound) {
-		compound.put("inventory", inventory.save(new CompoundNBT()));
-		return super.save(compound);
+	public void dropInventoryContents() {
+		InventoryHelper.dropContents(level, getBlockPos(), inventory);
 	}
 
-	@Override
-	public void load(BlockState state, CompoundNBT compound) {
-		inventory.load(compound.getCompound("inventory"));
-		super.load(state, compound);
+	public void interact(ServerPlayerEntity player, Hand hand) {
+		if (player.isCrouching()) {
+			if (isPlayerPostman(player)) {
+				toggleLockedState(player);
+			}
+		} else {
+			if (canBeOpenedByPlayer(player)) {
+				NetworkHooks.openGui(player, this, packetBuffer -> packetBuffer.writeBlockPos(getBlockPos()));
+			} else {
+				ItemStack itemInHand = player.getItemInHand(hand);
+				addItemIntoInventoryIfPossible(itemInHand);
+			}
+		}
 	}
 
-	public void openGui(BlockPos blockPos, PlayerEntity player) {
-		NetworkHooks.openGui((ServerPlayerEntity) player, this, packetBuffer -> packetBuffer.writeBlockPos(blockPos));
+	private void addItemIntoInventoryIfPossible(ItemStack itemInHand) {
+		if (itemInHand.isEmpty()) {
+			return;
+		}
+
+		for (int i = 0; i < inventory.getContainerSize(); i++) {
+			ItemStack itemInSlot = inventory.getItem(i);
+
+			if (itemInSlot.isEmpty()) {
+				ItemStack itemCopy = itemInHand.copy();
+				itemCopy.setCount(1);
+				inventory.setItem(i, itemCopy);
+				itemInHand.shrink(1);
+				level.playSound(null, getBlockPos(), SoundEvents.ITEM_PICKUP, SoundCategory.BLOCKS, 1F, 1F);
+				return;
+			}
+
+			boolean isSameItemInSlot = ItemStack.matches(itemInSlot, itemInHand);
+			boolean isStackInSlotFull = itemInSlot.getCount() == itemInSlot.getMaxStackSize();
+
+			if (isSameItemInSlot && !isStackInSlotFull) {
+				itemInSlot.grow(1);
+				itemInHand.shrink(1);
+				level.playSound(null, getBlockPos(), SoundEvents.ITEM_PICKUP, SoundCategory.BLOCKS, 1F, 1F);
+				return;
+			}
+		}
+	}
+
+	private boolean canBeOpenedByPlayer(ServerPlayerEntity player) {
+		return !isLocked || isLocked && isPlayerPostman(player);
+	}
+
+	private boolean isPlayerPostman(ServerPlayerEntity player) {
+		return CuriosApi.getCuriosHelper().findEquippedCurio(ItemInit.MASK_POSTMANSHAT.get(), player).isPresent();
+	}
+
+	private void toggleLockedState(ServerPlayerEntity player) {
+		if (isLocked) {
+			level.playSound(null, player, SoundEvents.IRON_DOOR_OPEN, SoundCategory.BLOCKS, 1F, 1F);
+		} else {
+			level.playSound(null, player, SoundEvents.IRON_DOOR_CLOSE, SoundCategory.BLOCKS, 1F, 1F);
+		}
+
+		isLocked ^= true;
 	}
 
 	@Override
@@ -51,6 +108,20 @@ public class PostboxTileEntity extends SyncableTileEntity implements INamedConta
 	public ITextComponent getDisplayName() {
 		String blockDescriptionId = level.getBlockState(getBlockPos()).getBlock().getDescriptionId();
 		return new TranslationTextComponent(blockDescriptionId);
+	}
+
+	@Override
+	public CompoundNBT save(CompoundNBT nbt) {
+		nbt.put("inventory", inventory.save(new CompoundNBT()));
+		nbt.putBoolean("locked", isLocked);
+		return super.save(nbt);
+	}
+
+	@Override
+	public void load(BlockState state, CompoundNBT nbt) {
+		inventory.load(nbt.getCompound("inventory"));
+		isLocked = nbt.getBoolean("locked");
+		super.load(state, nbt);
 	}
 
 	public static TileEntityType<PostboxTileEntity> createType() {
