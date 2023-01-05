a48a7732e66068b5629e2f1297cbd3edc6abe5ea
Daripher <saelver.serbek@gmail.com>
2023-01-05 13:44:16 +0500

    Fixed wrong formatting of bag container titles

--- src/main/java/com/superworldsun/superslegend/items/bags/BagItem.java
+++ src/main/java/com/superworldsun/superslegend/items/bags/BagItem.java
@@ -9,49 +9,44 @@ import net.minecraft.inventory.container.Container;
 import net.minecraft.inventory.container.INamedContainerProvider;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.*;
-import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvents;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.network.NetworkHooks;
 
-public abstract class BagItem extends Item
-{
-	public BagItem(Properties properties)
-	{
+public abstract class BagItem extends Item {
+	public BagItem(Properties properties) {
 		super(properties.stacksTo(1));
 	}
 
 	@Override
-	public ActionResult<ItemStack> use(World worldIn, PlayerEntity playerIn, Hand handIn)
-	{
-		if (!worldIn.isClientSide)
-		{
-			BlockPos currentPos = playerIn.blockPosition();
-			worldIn.playSound(null, currentPos.getX(), currentPos.getY(), currentPos.getZ(), SoundEvents.ARMOR_EQUIP_ELYTRA, SoundCategory.PLAYERS, 1f, 1f);
-			INamedContainerProvider containerProvider = new INamedContainerProvider()
-			{
-				@Override
-				public Container createMenu(int windowId, PlayerInventory playerInventory, PlayerEntity player)
-				{
-					return getContainer(windowId, playerInventory, player, handIn);
-				}
-
-				@Override
-				public ITextComponent getDisplayName()
-				{
-					return playerIn.getItemInHand(handIn).getDisplayName();
-				}
-			};
-
-			NetworkHooks.openGui((ServerPlayerEntity) playerIn, containerProvider, packetBuffer -> packetBuffer.writeEnum(handIn));
+	public ActionResult<ItemStack> use(World world, PlayerEntity player, Hand hand) {
+		if (!world.isClientSide) {
+			NetworkHooks.openGui((ServerPlayerEntity) player, createContainerProvider(player, hand), packetBuffer -> packetBuffer.writeEnum(hand));
+			world.playSound(null, player, SoundEvents.ARMOR_EQUIP_ELYTRA, SoundCategory.PLAYERS, 1f, 1f);
 		}
 
-		return ActionResult.success(playerIn.getItemInHand(handIn));
+		return ActionResult.success(player.getItemInHand(hand));
 	}
 
-	public Container getContainer(int windowId, PlayerInventory playerInventory, PlayerEntity player, Hand handIn)
-	{
+	protected INamedContainerProvider createContainerProvider(PlayerEntity player, Hand hand) {
+		return new INamedContainerProvider() {
+			@Override
+			public Container createMenu(int windowId, PlayerInventory playerInventory, PlayerEntity player) {
+				return createContainer(windowId, playerInventory, player, hand);
+			}
+
+			@Override
+			public ITextComponent getDisplayName() {
+				return player.getItemInHand(hand).getHoverName();
+			}
+		};
+	}
+
+	public Container createContainer(int windowId, PlayerInventory playerInventory, PlayerEntity player, Hand handIn) {
 		return new BagContainer(windowId, player.inventory, handIn);
 	}
 
