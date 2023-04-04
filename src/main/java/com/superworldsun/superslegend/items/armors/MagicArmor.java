aa52311c127bffba1e64235aafdb0a8cb8dced4e
superworldsun <superworldsun@gmail.com>
2023-04-04 04:33:18 -0400

    Updated MagicArmor, When the player takes damage 12 rupees are immediately consumed.

--- src/main/java/com/superworldsun/superslegend/items/armors/MagicArmor.java
+++ src/main/java/com/superworldsun/superslegend/items/armors/MagicArmor.java
@@ -25,11 +25,16 @@ import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE, modid = SupersLegendMain.MOD_ID)
 public class MagicArmor extends NonEnchantArmor
 {
 	private static final Map<EquipmentSlotType, BipedModel<?>> MODELS_CACHE = new HashMap<>();
-	
+
+	//TODO make it so when the player has 0 rupees it changes the texture of the armor
 	public MagicArmor(EquipmentSlotType slot, Properties properties)
 	{
 		super(ArmourInit.MAGIC, slot, properties);
@@ -44,87 +49,84 @@ public class MagicArmor extends NonEnchantArmor
 		list.add(new StringTextComponent(TextFormatting.GREEN + "Grants invicibility from rupee"));
 		list.add(new StringTextComponent(TextFormatting.RED + "Slows user when rupee are depleted"));
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	@Override
 	public <M extends BipedModel<?>> M getArmorModel(LivingEntity entityLiving, ItemStack itemStack, EquipmentSlotType armorSlot, M _default)
-	{		
+	{
 		if (!MODELS_CACHE.containsKey(armorSlot))
 		{
 			MODELS_CACHE.put(armorSlot, new MagicArmorModel(armorSlot));
 		}
-		
+
 		return (M) MODELS_CACHE.get(armorSlot);
 	}
-	
+
 	@Override
 	public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type)
 	{
 		return SupersLegendMain.MOD_ID + ":textures/models/armor/magic_armor.png";
 	}
-	
-	@Override
-	public void onArmorTick(ItemStack stack, World world, PlayerEntity player)
-	{
-		if (!world.isClientSide)
-		{
+
+	//When the player takes damage it takes 12 rupees from the players inventory
+	//TODO Add some sound effect for the damage
+	@SubscribeEvent
+	public static void onLivingHurt(LivingHurtEvent event) {
+		if (event.getEntity() instanceof PlayerEntity) {
+			PlayerEntity player = (PlayerEntity) event.getEntity();
 			boolean isHelmetOn = player.getItemBySlot(EquipmentSlotType.HEAD).getItem().getItem() == ItemInit.MAGIC_ARMOR_CAP.get();
 			boolean isChestplateOn = player.getItemBySlot(EquipmentSlotType.CHEST).getItem().getItem() == ItemInit.MAGIC_ARMOR_TUNIC.get();
 			boolean isLeggingsOn = player.getItemBySlot(EquipmentSlotType.LEGS).getItem().getItem() == ItemInit.MAGIC_ARMOR_LEGGINGS.get();
 			boolean isBootsOn = player.getItemBySlot(EquipmentSlotType.FEET).getItem().getItem() == ItemInit.MAGIC_ARMOR_BOOTS.get();
-			if (isHelmetOn & isChestplateOn & isLeggingsOn & isBootsOn)
-			{
-				EffectInstance effect = player.getEffect(Effects.DAMAGE_RESISTANCE);
-				if (effect != null && effect.getAmplifier() == 100)
-					
-					player.addEffect(new EffectInstance(Effect.byId(11), 14, 99, false, false));
-				else
-				{
-					for (int i = 0; i < player.inventory.getContainerSize(); ++i)
-					{
-						ItemStack armorStack = player.inventory.getItem(i);
-						if (armorStack.getItem().getItem() == ItemInit.RUPEE.get())
-						{
-							armorStack.shrink(1);
-							player.addEffect(new EffectInstance(Effects.DAMAGE_RESISTANCE, 10, 100, false, false, false));
-							break;
+			if (isHelmetOn && isChestplateOn && isLeggingsOn && isBootsOn) {
+				int rupeesTaken = 0;
+				for (int i = 0; i < player.inventory.getContainerSize() && rupeesTaken < 12; i++) {
+					ItemStack armorStack = player.inventory.getItem(i);
+					if (armorStack.getItem().getItem() == ItemInit.RUPEE.get()) {
+						int stackSize = armorStack.getCount();
+						if (stackSize >= 12 - rupeesTaken) {
+							armorStack.shrink(12 - rupeesTaken);
+							rupeesTaken = 12;
+						} else {
+							armorStack.setCount(0);
+							rupeesTaken += stackSize;
 						}
 					}
 				}
 			}
-			
-			if (isHelmetOn && isChestplateOn && isLeggingsOn && isBootsOn)
-			{
-				EffectInstance effect = player.getEffect(Effects.DAMAGE_RESISTANCE);
-				if (effect == null)
-				{
-					player.addEffect(new EffectInstance(Effects.MOVEMENT_SLOWDOWN, 10, 3, false, false, false));
-				}
-			}
-			
-			/*
-			 * if (isHelmetOn && isChestplateOn && isLeggingsOn && isBootsOn) {
-			 * if (player.attackEntityFrom(DamageSource.GENERIC, 1) ) {
-			 * 
-			 * BlockPos currentPos = player.getPosition(); world.playSound(null,
-			 * currentPos.getX(), currentPos.getY(), currentPos.getZ(),
-			 * SoundInit.JAWA, SoundCategory.PLAYERS, 1f, 1f);
-			 * 
-			 * for (int i = 0; i < player.inventory.getSizeInventory(); ++i) {
-			 * ItemStack armorStack = player.inventory.getStackInSlot(i); if
-			 * (armorStack.getItem() == ItemList.rupee) { armorStack.shrink(12);
-			 * break; } } } }
-			 */
 		}
 	}
+
+	//Slowly takes a rupee per 10 ticks to give invincibility, when no rupees the player becomes slow and cant walk.
+	@Override
+	public void onArmorTick(ItemStack stack, World world, PlayerEntity player) {
+		if (!world.isClientSide) {
+			boolean isHelmetOn = player.getItemBySlot(EquipmentSlotType.HEAD).getItem().getItem() == ItemInit.MAGIC_ARMOR_CAP.get();
+			boolean isChestplateOn = player.getItemBySlot(EquipmentSlotType.CHEST).getItem().getItem() == ItemInit.MAGIC_ARMOR_TUNIC.get();
+			boolean isLeggingsOn = player.getItemBySlot(EquipmentSlotType.LEGS).getItem().getItem() == ItemInit.MAGIC_ARMOR_LEGGINGS.get();
+			boolean isBootsOn = player.getItemBySlot(EquipmentSlotType.FEET).getItem().getItem() == ItemInit.MAGIC_ARMOR_BOOTS.get();
+			if (isHelmetOn & isChestplateOn & isLeggingsOn & isBootsOn) {
+				EffectInstance effect = player.getEffect(Effects.DAMAGE_RESISTANCE);
+				if (effect != null && effect.getAmplifier() == 100)
+					player.addEffect(new EffectInstance(Effect.byId(11), 14, 99, false, false));
+				else {
+					boolean hasRupees = false;
+					for (int i = 0; i < player.inventory.getContainerSize(); ++i) {
+						ItemStack armorStack = player.inventory.getItem(i);
+						if (armorStack.getItem().getItem() == ItemInit.RUPEE.get()) {
+							armorStack.shrink(1);
+							player.addEffect(new EffectInstance(Effects.DAMAGE_RESISTANCE, 10, 100, false, false, false));
+							hasRupees = true;
+							break;
+						}
+					}
+					if (!hasRupees) {
+						player.addEffect(new EffectInstance(Effects.MOVEMENT_SLOWDOWN, 10, 3, false, false, false));
+					}
+				}
+			}
+		}
+	}
+
+
 }
-
-// EffectInstance effect = player.getActivePotionEffect(Effects.RESISTANCE);
-// if (effect != null && effect.getAmplifier() == 100)
-
-// player.getMotion().getX() != 0 && player.getMotion().getY() != 0
-
-// @Override
-// public boolean hasEffect(ItemStack stack) {
-// return false;
-// }
