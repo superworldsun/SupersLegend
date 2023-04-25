f4466bf13da4dacd421494479c7f919cab74f594
Daripher <saelver.serbek@gmail.com>
2023-04-25 19:19:11 +0500

    Fixed locked dungeon door not working

--- src/main/java/com/superworldsun/superslegend/blocks/LockedDungeonDoor.java
+++ src/main/java/com/superworldsun/superslegend/blocks/LockedDungeonDoor.java
@@ -1,11 +1,20 @@
 package com.superworldsun.superslegend.blocks;
 
+import com.superworldsun.superslegend.registries.BlockInit;
 import com.superworldsun.superslegend.registries.ItemInit;
 import com.superworldsun.superslegend.registries.SoundInit;
-import net.minecraft.block.*;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.DoorBlock;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.*;
+import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.state.properties.DoubleBlockHalf;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Hand;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvent;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.world.World;
@@ -15,51 +24,52 @@ public class LockedDungeonDoor extends DoorBlock {
 		super(properties);
 	}
 
+//	@Override
+//	public ActionResultType use(BlockState state, World worldIn, BlockPos pos, PlayerEntity player, Hand handIn, BlockRayTraceResult hit) {
+//		ItemStack heldItem = player.getItemInHand(handIn);
+//		boolean holdingKey = heldItem.getItem() == ItemInit.SMALL_KEY.get() || heldItem.getItem() == ItemInit.MAGICAL_KEY.get();
+//		if (holdingKey) {
+//			SoundEvent sound = SoundInit.DOOR_UNLOCKED.get();
+//			worldIn.playSound(player, pos, sound, SoundCategory.BLOCKS, 1.0F, 1.0F);
+//			// Replace locked door with unlocked door
+//			if (!worldIn.isClientSide) {
+//				worldIn.destroyBlock(pos, false);
+//				if (!player.isCreative()) {
+//					heldItem.shrink(1);
+//				}
+//				return ActionResultType.SUCCESS;
+//			}
+//		}
+//		return ActionResultType.PASS;
+//
+//	}
+
 	@Override
 	public ActionResultType use(BlockState state, World worldIn, BlockPos pos, PlayerEntity player, Hand handIn, BlockRayTraceResult hit) {
 		ItemStack heldItem = player.getItemInHand(handIn);
-		if (heldItem.getItem() == ItemInit.SMALL_KEY.get()) {
-			SoundEvent sound = SoundInit.DOOR_UNLOCKED.get();
-			worldIn.playSound(player, pos, sound, SoundCategory.BLOCKS, 1.0F, 1.0F);
-			// Replace locked door with unlocked door
-			if (!worldIn.isClientSide) {
-				worldIn.destroyBlock(pos, false);
-				if (!player.isCreative()) {
-					heldItem.shrink(1);
-				}
-				return ActionResultType.SUCCESS;
-			}
-		}
-		if (heldItem.getItem() == ItemInit.MAGICAL_KEY.get()) {
-			SoundEvent sound = SoundInit.DOOR_UNLOCKED.get();
-			worldIn.playSound(player, pos, sound, SoundCategory.BLOCKS, 1.0F, 1.0F);
-			// Replace locked door with unlocked door
-			if (!worldIn.isClientSide) {
-				worldIn.destroyBlock(pos, false);
-				return ActionResultType.SUCCESS;
-			}
-		}
-			return ActionResultType.PASS;
-
-	}
-
-	//TODO Try and make it so when the player right clicks a locked door with a key, the door turns into a unlocked door instead of breaking
-	/*@Override
-	public ActionResultType use(BlockState state, World worldIn, BlockPos pos, PlayerEntity player, Hand handIn, BlockRayTraceResult hit) {
-		ItemStack heldItem = player.getItemInHand(handIn);
-		if (heldItem.getItem() == ItemInit.SMALL_KEY.get() || heldItem.getItem() == ItemInit.MAGICAL_KEY.get()) {
-			SoundEvent sound = SoundInit.DOOR_UNLOCKED.get();
-			worldIn.playSound(player, pos, sound, SoundCategory.BLOCKS, 1.0F, 1.0F);
-			// Replace locked door with unlocked door
+		boolean holdingKey = heldItem.getItem() == ItemInit.SMALL_KEY.get() || heldItem.getItem() == ItemInit.MAGICAL_KEY.get();
+		if (holdingKey) {
+			SoundEvent unlockSound = SoundInit.DOOR_UNLOCKED.get();
+			worldIn.playSound(player, pos, unlockSound, SoundCategory.BLOCKS, 1.0F, 1.0F);
 			if (!worldIn.isClientSide) {
 				BlockState unlockedDoorState = BlockInit.DUNGEON_DOOR.get().defaultBlockState()
 						.setValue(BlockStateProperties.DOUBLE_BLOCK_HALF, state.getValue(BlockStateProperties.DOUBLE_BLOCK_HALF))
-						.setValue(BlockStateProperties.FACING, state.getValue(BlockStateProperties.FACING))
-						.setValue(BlockStateProperties.OPEN, state.getValue(BlockStateProperties.OPEN))
+						.setValue(BlockStateProperties.HORIZONTAL_FACING, state.getValue(BlockStateProperties.HORIZONTAL_FACING))
+						.setValue(BlockStateProperties.OPEN, false)
 						.setValue(BlockStateProperties.POWERED, state.getValue(BlockStateProperties.POWERED))
-						.setValue(BlockStateProperties.DOOR_HINGE, state.getValue(BlockStateProperties.DOOR_HINGE))
-						.setValue(BlockStateProperties.WATERLOGGED, state.getValue(BlockStateProperties.WATERLOGGED));
-				worldIn.setBlock(pos, unlockedDoorState, 3);
+						.setValue(BlockStateProperties.DOOR_HINGE, state.getValue(BlockStateProperties.DOOR_HINGE));
+				worldIn.setBlockAndUpdate(pos, Blocks.AIR.defaultBlockState());
+				if (state.getValue(BlockStateProperties.DOUBLE_BLOCK_HALF) == DoubleBlockHalf.LOWER) {
+					worldIn.setBlockAndUpdate(pos.above(), Blocks.AIR.defaultBlockState());
+				} else {
+					worldIn.setBlockAndUpdate(pos.below(), Blocks.AIR.defaultBlockState());
+				}
+				worldIn.setBlockAndUpdate(pos, unlockedDoorState);
+				if (state.getValue(BlockStateProperties.DOUBLE_BLOCK_HALF) == DoubleBlockHalf.LOWER) {
+					worldIn.setBlockAndUpdate(pos.above(), unlockedDoorState.setValue(BlockStateProperties.DOUBLE_BLOCK_HALF, DoubleBlockHalf.UPPER));
+				} else {
+					worldIn.setBlockAndUpdate(pos.below(), unlockedDoorState.setValue(BlockStateProperties.DOUBLE_BLOCK_HALF, DoubleBlockHalf.LOWER));
+				}
 				if (!player.isCreative()) {
 					heldItem.shrink(1);
 				}
@@ -67,6 +77,6 @@ public class LockedDungeonDoor extends DoorBlock {
 			}
 		}
 		return ActionResultType.PASS;
-	}*/
+	}
 
 }
