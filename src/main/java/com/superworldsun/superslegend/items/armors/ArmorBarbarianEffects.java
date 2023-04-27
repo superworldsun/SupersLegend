42275f28710dd7fcb9ed60e9265f67e3d42a4304
superworldsun <superworldsun@gmail.com>
2023-04-26 20:56:21 -0400

    Update ArmorBarbarianEffects to add new armor model

--- src/main/java/com/superworldsun/superslegend/items/armors/ArmorBarbarianEffects.java
+++ src/main/java/com/superworldsun/superslegend/items/armors/ArmorBarbarianEffects.java
@@ -1,9 +1,14 @@
 package com.superworldsun.superslegend.items.armors;
 
+import com.superworldsun.superslegend.SupersLegendMain;
+import com.superworldsun.superslegend.client.model.armor.*;
 import com.superworldsun.superslegend.items.custom.NonEnchantArmor;
 import com.superworldsun.superslegend.registries.ArmourInit;
 import com.superworldsun.superslegend.registries.ItemInit;
+import net.minecraft.client.renderer.entity.model.BipedModel;
 import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.ItemStack;
@@ -16,12 +21,36 @@ import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ArmorBarbarianEffects extends NonEnchantArmor
 {
-
+    private static final Map<EquipmentSlotType, BipedModel<?>> MODELS_CACHE = new HashMap<>();
 
     public ArmorBarbarianEffects(EquipmentSlotType slot, Properties properties) {
         super(ArmourInit.BARBARIAN, slot, properties);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public <M extends BipedModel<?>> M getArmorModel(LivingEntity entityLiving, ItemStack itemStack, EquipmentSlotType armorSlot, M _default)
+    {
+        if (MODELS_CACHE.isEmpty())
+        {
+            MODELS_CACHE.put(EquipmentSlotType.HEAD, new BarbarianHelmetModel());
+            MODELS_CACHE.put(EquipmentSlotType.CHEST, new BarbarianChestModel());
+            MODELS_CACHE.put(EquipmentSlotType.LEGS, new BarbarianLegWrapsModel());
+            MODELS_CACHE.put(EquipmentSlotType.FEET, new BarbarianBootsModel());
+        }
+
+        return (M) MODELS_CACHE.get(armorSlot);
+    }
+
+    @Override
+    public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type)
+    {
+        return SupersLegendMain.MOD_ID + ":textures/models/armor/barbarian_armor.png";
     }
 
     @OnlyIn(Dist.CLIENT)
