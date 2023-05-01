fcfc985bd3c904398f8ca462754533b08842cb0d
superworldsun <superworldsun@gmail.com>
2023-05-01 06:09:36 -0400

    Update LockedBossDoor

--- src/main/java/com/superworldsun/superslegend/blocks/LockedBossDoor.java
+++ src/main/java/com/superworldsun/superslegend/blocks/LockedBossDoor.java
@@ -1,11 +1,15 @@
 package com.superworldsun.superslegend.blocks;
 
+import com.superworldsun.superslegend.registries.BlockInit;
 import com.superworldsun.superslegend.registries.ItemInit;
 import com.superworldsun.superslegend.registries.SoundInit;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.DoorBlock;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.state.properties.DoubleBlockHalf;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
 import net.minecraft.util.SoundCategory;
@@ -22,19 +26,35 @@ public class LockedBossDoor extends DoorBlock {
 	@Override
 	public ActionResultType use(BlockState state, World worldIn, BlockPos pos, PlayerEntity player, Hand handIn, BlockRayTraceResult hit) {
 		ItemStack heldItem = player.getItemInHand(handIn);
-		if (heldItem.getItem() == ItemInit.BOSS_KEY.get()) {
-			SoundEvent sound = SoundInit.DOOR_UNLOCKED.get();
-			worldIn.playSound(player, pos, sound, SoundCategory.BLOCKS, 1.0F, 1.0F);
-			// Replace locked door with unlocked door
+		boolean holdingKey = heldItem.getItem() == ItemInit.BOSS_KEY.get();
+		if (holdingKey) {
+			SoundEvent unlockSound = SoundInit.DOOR_UNLOCKED.get();
+			worldIn.playSound(player, pos, unlockSound, SoundCategory.BLOCKS, 1.0F, 1.0F);
 			if (!worldIn.isClientSide) {
-				worldIn.destroyBlock(pos, false);
+				BlockState unlockedDoorState = BlockInit.BOSS_DOOR.get().defaultBlockState()
+						.setValue(BlockStateProperties.DOUBLE_BLOCK_HALF, state.getValue(BlockStateProperties.DOUBLE_BLOCK_HALF))
+						.setValue(BlockStateProperties.HORIZONTAL_FACING, state.getValue(BlockStateProperties.HORIZONTAL_FACING))
+						.setValue(BlockStateProperties.OPEN, false)
+						.setValue(BlockStateProperties.POWERED, state.getValue(BlockStateProperties.POWERED))
+						.setValue(BlockStateProperties.DOOR_HINGE, state.getValue(BlockStateProperties.DOOR_HINGE));
+				worldIn.setBlockAndUpdate(pos, Blocks.AIR.defaultBlockState());
+				if (state.getValue(BlockStateProperties.DOUBLE_BLOCK_HALF) == DoubleBlockHalf.LOWER) {
+					worldIn.setBlockAndUpdate(pos.above(), Blocks.AIR.defaultBlockState());
+				} else {
+					worldIn.setBlockAndUpdate(pos.below(), Blocks.AIR.defaultBlockState());
+				}
+				worldIn.setBlockAndUpdate(pos, unlockedDoorState);
+				if (state.getValue(BlockStateProperties.DOUBLE_BLOCK_HALF) == DoubleBlockHalf.LOWER) {
+					worldIn.setBlockAndUpdate(pos.above(), unlockedDoorState.setValue(BlockStateProperties.DOUBLE_BLOCK_HALF, DoubleBlockHalf.UPPER));
+				} else {
+					worldIn.setBlockAndUpdate(pos.below(), unlockedDoorState.setValue(BlockStateProperties.DOUBLE_BLOCK_HALF, DoubleBlockHalf.LOWER));
+				}
 				if (!player.isCreative()) {
 					heldItem.shrink(1);
 				}
 				return ActionResultType.SUCCESS;
 			}
 		}
-			return ActionResultType.PASS;
-
+		return ActionResultType.PASS;
 	}
 }
