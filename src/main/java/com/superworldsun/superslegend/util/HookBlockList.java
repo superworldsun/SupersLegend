3436092a07b408cc7807e2f75a1d767fa7f724a9
superworldsun <superworldsun@gmail.com>
2023-06-26 05:27:17 -0400

    Updated HookBlockList, Hookshots will now grapple all Wood Material blocks instead of a preset list.

--- src/main/java/com/superworldsun/superslegend/util/HookBlockList.java
+++ src/main/java/com/superworldsun/superslegend/util/HookBlockList.java
@@ -4,6 +4,7 @@ import com.superworldsun.superslegend.registries.BlockInit;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.material.Material;
+import net.minecraft.util.registry.Registry;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,46 +18,13 @@ public class HookBlockList {
 
     public static void setHookableBlocks() {
         hookableBlocks.add(BlockInit.GRAPPLE_BLOCK.get());
-        hookableBlocks.add(Blocks.OAK_WOOD);
-        hookableBlocks.add(Blocks.SPRUCE_WOOD);
-        hookableBlocks.add(Blocks.BIRCH_WOOD);
-        hookableBlocks.add(Blocks.JUNGLE_WOOD);
-        hookableBlocks.add(Blocks.ACACIA_WOOD);
-        hookableBlocks.add(Blocks.DARK_OAK_WOOD);
-        hookableBlocks.add(Blocks.CRIMSON_HYPHAE);
-        hookableBlocks.add(Blocks.WARPED_HYPHAE);
-        hookableBlocks.add(Blocks.STRIPPED_OAK_WOOD);
-        hookableBlocks.add(Blocks.STRIPPED_SPRUCE_WOOD);
-        hookableBlocks.add(Blocks.STRIPPED_BIRCH_WOOD);
-        hookableBlocks.add(Blocks.STRIPPED_JUNGLE_WOOD);
-        hookableBlocks.add(Blocks.STRIPPED_ACACIA_WOOD);
-        hookableBlocks.add(Blocks.STRIPPED_DARK_OAK_WOOD);
-        hookableBlocks.add(Blocks.STRIPPED_CRIMSON_HYPHAE);
-        hookableBlocks.add(Blocks.STRIPPED_WARPED_HYPHAE);
-        hookableBlocks.add(Blocks.OAK_LOG);
-        hookableBlocks.add(Blocks.SPRUCE_LOG);
-        hookableBlocks.add(Blocks.BIRCH_LOG);
-        hookableBlocks.add(Blocks.JUNGLE_LOG);
-        hookableBlocks.add(Blocks.ACACIA_LOG);
-        hookableBlocks.add(Blocks.DARK_OAK_LOG);
-        hookableBlocks.add(Blocks.CRIMSON_STEM);
-        hookableBlocks.add(Blocks.WARPED_STEM);
-        hookableBlocks.add(Blocks.STRIPPED_OAK_LOG);
-        hookableBlocks.add(Blocks.STRIPPED_SPRUCE_LOG);
-        hookableBlocks.add(Blocks.STRIPPED_BIRCH_LOG);
-        hookableBlocks.add(Blocks.STRIPPED_JUNGLE_LOG);
-        hookableBlocks.add(Blocks.STRIPPED_ACACIA_LOG);
-        hookableBlocks.add(Blocks.STRIPPED_DARK_OAK_LOG);
-        hookableBlocks.add(Blocks.STRIPPED_CRIMSON_STEM);
-        hookableBlocks.add(Blocks.STRIPPED_WARPED_STEM);
-        hookableBlocks.add(Blocks.OAK_PLANKS);
-        hookableBlocks.add(Blocks.SPRUCE_PLANKS);
-        hookableBlocks.add(Blocks.BIRCH_PLANKS);
-        hookableBlocks.add(Blocks.JUNGLE_PLANKS);
-        hookableBlocks.add(Blocks.ACACIA_PLANKS);
-        hookableBlocks.add(Blocks.DARK_OAK_PLANKS);
-        hookableBlocks.add(Blocks.CRIMSON_PLANKS);
-        hookableBlocks.add(Blocks.WARPED_PLANKS);
 
+
+        // Include blocks with material wood
+        for (Block block : Registry.BLOCK) {
+            if (block.defaultBlockState().getMaterial() == Material.WOOD) {
+                hookableBlocks.add(block);
+            }
+        }
     }
 }
