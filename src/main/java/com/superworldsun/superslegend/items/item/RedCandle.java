6b5f5c964ed172b029bee9348184f4c874f58ce7
superworldsun <superworldsun@gmail.com>
2023-08-09 06:16:35 -0400

    Ported Blue & Red Candle

---
+++ src/main/java/com/superworldsun/superslegend/items/item/RedCandle.java
@@ -0,0 +1,55 @@
+package com.superworldsun.superslegend.items.item;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.BaseFireBlock;
+import net.minecraft.world.level.block.CampfireBlock;
+import net.minecraft.world.level.block.CandleBlock;
+import net.minecraft.world.level.block.CandleCakeBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.gameevent.GameEvent;
+
+public class RedCandle extends Item {
+    public RedCandle(Properties pProperties) {
+        super(pProperties);
+    }
+
+    /**
+     * Called when this item is used when targeting a Block
+     */
+    //TODO, right now this is pretty much just a flint and steel, in future change this to maybe fire a small short range fire projectile that starts fires
+    public InteractionResult useOn(UseOnContext pContext) {
+        Player player = pContext.getPlayer();
+        Level level = pContext.getLevel();
+        BlockPos blockpos = pContext.getClickedPos();
+        BlockState blockstate = level.getBlockState(blockpos);
+        if (!CampfireBlock.canLight(blockstate) && !CandleBlock.canLight(blockstate) && !CandleCakeBlock.canLight(blockstate)) {
+            BlockPos blockpos1 = blockpos.relative(pContext.getClickedFace());
+            if (BaseFireBlock.canBePlacedAt(level, blockpos1, pContext.getHorizontalDirection())) {
+                level.playSound(player, blockpos1, SoundEvents.FIRECHARGE_USE, SoundSource.BLOCKS, 1.0F, level.getRandom().nextFloat() * 0.4F + 0.8F);
+                player.getCooldowns().addCooldown(this, 11);
+                BlockState blockstate1 = BaseFireBlock.getState(level, blockpos1);
+                level.setBlock(blockpos1, blockstate1, 11);
+                level.gameEvent(player, GameEvent.BLOCK_PLACE, blockpos);
+
+                return InteractionResult.sidedSuccess(level.isClientSide());
+            } else {
+                return InteractionResult.FAIL;
+            }
+        } else {
+            level.playSound(player, blockpos, SoundEvents.FIRECHARGE_USE, SoundSource.BLOCKS, 1.0F, level.getRandom().nextFloat() * 0.4F + 0.8F);
+            player.getCooldowns().addCooldown(this, 11);
+            level.setBlock(blockpos, blockstate.setValue(BlockStateProperties.LIT, Boolean.valueOf(true)), 11);
+            level.gameEvent(player, GameEvent.BLOCK_CHANGE, blockpos);
+
+            return InteractionResult.sidedSuccess(level.isClientSide());
+        }
+    }
+}
