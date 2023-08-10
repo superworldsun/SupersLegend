9a8fe7c7969c8542df31d731d881427816429dfa
Daripher <saelver.serbek@gmail.com>
2023-08-11 04:23:17 +0500

    Rings code cleanup

--- src/main/java/com/superworldsun/superslegend/events/ItemEvents.java
+++ src/main/java/com/superworldsun/superslegend/events/ItemEvents.java
@@ -1,31 +1,31 @@
 package com.superworldsun.superslegend.events;
 
 import com.superworldsun.superslegend.SupersLegendMain;
-import com.superworldsun.superslegend.api.DamageReductionItem;
+import com.superworldsun.superslegend.api.IncomingDamageModifier;
 
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import top.theillusivec4.curios.api.CuriosApi;
 
-@Mod.EventBusSubscriber(modid = SupersLegendMain.MOD_ID)
+@EventBusSubscriber(modid = SupersLegendMain.MOD_ID)
 public class ItemEvents {
 	/**
-	 * Applies damage reduction from all instances of {@link DamageReductionItem}
+	 * Applies damage modifiers from all instances of {@link IncomingDamageModifier}
 	 */
 	@SubscribeEvent
 	public static void applyDamageReductionBonus(LivingHurtEvent event) {
-		CuriosApi.getCuriosHelper().findCurios(event.getEntity(), stack -> stack.getItem() instanceof DamageReductionItem).forEach(slot -> {
-			DamageReductionItem item = (DamageReductionItem) slot.stack().getItem();
-			if (item.canReduceDamage(event.getSource())) {
-				event.setAmount(event.getAmount() * (1 - item.getDamageReduction()));
+		CuriosApi.getCuriosHelper().findCurios(event.getEntity(), stack -> stack.getItem() instanceof IncomingDamageModifier).forEach(slot -> {
+			IncomingDamageModifier item = (IncomingDamageModifier) slot.stack().getItem();
+			if (item.canModifyDamage(event.getSource())) {
+				event.setAmount(event.getAmount() * (1 + item.getDamageModifier()));
 			}
 		});
 		event.getEntity().getArmorSlots().forEach(stack -> {
-			if (stack.getItem() instanceof DamageReductionItem) {
-				DamageReductionItem item = (DamageReductionItem) stack.getItem();
-				if (item.canReduceDamage(event.getSource())) {
-					event.setAmount(event.getAmount() * (1 - item.getDamageReduction()));
+			if (stack.getItem() instanceof IncomingDamageModifier) {
+				IncomingDamageModifier item = (IncomingDamageModifier) stack.getItem();
+				if (item.canModifyDamage(event.getSource())) {
+					event.setAmount(event.getAmount() * (1 + item.getDamageModifier()));
 				}
 			}
 		});
