a91eb446d99809504857ae22617dc1dc2bd8afbf
superworldsun <superworldsun@gmail.com>
2023-08-11 06:40:43 -0400

    Ported KokiriArmor Item

---
+++ src/main/java/com/superworldsun/superslegend/items/armors/KokiriArmor.java
@@ -0,0 +1,10 @@
+package com.superworldsun.superslegend.items.armors;
+
+import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.item.ArmorMaterial;
+
+public class KokiriArmor extends ArmorItem {
+    public KokiriArmor(ArmorMaterial material, Type type, Properties properties) {
+        super(material, type, properties);
+    }
+}
