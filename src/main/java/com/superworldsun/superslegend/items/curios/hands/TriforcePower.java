7ed2e90f2b50203958a99874f321b1651f96066a
superworldsun <superworldsun@gmail.com>
2023-08-11 06:41:23 -0400

    Add TriforcePower item, wip

---
+++ src/main/java/com/superworldsun/superslegend/items/curios/hands/TriforcePower.java
@@ -0,0 +1,45 @@
+package com.superworldsun.superslegend.items.curios.hands;
+
+import com.superworldsun.superslegend.SupersLegendMain;
+import com.superworldsun.superslegend.items.customclass.HandsItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = SupersLegendMain.MOD_ID)
+public class TriforcePower extends HandsItem
+{
+
+    public TriforcePower(Properties properties)
+    {
+        super(properties);
+    }
+
+    //TODO eventually make into curio item with abilties
+
+    /*public ActionResult<ItemStack> use(World world, PlayerEntity player, Hand hand)
+    {
+        @SuppressWarnings("unused")
+        ItemStack stack = player.getItemInHand(hand);
+        {
+            BlockPos currentPos = player.blockPosition();
+            world.playSound(null, currentPos.getX(), currentPos.getY(), currentPos.getZ(), SoundEvents.EXPERIENCE_ORB_PICKUP, SoundCategory.PLAYERS, 1f, 1f);
+            //player.addEffect(new EffectInstance(Effect.byId(20), 44, 0, false, false));
+            //player.addEffect(new EffectInstance(Effect.byId(19), 6000, 0, false, false));
+            player.addEffect(new EffectInstance(Effect.byId(5), 6000, 0, false, true));
+            player.addEffect(new EffectInstance(Effect.byId(3), 6000, 0, false, false));
+            player.getCooldowns().addCooldown(this, 100);
+        }
+        return new ActionResult<>(ActionResultType.PASS, player.getItemInHand(hand));
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    @Override
+    public void appendHoverText(ItemStack stack, World world, List<ITextComponent> list, ITooltipFlag flag)
+    {
+        super.appendHoverText(stack, world, list, flag);
+        list.add(new StringTextComponent(TextFormatting.RED + "This gives the you Power to destroy"));
+        list.add(new StringTextComponent(TextFormatting.GREEN + "Right-click to use"));
+    }*/
+}
