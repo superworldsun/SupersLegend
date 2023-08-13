92a22968bda1aabb44064f7d5083970ded20a0cc
superworldsun <superworldsun@gmail.com>
2023-08-13 06:22:30 -0400

    Add ZoraArmor

---
+++ src/main/java/com/superworldsun/superslegend/items/armors/ZoraArmor.java
@@ -0,0 +1,10 @@
+package com.superworldsun.superslegend.items.armors;
+
+import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.item.ArmorMaterial;
+
+public class ZoraArmor extends ArmorItem {
+    public ZoraArmor(ArmorMaterial material, Type type, Properties properties) {
+        super(material, type, properties);
+    }
+}
