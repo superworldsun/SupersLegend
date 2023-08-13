2f07e9dc7b5e1dd1b6e40e91345e71b4dd04e1ab
superworldsun <superworldsun@gmail.com>
2023-08-13 06:21:58 -0400

    Update ArmorInit, now contains all Armor Materials

--- src/main/java/com/superworldsun/superslegend/registries/ArmorInit.java
+++ src/main/java/com/superworldsun/superslegend/registries/ArmorInit.java
@@ -1,73 +1,212 @@
 package com.superworldsun.superslegend.registries;
 
-import com.superworldsun.superslegend.SupersLegendMain;
-import net.minecraft.sounds.SoundEvent;
-import net.minecraft.world.item.ArmorItem;
-import net.minecraft.world.item.ArmorMaterial;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.item.crafting.Ingredient;
 
-import java.util.function.Supplier;
 
+public class ArmorInit {
 
-public class ArmorInit implements ArmorMaterial {
+    //first new Int is Durablity, second is armor defense
+    //HEAD, CHEST, LEGS, FEET
+    public static class ArmorTiers {
+        public static final ArmorMaterialInit KOKIRI = new ArmorMaterialInit(
+                new int[]{0, 0, 0, 0},
+                new int[]{1, 5, 3, 1},
+                0,
+                SoundEvents.ARMOR_EQUIP_LEATHER,
+                () -> Ingredient.of(ItemInit.KOKIRI_TUNIC::get),
+                "kokiri",
+                0.0f,
+                0.0f
+        );
 
-    private final int[] durabilityForType;
-    private final int[] defenseForType;
-    private final int enchantability;
-    private final SoundEvent equipSound;
-    private final Supplier<Ingredient> repairMaterial;
-    private final String name;
-    private final float toughness;
-    private final float knockbackResistance;
+        public static final ArmorMaterialInit ZORA = new ArmorMaterialInit(
+                new int[]{0, 0, 0, 0},
+                new int[]{1, 5, 3, 1},
+                0,
+                SoundEvents.ARMOR_EQUIP_LEATHER,
+                () -> Ingredient.of(ItemInit.KOKIRI_TUNIC::get),
+                "zora",
+                0.0f,
+                0.0f
+        );
 
-    public ArmorInit(int[] durabilityForType, int[] defenseForType, int enchantability, SoundEvent equipSound, Supplier<Ingredient> repairMaterial, String name, float toughness, float knockbackResistance) {
-        this.durabilityForType = durabilityForType;
-        this.defenseForType = defenseForType;
-        this.enchantability = enchantability;
-        this.equipSound = equipSound;
-        this.repairMaterial = repairMaterial;
-        this.name = name;
-        this.toughness = toughness;
-        this.knockbackResistance = knockbackResistance;
+        public static final ArmorMaterialInit GORON = new ArmorMaterialInit(
+                new int[]{0, 0, 0, 0},
+                new int[]{1, 5, 3, 1},
+                0,
+                SoundEvents.ARMOR_EQUIP_LEATHER,
+                () -> Ingredient.of(ItemInit.KOKIRI_TUNIC::get),
+                "goron",
+                0.0f,
+                0.0f
+        );
+
+        public static final ArmorMaterialInit PURPLE = new ArmorMaterialInit(
+                new int[]{0, 0, 0, 0},
+                new int[]{1, 5, 3, 1},
+                0,
+                SoundEvents.ARMOR_EQUIP_LEATHER,
+                () -> Ingredient.of(ItemInit.KOKIRI_TUNIC::get),
+                "purple",
+                0.0f,
+                0.0f
+        );
+
+        public static final ArmorMaterialInit MAGIC = new ArmorMaterialInit(
+                new int[]{0, 0, 0, 0},
+                new int[]{1, 1, 1, 1},
+                0,
+                SoundEvents.ARMOR_EQUIP_LEATHER,
+                () -> Ingredient.of(ItemInit.KOKIRI_TUNIC::get),
+                "magic",
+                0.0f,
+                0.0f
+        );
+
+        public static final ArmorMaterialInit HEROS_NEW = new ArmorMaterialInit(
+                new int[]{0, 0, 0, 0},
+                new int[]{1, 5, 3, 1},
+                0,
+                SoundEvents.ARMOR_EQUIP_LEATHER,
+                () -> Ingredient.of(ItemInit.KOKIRI_TUNIC::get),
+                "heros_new",
+                0.0f,
+                0.0f
+        );
+
+        public static final ArmorMaterialInit DARK = new ArmorMaterialInit(
+                new int[]{0, 0, 0, 0},
+                new int[]{1, 3, 2, 1},
+                0,
+                SoundEvents.ARMOR_EQUIP_LEATHER,
+                () -> Ingredient.of(ItemInit.KOKIRI_TUNIC::get),
+                "dark",
+                0.0f,
+                0.0f
+        );
+
+        public static final ArmorMaterialInit ZORAARMOR = new ArmorMaterialInit(
+                new int[]{0, 0, 0, 0},
+                new int[]{1, 3, 2, 1},
+                0,
+                SoundEvents.ARMOR_EQUIP_LEATHER,
+                () -> Ingredient.of(ItemInit.KOKIRI_TUNIC::get),
+                "zoraarmor",
+                0.0f,
+                0.0f
+        );
+
+        public static final ArmorMaterialInit FLAMEBREAKER = new ArmorMaterialInit(
+                new int[]{0, 0, 0, 0},
+                new int[]{1, 5, 3, 1},
+                0,
+                SoundEvents.ARMOR_EQUIP_LEATHER,
+                () -> Ingredient.of(ItemInit.KOKIRI_TUNIC::get),
+                "flamebreaker",
+                0.0f,
+                0.0f
+        );
+        public static final ArmorMaterialInit ANCIENT = new ArmorMaterialInit(
+                new int[]{0, 0, 0, 0},
+                new int[]{1, 5, 3, 1},
+                0,
+                SoundEvents.ARMOR_EQUIP_LEATHER,
+                () -> Ingredient.of(ItemInit.KOKIRI_TUNIC::get),
+                "ancient",
+                3.0f,
+                0.0f
+        );
+        public static final ArmorMaterialInit BARBARIAN = new ArmorMaterialInit(
+                new int[]{0, 0, 0, 0},
+                new int[]{1, 2, 1, 1},
+                0,
+                SoundEvents.ARMOR_EQUIP_LEATHER,
+                () -> Ingredient.of(ItemInit.KOKIRI_TUNIC::get),
+                "barbarian",
+                0.0f,
+                0.0f
+        );
+        public static final ArmorMaterialInit SNOWQUILL = new ArmorMaterialInit(
+                new int[]{0, 0, 0, 0},
+                new int[]{1, 3, 2, 1},
+                0,
+                SoundEvents.ARMOR_EQUIP_LEATHER,
+                () -> Ingredient.of(ItemInit.KOKIRI_TUNIC::get),
+                "snowquill",
+                0.0f,
+                0.0f
+        );
+        public static final ArmorMaterialInit DESERTVOE = new ArmorMaterialInit(
+                new int[]{0, 0, 0, 0},
+                new int[]{1, 3, 2, 1},
+                0,
+                SoundEvents.ARMOR_EQUIP_LEATHER,
+                () -> Ingredient.of(ItemInit.KOKIRI_TUNIC::get),
+                "desertvoe",
+                0.0f,
+                0.0f
+        );
+        public static final ArmorMaterialInit CLIMBING = new ArmorMaterialInit(
+                new int[]{0, 0, 0, 0},
+                new int[]{1, 3, 2, 1},
+                0,
+                SoundEvents.ARMOR_EQUIP_LEATHER,
+                () -> Ingredient.of(ItemInit.KOKIRI_TUNIC::get),
+                "climbing",
+                0.0f,
+                0.0f
+        );
+        public static final ArmorMaterialInit FLIPPERS = new ArmorMaterialInit(
+                new int[]{0, 0, 0, 0},
+                new int[]{0, 0, 0, 1},
+                0,
+                SoundEvents.ARMOR_EQUIP_LEATHER,
+                () -> Ingredient.of(ItemInit.KOKIRI_TUNIC::get),
+                "kokiri",
+                0.0f,
+                0.0f
+        );
+        public static final ArmorMaterialInit ROCSCAPE = new ArmorMaterialInit(
+                new int[]{0, 0, 0, 0},
+                new int[]{0, 2, 0, 0},
+                0,
+                SoundEvents.ARMOR_EQUIP_LEATHER,
+                () -> Ingredient.of(ItemInit.KOKIRI_TUNIC::get),
+                "kokiri",
+                0.0f,
+                0.0f
+        );
+        public static final ArmorMaterialInit HOVER_BOOTS = new ArmorMaterialInit(
+                new int[]{0, 0, 0, 0},
+                new int[]{0, 0, 0, 1},
+                0,
+                SoundEvents.ARMOR_EQUIP_LEATHER,
+                () -> Ingredient.of(ItemInit.KOKIRI_TUNIC::get),
+                "kokiri",
+                0.0f,
+                0.0f
+        );
+        public static final ArmorMaterialInit IRON_BOOTS = new ArmorMaterialInit(
+                new int[]{0, 0, 0, 0},
+                new int[]{0, 0, 0, 1},
+                0,
+                SoundEvents.ARMOR_EQUIP_LEATHER,
+                () -> Ingredient.of(ItemInit.KOKIRI_TUNIC::get),
+                "kokiri",
+                0.0f,
+                0.0f
+        );
+        public static final ArmorMaterialInit PEGASUS_BOOTS = new ArmorMaterialInit(
+                new int[]{0, 0, 0, 0},
+                new int[]{0, 0, 0, 1},
+                0,
+                SoundEvents.ARMOR_EQUIP_LEATHER,
+                () -> Ingredient.of(ItemInit.KOKIRI_TUNIC::get),
+                "kokiri",
+                0.0f,
+                0.0f
+        );
     }
 
-    @Override
-    public int getDurabilityForType(ArmorItem.Type type) {
-        return this.durabilityForType[type.ordinal()];
-    }
-
-    @Override
-    public int getDefenseForType(ArmorItem.Type type) {
-        return this.defenseForType[type.ordinal()];
-    }
-
-    @Override
-    public int getEnchantmentValue() {
-        return this.enchantability;
-    }
-
-    @Override
-    public SoundEvent getEquipSound() {
-        return this.equipSound;
-    }
-
-    @Override
-    public Ingredient getRepairIngredient() {
-        return this.repairMaterial.get();
-    }
-
-    @Override
-    public String getName() {
-        return SupersLegendMain.MOD_ID + ":" + this.name;
-    }
-
-    @Override
-    public float getToughness() {
-        return this.toughness;
-    }
-
-    @Override
-    public float getKnockbackResistance() {
-        return this.knockbackResistance;
-    }
 }
