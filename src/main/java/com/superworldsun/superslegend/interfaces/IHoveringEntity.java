2572b8bb4d799bb60e95e790ea045fbb44837d32
superworldsun <superworldsun@gmail.com>
2023-08-16 05:55:49 -0400

    Add IHoveringEntity Interface

---
+++ src/main/java/com/superworldsun/superslegend/interfaces/IHoveringEntity.java
@@ -0,0 +1,18 @@
+package com.superworldsun.superslegend.interfaces;
+
+public interface IHoveringEntity
+{
+    int increaseHoverTime();
+
+    int getHoverTime();
+
+    void setHoverTime(int time);
+
+    void setHoverHeight(int height);
+
+    int getHoverHeight();
+
+    void setJumpedFromBlock(boolean state);
+
+    boolean jumpedFromBlock();
+}
