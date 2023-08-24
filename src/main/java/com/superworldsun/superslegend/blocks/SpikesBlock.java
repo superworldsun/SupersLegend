22e75750091c9f8b8850a1dc0882cdd6eaa7b7c7
superworldsun <superworldsun@gmail.com>
2023-08-23 22:40:31 -0400

    Ported SpikesBlock

---
+++ src/main/java/com/superworldsun/superslegend/blocks/SpikesBlock.java
@@ -0,0 +1,40 @@
+package com.superworldsun.superslegend.blocks;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.pathfinder.PathComputationType;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
+
+public class SpikesBlock extends Block
+
+{
+    protected static final VoxelShape SHAPE = Block.box(0.0D, 0.0D, 0.0D, 16.0D, 12.0D, 16.0D);
+
+    public SpikesBlock(Properties properties) {
+        super(properties);
+    }
+
+    public VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
+        return SHAPE;
+    }
+
+    @Override
+    public void entityInside(BlockState pState, Level level, BlockPos pPos, Entity entity) {
+        entity.hurt(level.damageSources().cactus(), 6.0F);
+    }
+
+    @Override
+    public boolean isPathfindable(BlockState pState, BlockGetter pLevel, BlockPos pPos, PathComputationType pType) {
+        return false;
+    }
+
+    @Override
+    public boolean isPossibleToRespawnInThis(BlockState pState) {
+        return false;
+    }
+}
