bd8b428fc671ea2999cc429f1b386f748d46733a
superworldsun <superworldsun@gmail.com>
2023-08-29 04:01:11 -0400

    Ported ShadowModelBlock

---
+++ src/main/java/com/superworldsun/superslegend/blocks/ShadowModelBlock.java
@@ -0,0 +1,10 @@
+package com.superworldsun.superslegend.blocks;
+
+import net.minecraft.world.level.block.Block;
+
+public class ShadowModelBlock extends Block
+{
+    public ShadowModelBlock(Properties pProperties) {
+        super(pProperties);
+    }
+}
