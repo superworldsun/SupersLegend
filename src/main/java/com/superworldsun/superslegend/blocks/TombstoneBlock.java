05f2eb90d632b85660665d9b94be2964e946f532
superworldsun <superworldsun@gmail.com>
2023-08-29 04:01:40 -0400

    Ported Tombstone

---
+++ src/main/java/com/superworldsun/superslegend/blocks/TombstoneBlock.java
@@ -0,0 +1,99 @@
+package com.superworldsun.superslegend.blocks;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
+import net.minecraft.world.level.block.SimpleWaterloggedBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.*;
+import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.level.material.Fluids;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
+import org.jetbrains.annotations.Nullable;
+
+public class TombstoneBlock extends Block implements SimpleWaterloggedBlock
+{
+    public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
+    public static final EnumProperty<SlabType> TYPE = BlockStateProperties.SLAB_TYPE;
+    public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
+
+    protected static final VoxelShape FACE_EAST_AABB = Block.box(0.0D, 0.0D, 0.0D, 3.0D, 19.0D, 16.0D);
+    protected static final VoxelShape FACE_WEST_AABB = Block.box(13.0D, 0.0D, 0.0D, 16.0D, 19.0D, 16.0D);
+    protected static final VoxelShape FACE_SOUTH_AABB = Block.box(0.0D, 0.0D, 0.0D, 16.0D, 19.0D, 3.0D);
+    protected static final VoxelShape FACE_NORTH_AABB = Block.box(0.0D, 0.0D, 13.0D, 16.0D, 19.0D, 16.0D);
+
+    public TombstoneBlock(Properties properties) {
+        super(properties);
+        this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH));
+        this.registerDefaultState(defaultBlockState().setValue(WATERLOGGED, false));
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState state, BlockGetter worldIn, BlockPos pos, CollisionContext context) {
+        switch((Direction)state.getValue(FACING)) {
+            case EAST:
+            default:
+                return FACE_EAST_AABB;
+            case WEST:
+                return FACE_WEST_AABB;
+            case SOUTH:
+                return FACE_SOUTH_AABB;
+            case NORTH:
+                return FACE_NORTH_AABB;
+        }
+    }
+
+    @Nullable
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context)
+    {
+        return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FACING, TYPE, WATERLOGGED);
+    }
+
+
+    public FluidState getFluidState(BlockState state) {
+        return state.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(state);
+    }
+
+    @Override
+    public BlockState updateShape(BlockState stateIn, Direction facing, BlockState facingState, LevelAccessor levelIn, BlockPos currentPos, BlockPos facingPos) {
+        if (stateIn.getValue(WATERLOGGED)) {
+            levelIn.scheduleTick(currentPos, Fluids.WATER, Fluids.WATER.getTickDelay(levelIn));
+        }
+        return facing == Direction.DOWN && !this.canSurvive(stateIn, levelIn, currentPos) ? Blocks.AIR.defaultBlockState() : super.updateShape(stateIn, facing, facingState, levelIn, currentPos, facingPos);
+    }
+
+    @Override
+    public boolean canSurvive(BlockState state, LevelReader level, BlockPos pos) {
+        return !level.isEmptyBlock(pos.below());
+    }
+
+    @Override
+    public VoxelShape getCollisionShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
+        switch((Direction)pState.getValue(FACING)) {
+            case EAST:
+                return Shapes.box(0,0,0,0.2,1,1);
+            case WEST:
+                return Shapes.box(0.8,0,0,1,1,1);
+            case SOUTH:
+                return Shapes.box(0,0,0,1,1,0.2);
+            case NORTH:
+                return Shapes.box(0,0,0.8,1,1,1);
+        }
+        return Shapes.box(0,0.8,0.8,1,1,1);
+    }
+}
