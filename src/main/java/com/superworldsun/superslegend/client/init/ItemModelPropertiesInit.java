b4c8ef0d382f3ab3b143e6fe9415e1b20a389bb1
superworldsun <superworldsun@gmail.com>
2023-09-13 04:14:38 -0400

    Updated ItemModelPropertiesInit, added fishing rod

--- src/main/java/com/superworldsun/superslegend/client/init/ItemModelPropertiesInit.java
+++ src/main/java/com/superworldsun/superslegend/client/init/ItemModelPropertiesInit.java
@@ -3,6 +3,8 @@ package com.superworldsun.superslegend.client.init;
 import com.superworldsun.superslegend.registries.ItemInit;
 import net.minecraft.client.renderer.item.ItemProperties;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.FishingRodItem;
 import net.minecraft.world.item.Item;
 
 public class ItemModelPropertiesInit {
@@ -15,6 +17,8 @@ public class ItemModelPropertiesInit {
         makeShield(ItemInit.DEKU_SHIELD.get());
         makeShield(ItemInit.HYLIAN_SHIELD.get());
         makeShield(ItemInit.SACRED_SHIELD.get());
+
+        makeFishingrod(ItemInit.FISHING_ROD.get());
     }
 
     private static void makeBow(Item item) {
@@ -32,10 +36,25 @@ public class ItemModelPropertiesInit {
         });
     }
 
-    //TODO, fix shields so they can render their blocking model
     private static void makeShield(Item item) {
         ItemProperties.register(item, new ResourceLocation("blocking"), (p_174575_, p_174576_, p_174577_, p_174578_) -> {
             return p_174577_ != null && p_174577_.isUsingItem() && p_174577_.getUseItem() == p_174575_ ? 1.0F : 0.0F;
+        });
+    }
+
+    private static void makeFishingrod(Item item) {
+        ItemProperties.register(item, new ResourceLocation("cast"), (p_174585_, p_174586_, p_174587_, p_174588_) -> {
+            if (p_174587_ == null) {
+                return 0.0F;
+            } else {
+                boolean flag = p_174587_.getMainHandItem() == p_174585_;
+                boolean flag1 = p_174587_.getOffhandItem() == p_174585_;
+                if (p_174587_.getMainHandItem().getItem() instanceof FishingRodItem) {
+                    flag1 = false;
+                }
+
+                return (flag || flag1) && p_174587_ instanceof Player && ((Player)p_174587_).fishing != null ? 1.0F : 0.0F;
+            }
         });
     }
 }
