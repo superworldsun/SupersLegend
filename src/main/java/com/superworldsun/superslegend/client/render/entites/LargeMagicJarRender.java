d5331a074c1aab573beb9372ee5b5d38bca325a0
superworldsun <superworldsun@gmail.com>
2023-09-13 03:38:02 -0400

    Ported MagicJarEntities

---
+++ src/main/java/com/superworldsun/superslegend/client/render/entites/LargeMagicJarRender.java
@@ -0,0 +1,62 @@
+package com.superworldsun.superslegend.client.render.entites;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.mojang.math.Axis;
+import com.superworldsun.superslegend.SupersLegendMain;
+import com.superworldsun.superslegend.entities.HeartEntity;
+import com.superworldsun.superslegend.entities.LargeMagicJarEntity;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.entity.EntityRenderer;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import org.joml.Matrix3f;
+import org.joml.Matrix4f;
+
+@OnlyIn(Dist.CLIENT)
+public class LargeMagicJarRender extends EntityRenderer<LargeMagicJarEntity> {
+    private static final ResourceLocation TEXTURE_LOCATION = new ResourceLocation(SupersLegendMain.MOD_ID, "textures/entity/large_magic_jar.png");
+    private static final RenderType RENDER_TYPE = RenderType.entityCutoutNoCull(TEXTURE_LOCATION);
+
+    public LargeMagicJarRender(EntityRendererProvider.Context pContext) {
+        super(pContext);
+        this.shadowRadius = 0.15F;
+        this.shadowStrength = 0.75F;
+    }
+
+    public void render(LargeMagicJarEntity pEntity, float pEntityYaw, float pPartialTicks, PoseStack pMatrixStack, MultiBufferSource pBuffer, int pPackedLight) {
+        pMatrixStack.pushPose();
+        //Raise the entity slightly so that it is not cutting through the ground.
+        pMatrixStack.translate(0.0F, 0.1F, 0.0F);
+        //Makes the entity always looking at the camera.
+        pMatrixStack.mulPose(this.entityRenderDispatcher.cameraOrientation());
+        //Rotation
+        pMatrixStack.mulPose(Axis.YP.rotationDegrees(180.0F));
+        //Scale the entity.
+        PoseStack.Pose posestack$pose = pMatrixStack.last();
+        Matrix4f matrix4f = posestack$pose.pose();
+        Matrix3f matrix3f = posestack$pose.normal();
+        //Size of the entity
+        pMatrixStack.scale(0.7F, 0.7F, 0.7F);
+        VertexConsumer vertexconsumer = pBuffer.getBuffer(RENDER_TYPE);
+        vertex(vertexconsumer, matrix4f, matrix3f, pPackedLight, 0.0F, 0, 0, 1);
+        vertex(vertexconsumer, matrix4f, matrix3f, pPackedLight, 1.0F, 0, 1, 1);
+        vertex(vertexconsumer, matrix4f, matrix3f, pPackedLight, 1.0F, 1, 1, 0);
+        vertex(vertexconsumer, matrix4f, matrix3f, pPackedLight, 0.0F, 1, 0, 0);
+        pMatrixStack.popPose();
+        super.render(pEntity, pEntityYaw, pPartialTicks, pMatrixStack, pBuffer, pPackedLight);
+    }
+
+    private static void vertex(VertexConsumer pConsumer, Matrix4f p_254477_, Matrix3f p_253948_, int pLightmapUV, float pX, int pY, int pU, int pV) {
+        pConsumer.vertex(p_254477_, pX - 0.5F, (float)pY - 0.25F, 0.0F).color(255, 255, 255, 255).uv((float)pU, (float)pV).overlayCoords(OverlayTexture.NO_OVERLAY).uv2(pLightmapUV).normal(p_253948_, 0.0F, 1.0F, 0.0F).endVertex();
+    }
+
+    @Override
+    public ResourceLocation getTextureLocation(LargeMagicJarEntity pEntity) {
+        return TEXTURE_LOCATION;
+    }
+}
---
+++ src/main/java/com/superworldsun/superslegend/client/render/entites/MagicJarRender.java
@@ -0,0 +1,63 @@
+package com.superworldsun.superslegend.client.render.entites;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.mojang.math.Axis;
+import com.superworldsun.superslegend.SupersLegendMain;
+import com.superworldsun.superslegend.entities.HeartEntity;
+import com.superworldsun.superslegend.entities.LargeMagicJarEntity;
+import com.superworldsun.superslegend.entities.MagicJarEntity;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.entity.EntityRenderer;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import org.joml.Matrix3f;
+import org.joml.Matrix4f;
+
+@OnlyIn(Dist.CLIENT)
+public class MagicJarRender extends EntityRenderer<MagicJarEntity> {
+    private static final ResourceLocation TEXTURE_LOCATION = new ResourceLocation(SupersLegendMain.MOD_ID, "textures/entity/magic_jar.png");
+    private static final RenderType RENDER_TYPE = RenderType.entityCutoutNoCull(TEXTURE_LOCATION);
+
+    public MagicJarRender(EntityRendererProvider.Context pContext) {
+        super(pContext);
+        this.shadowRadius = 0.15F;
+        this.shadowStrength = 0.75F;
+    }
+
+    public void render(MagicJarEntity pEntity, float pEntityYaw, float pPartialTicks, PoseStack pMatrixStack, MultiBufferSource pBuffer, int pPackedLight) {
+        pMatrixStack.pushPose();
+        //Raise the entity slightly so that it is not cutting through the ground.
+        pMatrixStack.translate(0.0F, 0.1F, 0.0F);
+        //Makes the entity always looking at the camera.
+        pMatrixStack.mulPose(this.entityRenderDispatcher.cameraOrientation());
+        //Rotation
+        pMatrixStack.mulPose(Axis.YP.rotationDegrees(180.0F));
+        //Scale the entity.
+        PoseStack.Pose posestack$pose = pMatrixStack.last();
+        Matrix4f matrix4f = posestack$pose.pose();
+        Matrix3f matrix3f = posestack$pose.normal();
+        //Size of the entity
+        pMatrixStack.scale(0.6F, 0.6F, 0.6F);
+        VertexConsumer vertexconsumer = pBuffer.getBuffer(RENDER_TYPE);
+        vertex(vertexconsumer, matrix4f, matrix3f, pPackedLight, 0.0F, 0, 0, 1);
+        vertex(vertexconsumer, matrix4f, matrix3f, pPackedLight, 1.0F, 0, 1, 1);
+        vertex(vertexconsumer, matrix4f, matrix3f, pPackedLight, 1.0F, 1, 1, 0);
+        vertex(vertexconsumer, matrix4f, matrix3f, pPackedLight, 0.0F, 1, 0, 0);
+        pMatrixStack.popPose();
+        super.render(pEntity, pEntityYaw, pPartialTicks, pMatrixStack, pBuffer, pPackedLight);
+    }
+
+    private static void vertex(VertexConsumer pConsumer, Matrix4f p_254477_, Matrix3f p_253948_, int pLightmapUV, float pX, int pY, int pU, int pV) {
+        pConsumer.vertex(p_254477_, pX - 0.5F, (float)pY - 0.25F, 0.0F).color(255, 255, 255, 255).uv((float)pU, (float)pV).overlayCoords(OverlayTexture.NO_OVERLAY).uv2(pLightmapUV).normal(p_253948_, 0.0F, 1.0F, 0.0F).endVertex();
+    }
+
+    @Override
+    public ResourceLocation getTextureLocation(MagicJarEntity pEntity) {
+        return TEXTURE_LOCATION;
+    }
+}
