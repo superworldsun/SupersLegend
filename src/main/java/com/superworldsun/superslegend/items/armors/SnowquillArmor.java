259842678daa6ea6ff2e4022fa7fade89c8b8d1a
superworldsun <superworldsun@gmail.com>
2023-09-24 07:42:01 -0400

    Ported SnowquillArmor Model

--- src/main/java/com/superworldsun/superslegend/items/armors/SnowquillArmor.java
+++ src/main/java/com/superworldsun/superslegend/items/armors/SnowquillArmor.java
@@ -1,10 +1,58 @@
 package com.superworldsun.superslegend.items.armors;
 
+import com.superworldsun.superslegend.client.render.armor.DesertVoeArmorRenderer;
+import com.superworldsun.superslegend.client.render.armor.SnowQuillArmorRenderer;
 import com.superworldsun.superslegend.items.customclass.NonEnchantArmor;
+import net.minecraft.client.model.HumanoidModel;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ArmorMaterial;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.client.extensions.common.IClientItemExtensions;
+import org.jetbrains.annotations.NotNull;
+import software.bernie.geckolib.animatable.GeoItem;
+import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache;
+import software.bernie.geckolib.core.animatable.instance.SingletonAnimatableInstanceCache;
+import software.bernie.geckolib.core.animation.*;
+import software.bernie.geckolib.core.object.PlayState;
 
-public class SnowquillArmor extends NonEnchantArmor {
+import java.util.function.Consumer;
+
+public class SnowquillArmor extends NonEnchantArmor implements GeoItem {
+    private AnimatableInstanceCache cache = new SingletonAnimatableInstanceCache(this);
     public SnowquillArmor(ArmorMaterial material, Type type, Properties properties) {
         super(material, type, properties);
+    }
+
+    @Override
+    public void initializeClient(Consumer<IClientItemExtensions> consumer) {
+        consumer.accept(new IClientItemExtensions() {
+            private SnowQuillArmorRenderer renderer;
+
+            @Override
+            public @NotNull HumanoidModel<?> getHumanoidArmorModel(LivingEntity livingEntity, ItemStack itemStack,
+                                                                   EquipmentSlot equipmentSlot, HumanoidModel<?> original) {
+                if (this.renderer == null)
+                    this.renderer = new SnowQuillArmorRenderer();
+
+                this.renderer.prepForRender(livingEntity, itemStack, equipmentSlot, original);
+                return this.renderer;
+            }
+        });
+    }
+
+    private PlayState predicate(AnimationState animationState){
+        animationState.getController().setAnimation(RawAnimation.begin().then("idle", Animation.LoopType.LOOP));
+        return PlayState.CONTINUE;
+    }
+
+    @Override
+    public void registerControllers(AnimatableManager.ControllerRegistrar controllerRegistrar) {
+        controllerRegistrar.add(new AnimationController(this, "controller", 0, this::predicate));
+    }
+
+    @Override
+    public AnimatableInstanceCache getAnimatableInstanceCache() {
+        return cache;
     }
 }
