c5e9865d3dda8cb9d8178de46a7acdc103a7f085
superworldsun <superworldsun@gmail.com>
2023-10-01 07:59:45 -0400

    Add PurpleArmor, no model yet

---
+++ src/main/java/com/superworldsun/superslegend/items/armors/PurpleArmor.java
@@ -0,0 +1,39 @@
+package com.superworldsun.superslegend.items.armors;
+
+import com.superworldsun.superslegend.items.customclass.NonEnchantArmor;
+import net.minecraft.client.model.HumanoidModel;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ArmorMaterial;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.client.extensions.common.IClientItemExtensions;
+import org.jetbrains.annotations.NotNull;
+import software.bernie.geckolib.animatable.GeoItem;
+import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache;
+import software.bernie.geckolib.core.animatable.instance.SingletonAnimatableInstanceCache;
+import software.bernie.geckolib.core.animation.*;
+import software.bernie.geckolib.core.object.PlayState;
+
+import java.util.function.Consumer;
+
+public class PurpleArmor extends NonEnchantArmor implements GeoItem {
+    private AnimatableInstanceCache cache = new SingletonAnimatableInstanceCache(this);
+    public PurpleArmor(ArmorMaterial material, Type type, Properties properties) {
+        super(material, type, properties);
+    }
+
+    private PlayState predicate(AnimationState animationState){
+        animationState.getController().setAnimation(RawAnimation.begin().then("idle", Animation.LoopType.LOOP));
+        return PlayState.CONTINUE;
+    }
+
+    @Override
+    public void registerControllers(AnimatableManager.ControllerRegistrar controllerRegistrar) {
+        controllerRegistrar.add(new AnimationController(this, "controller", 0, this::predicate));
+    }
+
+    @Override
+    public AnimatableInstanceCache getAnimatableInstanceCache() {
+        return cache;
+    }
+}
