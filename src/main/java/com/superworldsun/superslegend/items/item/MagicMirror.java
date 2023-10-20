c5235116801c19bbd9af4802b8f6708f2d366fc7
Daripher <saelver.serbek@gmail.com>
2023-10-20 12:31:45 +0500

    Fixed magic mirror not working

--- src/main/java/com/superworldsun/superslegend/items/item/MagicMirror.java
+++ src/main/java/com/superworldsun/superslegend/items/item/MagicMirror.java
@@ -1,134 +1,129 @@
 package com.superworldsun.superslegend.items.item;
 
-import com.superworldsun.superslegend.SupersLegendMain;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.NbtUtils;
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.UseAnim;
 import net.minecraft.world.level.Level;
-import net.minecraftforge.fml.common.Mod;
+import net.minecraft.world.level.LightLayer;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-import java.util.Random;
-
-@Mod.EventBusSubscriber(modid = SupersLegendMain.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class MagicMirror extends Item {
+	private static final int USE_DURATION = 25;
 
-    private static int duration = 25;
+	public MagicMirror(Properties properties) {
+		super(properties);
+	}
 
-    public MagicMirror(Properties pProperties) {
-        super(pProperties);
-    }
+	@Override
+	public void inventoryTick(@NotNull ItemStack stack, @NotNull Level level, @NotNull Entity entity, int slot, boolean isSelected) {
+		if (isPositionSafe(level, entity)) {
+			setReturnPosition(stack, entity.blockPosition());
+		}
+	}
 
+	@Override
+	public @NotNull InteractionResultHolder<ItemStack> use(@NotNull Level level, Player player, @NotNull InteractionHand hand) {
+		ItemStack stack = player.getItemInHand(hand);
+		player.startUsingItem(hand);
+		return new InteractionResultHolder<>(InteractionResult.SUCCESS, stack);
+	}
 
-    //TODO, the item doesn't have any errors but the magic mirror isn't working.
-    @Override
-    public @NotNull InteractionResultHolder<ItemStack> use(@NotNull Level level, Player player, @NotNull InteractionHand hand) {
-        ItemStack stack = player.getItemInHand(hand);
-        player.startUsingItem(hand);
-        return new InteractionResultHolder<ItemStack>(InteractionResult.SUCCESS, stack);
-    }
+	@Override
+	public void onUseTick(@NotNull Level level, LivingEntity entity, @NotNull ItemStack stack, int remainingUseDuration) {
+		RandomSource rand = entity.level().random;
+		for (int i = 0; i < 45; i++) {
+			entity.level().addParticle(ParticleTypes.CLOUD,
+					entity.xo + (rand.nextBoolean() ? -1 : 1) * Math.pow(rand.nextFloat(), 1) * 2,
+					entity.yo + rand.nextFloat() * 3 - 2,
+					entity.zo + (rand.nextBoolean() ? -1 : 1) * Math.pow(rand.nextFloat(), 1) * 2,
+					0,
+					0.105D,
+					0);
+		}
+	}
 
-    public void onUsingTick (ItemStack stack, LivingEntity entity, int count)
-    {
-        Random rand = (Random) entity.level().random;
-        for (int i = 0; i < 45; i++)
-        {
-            entity.level().addParticle(ParticleTypes.CLOUD,
-                    entity.xo + (rand.nextBoolean() ? -1 : 1) * Math.pow(rand.nextFloat(), 1) * 2,
-                    entity.yo + rand.nextFloat() * 3 - 2,
-                    entity.zo + (rand.nextBoolean() ? -1 : 1) * Math.pow(rand.nextFloat(), 1) * 2,
-                    0, 0.105D, 0);
-        }
-    }
+	@Override
+	public @NotNull ItemStack finishUsingItem(@NotNull ItemStack stack, Level level, @NotNull LivingEntity entity) {
+		if (level.isClientSide) {
+			return stack;
+		}
+		ServerPlayer player = (ServerPlayer) entity;
+		if (!level.dimension().equals(Level.OVERWORLD)) {
+			player.displayClientMessage(Component.literal("Can only use in OverWorld"), true);
+			return stack;
+		}
+		BlockPos returnLoc = getReturnPosition(stack);
+		if (returnLoc == null) {
+			player.displayClientMessage(Component.literal("No position saved"), true);
+			return stack;
+		}
+		if (player.isPassenger()) {
+			player.stopRiding();
+		}
+		teleportEntity(entity, returnLoc);
+		BlockPos currentPos = player.blockPosition();
+		level.playSound(null,
+				currentPos.getX(),
+				currentPos.getY(),
+				currentPos.getZ(),
+				SoundEvents.CHORUS_FRUIT_TELEPORT,
+				SoundSource.PLAYERS,
+				1f,
+				1f);
+		player.displayClientMessage(Component.literal("Returned to saved position"), true);
+		return stack;
+	}
 
-    @Override
-    public @NotNull ItemStack finishUsingItem(@NotNull ItemStack stack, Level level, @NotNull LivingEntity entity)
-    {
-        if (!level.isClientSide)
-        {
-            ServerPlayer player = (ServerPlayer) entity;
+	@Override
+	public @NotNull UseAnim getUseAnimation(@NotNull ItemStack stack) {
+		return UseAnim.BOW;
+	}
 
-            if(level.dimension().equals(level.OVERWORLD)) //if dimension is Overworld
-            {
-                BlockPos returnLoc = getPosition(stack); //get saved position
-                if(returnLoc != null) //saved location not null
-                {
-                    BlockPos currentPos = player.blockPosition();
+	@Override
+	public int getUseDuration(@NotNull ItemStack stack) {
+		return USE_DURATION;
+	}
 
-                    if (player.isPassenger())
-                    {
-                        player.stopRiding();
-                    }
+	@Override
+	public boolean isFoil(@NotNull ItemStack stack) {
+		return getReturnPosition(stack) != null;
+	}
 
-                    setPositionAndUpdate(entity, level, returnLoc);
-                    level.playSound(null, currentPos.getX(), currentPos.getY(), currentPos.getZ(), SoundEvents.CHORUS_FRUIT_TELEPORT, SoundSource.PLAYERS, 1f, 1f);
-                    //TODO, i dont know what the new "TranslationTextComponent" is for 1.20.1, fix these 3 messages
-                    //player.sendStatusMessage(new TranslationTextComponent("Returned to saved position"), true);
-                }
-                else
-                {
-                    //player.displayClientMessage(new TranslationTextComponent("No position saved"), true);
-                    return stack;
-                }
-            }
-            else
-            {
-                //player.displayClientMessage(new TranslationTextComponent("Can only use in OverWorld"), true);
-            }
-        }
+	public void teleportEntity(LivingEntity entity, BlockPos pos) {
+		entity.teleportTo(pos.getX() + 0.5F, pos.getY() + 0.6F, pos.getZ() + 0.5F);
+		entity.fallDistance = 0;
+	}
 
-        return stack;
-    }
+	private static boolean isPositionSafe(@NotNull Level level, @NotNull Entity entity) {
+		return entity.position().y > 60 && level.getBrightness(LightLayer.SKY, entity.blockPosition()) > 11
+				&& !level.getBlockState(entity.blockPosition().below()).isAir();
+	}
 
-    @Override
-    public @NotNull UseAnim getUseAnimation (@NotNull ItemStack stack)
-    {
-        return UseAnim.BOW;
-    }
+	public static @Nullable BlockPos getReturnPosition(ItemStack stack) {
+		CompoundTag tag = stack.getTag();
+		if (tag != null && tag.contains("SavedPos")) {
+			return NbtUtils.readBlockPos(tag.getCompound("SavedPos"));
+		}
+		return null;
+	}
 
-    @Override
-    public int getUseDuration(@NotNull ItemStack stack)
-    {
-        return duration;
-    }
-
-    public void setPositionAndUpdate(LivingEntity entity, Level level, BlockPos bedLoc)
-    {
-        entity.teleportTo(bedLoc.getX() + 0.5F, bedLoc.getY() + 0.6F, bedLoc.getZ() + 0.5F);
-        entity.fallDistance = 0;
-    }
-
-    @Override
-    public boolean isFoil(@NotNull ItemStack stack) {
-        return getPosition(stack) != null;
-    }
-
-    public static BlockPos getPosition(ItemStack stack)
-    {
-        if (!stack.hasTag())
-        {
-            return null;
-        }
-
-        CompoundTag tags = stack.getTag();
-
-        if (tags.contains("pos"))
-        {
-            return NbtUtils.readBlockPos((CompoundTag) tags.get("pos"));
-        }
-        return null;
-    }
-
+	public static void setReturnPosition(ItemStack stack, BlockPos pos) {
+		stack.getOrCreateTag().put("SavedPos", NbtUtils.writeBlockPos(pos));
+	}
 }
