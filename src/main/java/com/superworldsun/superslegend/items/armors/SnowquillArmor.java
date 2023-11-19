e59c3cd7de72337a8a4b7cdd5622b624bf9dd767
superworldsun <superworldsun@gmail.com>
2023-11-19 00:19:18 -0500

    Updated SnowquillArmor

--- src/main/java/com/superworldsun/superslegend/items/armors/SnowquillArmor.java
+++ src/main/java/com/superworldsun/superslegend/items/armors/SnowquillArmor.java
@@ -1,12 +1,16 @@
 package com.superworldsun.superslegend.items.armors;
 
+import com.superworldsun.superslegend.client.render.armor.GeoArmorRendererExtension;
 import com.superworldsun.superslegend.items.customclass.NonEnchantArmor;
 import net.minecraft.world.item.ArmorMaterial;
+import net.minecraftforge.client.extensions.common.IClientItemExtensions;
 import software.bernie.geckolib.animatable.GeoItem;
 import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache;
 import software.bernie.geckolib.core.animatable.instance.SingletonAnimatableInstanceCache;
 import software.bernie.geckolib.core.animation.*;
 import software.bernie.geckolib.core.object.PlayState;
+
+import java.util.function.Consumer;
 
 public class SnowquillArmor extends NonEnchantArmor implements GeoItem {
     private AnimatableInstanceCache cache = new SingletonAnimatableInstanceCache(this);
@@ -14,14 +18,20 @@ public class SnowquillArmor extends NonEnchantArmor implements GeoItem {
         super(material, type, properties);
     }
 
-    private PlayState predicate(AnimationState animationState){
+    @Override
+    public void initializeClient(Consumer<IClientItemExtensions> consumer) {
+        GeoArmorRendererExtension<SnowquillArmor> extension = new GeoArmorRendererExtension<>("snow_quill");
+        consumer.accept(extension);
+    }
+
+    private PlayState predicate(AnimationState<SnowquillArmor> animationState) {
         animationState.getController().setAnimation(RawAnimation.begin().then("idle", Animation.LoopType.LOOP));
         return PlayState.CONTINUE;
     }
 
     @Override
     public void registerControllers(AnimatableManager.ControllerRegistrar controllerRegistrar) {
-        controllerRegistrar.add(new AnimationController(this, "controller", 0, this::predicate));
+        controllerRegistrar.add(new AnimationController<>(this, "controller", 0, this::predicate));
     }
 
     @Override
