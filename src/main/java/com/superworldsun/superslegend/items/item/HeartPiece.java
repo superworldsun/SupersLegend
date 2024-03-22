00e0a420e07e7163ca10ef9aa905d50b41d7ed02
superworldsun <superworldsun@gmail.com>
2024-03-22 07:39:00 -0400

    Add HeartPiece item class

---
+++ src/main/java/com/superworldsun/superslegend/items/item/HeartPiece.java
@@ -0,0 +1,35 @@
+package com.superworldsun.superslegend.items.item;
+
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import org.jetbrains.annotations.NotNull;
+
+import javax.annotation.Nullable;
+import java.util.List;
+
+public class HeartPiece extends Item {
+    public HeartPiece(Properties pProperties) {
+        super(pProperties);
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    @Override
+    public void appendHoverText(@NotNull ItemStack stack, @Nullable Level level, @NotNull List<Component> tooltip, @NotNull TooltipFlag flag) {
+        if(!Screen.hasShiftDown()) {
+            tooltip.add(Component.literal("A piece of a heart container").withStyle(ChatFormatting.RED));
+            tooltip.add(Component.literal("[Hold Shift for Info]").withStyle(ChatFormatting.DARK_GRAY));
+        }
+        else if(Screen.hasShiftDown()) {
+            tooltip.add(Component.literal("Collect 4 heart pieces and craft them together to make 1 Heart Container").withStyle(ChatFormatting.RED).withStyle(ChatFormatting.ITALIC));
+
+        }
+        super.appendHoverText(stack, level, tooltip, flag);
+    }
+}
