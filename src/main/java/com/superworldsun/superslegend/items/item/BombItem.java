38d86de7e1d422a52c9c08b86a79fdccf99476a1
superworldsun <superworldsun@gmail.com>
2024-03-25 00:49:18 -0400

    Updated/ported BombItem

--- src/main/java/com/superworldsun/superslegend/items/item/BombItem.java
+++ src/main/java/com/superworldsun/superslegend/items/item/BombItem.java
@@ -1,9 +1,16 @@
 package com.superworldsun.superslegend.items.item;
 
+import com.superworldsun.superslegend.entities.projectiles.bombs.BombEntity;
 import com.superworldsun.superslegend.items.customclass.NonEnchantItem;
+import com.superworldsun.superslegend.registries.EntityTypeInit;
+import com.superworldsun.superslegend.registries.SoundInit;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
@@ -16,9 +23,33 @@ import javax.annotation.Nullable;
 import java.util.List;
 
 public class BombItem extends NonEnchantItem {
-    // Bomb rendering, entity and logic code credited to Spelunkcraft contributor ntfwc
     public BombItem(Item.Properties properties) {
         super(properties);
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
+        ItemStack bombStack = player.getItemInHand(hand);
+
+        if (!level.isClientSide) {
+            if (player.isShiftKeyDown()) {
+                BombEntity bomb = new BombEntity(EntityTypeInit.BOMB.get(), level);
+                bomb.setPos(player.getX(), player.getY(), player.getZ());
+                level.playSound(null, player.blockPosition(), SoundInit.BOMB_FUSE.get(), SoundSource.PLAYERS, 1.0F, 1.0F);
+                level.addFreshEntity(bomb);
+            } else {
+                BombEntity bombEntity = new BombEntity(player, level);
+                float pitch = 0;
+                float throwingForce = 0.7F;
+                bombEntity.shootFromRotation(player, player.xRotO, player.yRotO, pitch, throwingForce, 0.9F);
+                level.playSound(null, player.blockPosition(), SoundInit.BOMB_FUSE.get(), SoundSource.PLAYERS, 1.0F, 1.0F);
+                level.addFreshEntity(bombEntity);
+            }
+            if (!player.isCreative()) {
+                bombStack.shrink(1);
+            }
+        }
+        return InteractionResultHolder.consume(bombStack);
     }
 
     @OnlyIn(Dist.CLIENT)
