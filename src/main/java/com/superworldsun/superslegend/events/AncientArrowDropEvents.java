6f4625af617832939c8b9b4773daf096426b6640
superworldsun <superworldsun@gmail.com>
2025-10-10 22:05:31 -0400

    Update AncientArrowDropEvents, Added config

--- src/main/java/com/superworldsun/superslegend/events/AncientArrowDropEvents.java
+++ src/main/java/com/superworldsun/superslegend/events/AncientArrowDropEvents.java
@@ -2,6 +2,8 @@ package com.superworldsun.superslegend.events;
 
 import com.superworldsun.superslegend.SupersLegendMain;
 import com.superworldsun.superslegend.entities.projectiles.arrows.AncientArrowEntity;
+import com.superworldsun.superslegend.Config;
+import com.superworldsun.superslegend.client.config.SupersLegendConfig;
 
 import net.minecraftforge.event.entity.living.LivingDropsEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
@@ -10,10 +12,13 @@ import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 
 @EventBusSubscriber(modid = SupersLegendMain.MOD_ID)
 public class AncientArrowDropEvents {
-    // TODO MAKE A CONFIG TO TURN THIS OFF FOR PLAYERS UPON DEATH.
+    //TODO Make sure config works
     @SubscribeEvent(priority = EventPriority.LOWEST)
     public static void onLivingDrops(LivingDropsEvent event) {
-        if (event.getSource().getDirectEntity() instanceof AncientArrowEntity)
-            event.getDrops().clear();
+        if (event.getSource().getDirectEntity() instanceof AncientArrowEntity) {
+            if (Config.disableAncientArrowDrops()) {
+                event.getDrops().clear();
+            }
+        }
     }
 }
