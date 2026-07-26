35044d55b5aa372e31148ace32b76eac24890c87
superworldsun <superworldsun@gmail.com>
2026-07-26 06:07:34 -0400

    Add DekuFlowerBlock, ported.

---
+++ src/main/java/com/superworldsun/superslegend/blocks/DekuFlowerBlock.java
@@ -0,0 +1,26 @@
+package com.superworldsun.superslegend.blocks;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
+
+public class DekuFlowerBlock extends Block {
+    private static final VoxelShape SHAPE = Block.box(2.0D, 0.0D, 2.0D, 14.0D, 6.0D, 14.0D);
+
+    public DekuFlowerBlock(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+        return SHAPE;
+    }
+
+    @Override
+    public VoxelShape getCollisionShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+        return SHAPE;
+    }
+}
