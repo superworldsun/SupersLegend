a241a9515f4c3b9a32ae2c062acceda8f310ada5
superworldsun <superworldsun@gmail.com>
2026-07-29 09:42:56 -0400

    Update ClientModHandler

--- src/main/java/com/superworldsun/superslegend/client/handler/ClientModHandler.java
+++ src/main/java/com/superworldsun/superslegend/client/handler/ClientModHandler.java
@@ -5,27 +5,23 @@ import com.superworldsun.superslegend.client.model.FanModel;
 import com.superworldsun.superslegend.client.model.ModelLayers;
 import com.superworldsun.superslegend.client.model.OpenOwlStatueModel;
 import com.superworldsun.superslegend.client.model.OwlStatueModel;
+import com.superworldsun.superslegend.client.model.entities.DekuLinkBubbleModel;
 import com.superworldsun.superslegend.client.model.hooks.HookshotModel;
 import com.superworldsun.superslegend.client.model.hooks.LongshotModel;
 import com.superworldsun.superslegend.client.model.player.DekuPlayerModel;
 import com.superworldsun.superslegend.client.model.player.GoronPlayerModel;
 import com.superworldsun.superslegend.client.model.player.ZoraPlayerModel;
 import com.superworldsun.superslegend.client.render.blocks.*;
-import com.superworldsun.superslegend.client.render.hookshot.HookshotRender;
 import com.superworldsun.superslegend.client.screen.PostboxScreen;
+import com.superworldsun.superslegend.client.screen.RupeeTradeScreen;
 import com.superworldsun.superslegend.registries.BlockEntityInit;
-import com.superworldsun.superslegend.registries.ContainerInit;
 import com.superworldsun.superslegend.registries.MenuTypeInit;
 import net.minecraft.client.gui.screens.MenuScreens;
-import net.minecraft.client.renderer.entity.EntityRenderers;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-
-import static com.superworldsun.superslegend.registries.EntityTypeInit.HOOKSHOT_ENTITY;
-
 
 @Mod.EventBusSubscriber(modid = SupersLegendMain.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class ClientModHandler {
@@ -33,6 +29,7 @@ public class ClientModHandler {
     public static void clientSetup(FMLClientSetupEvent event) {
         event.enqueueWork(() -> {
             MenuScreens.register(MenuTypeInit.POSTBOX_MENU.get(), PostboxScreen::new);
+            MenuScreens.register(MenuTypeInit.RUPEE_TRADE_MENU.get(), RupeeTradeScreen::new);
         });
     }
 
@@ -56,5 +53,6 @@ public class ClientModHandler {
         event.registerLayerDefinition(ModelLayers.GORON_PLAYER, GoronPlayerModel::createLayer);
         event.registerLayerDefinition(ModelLayers.ZORA_PLAYER, ZoraPlayerModel::createLayer);
         event.registerLayerDefinition(ModelLayers.DEKU_PLAYER, DekuPlayerModel::createLayer);
+        event.registerLayerDefinition(ModelLayers.DEKU_MAGIC_BUBBLE, DekuLinkBubbleModel::createBodyLayer);
     }
 }
