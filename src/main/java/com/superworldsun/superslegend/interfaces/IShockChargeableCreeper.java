2977bda57a3d23b7522ec2bf1e35c8149c23b82c
superworldsun <superworldsun@gmail.com>
2026-07-29 09:25:49 -0400

    Add IShockChargeableCreeper, can charge creeper without setting them on fire

---
+++ src/main/java/com/superworldsun/superslegend/interfaces/IShockChargeableCreeper.java
@@ -0,0 +1,6 @@
+package com.superworldsun.superslegend.interfaces;
+
+/** Allows a creeper to be powered without applying a real lightning strike. */
+public interface IShockChargeableCreeper {
+    void superslegend$setShockCharged();
+}
