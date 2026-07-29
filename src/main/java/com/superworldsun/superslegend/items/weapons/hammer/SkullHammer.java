bd42889560f030cfc210b467f6dcd71fa95adfcf
superworldsun <superworldsun@gmail.com>
2026-07-29 09:15:10 -0400

    Update SkullHammer

--- src/main/java/com/superworldsun/superslegend/items/weapons/hammer/SkullHammer.java
+++ src/main/java/com/superworldsun/superslegend/items/weapons/hammer/SkullHammer.java
@@ -5,9 +5,8 @@ import com.superworldsun.superslegend.util.ItemToolTiers;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.EquipmentSlot;
-import net.minecraft.world.entity.player.Player;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
@@ -22,34 +21,19 @@ public class SkullHammer extends HammerItem
 {
     public SkullHammer(Properties properties)
     {
-        super(ItemToolTiers.SKULL_HAMMER, 2, new Properties());
+        super(ItemToolTiers.SKULL_HAMMER, 2, properties);
     }
 
-    //TODO When breaking plants there is no particle effect or sound played
     //TODO Add blocks to the Init, list isnt full
-    //TODO Add a sound for when the hammer hits a block & entity
     @Override
     protected int getLeftClickCooldown() {
         return 24;
     }
-
-    public void inventoryTick(@NotNull ItemStack stack, @NotNull Level world, @NotNull Entity entity, int itemSlot, boolean isSelected)
-    {
-        if(entity instanceof Player)
-        {
-            Player player = (Player)entity;
-            ItemStack equipped = player.getMainHandItem();
-            {
-                if(stack == equipped)
-                {
-                    if(player.hasItemInSlot(EquipmentSlot.OFFHAND))
-                    {
-                        player.spawnAtLocation(player.getOffhandItem());
-                        player.setItemSlot(EquipmentSlot.OFFHAND, ItemStack.EMPTY);
-                    }
-                }
-            }
-        }
+    //TODO Add a new sound for when the hammer hits a block & entity
+    @Override
+    protected SoundEvent getHitSound() {
+        // Placeholder kept separate so the Skull Hammer can receive its own custom sound.
+        return SoundEvents.ZOMBIE_ATTACK_IRON_DOOR;
     }
 
     @OnlyIn(Dist.CLIENT)
