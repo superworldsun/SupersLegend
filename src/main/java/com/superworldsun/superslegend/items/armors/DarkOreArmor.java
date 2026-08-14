b30208636bf3e8bc5d1a9ee31ffdc599b74840b4
superworldsun <superworldsun@gmail.com>
2026-08-14 08:02:40 -0400

    Add DarkOreArmor & Texture

--- src/main/java/com/superworldsun/superslegend/items/armors/DarkOreArmor.java
+++ src/main/java/com/superworldsun/superslegend/items/armors/DarkOreArmor.java
@@ -1,37 +1,32 @@
 package com.superworldsun.superslegend.items.armors;
 
+import com.superworldsun.superslegend.SupersLegendMain;
 import com.superworldsun.superslegend.items.customclass.NonEnchantArmor;
 
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ArmorMaterial;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
-import software.bernie.geckolib.animatable.GeoItem;
-import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache;
-import software.bernie.geckolib.core.animatable.instance.SingletonAnimatableInstanceCache;
-import software.bernie.geckolib.core.animation.*;
-import software.bernie.geckolib.core.object.PlayState;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 
-public class DarkOreArmor extends NonEnchantArmor implements GeoItem {
-    private AnimatableInstanceCache cache = new SingletonAnimatableInstanceCache(this);
+public class DarkOreArmor extends NonEnchantArmor {
     public DarkOreArmor(ArmorMaterial material, Type type, Properties properties) {
         super(material, type, properties);
     }
 
-    private PlayState predicate(AnimationState animationState){
-        animationState.getController().setAnimation(RawAnimation.begin().then("idle", Animation.LoopType.LOOP));
-        return PlayState.CONTINUE;
+    @Override
+    public boolean shouldCauseReequipAnimation(ItemStack oldStack, ItemStack newStack, boolean slotChanged) {
+        return slotChanged || oldStack.getItem() != newStack.getItem();
     }
 
+    @OnlyIn(Dist.CLIENT)
     @Override
-    public void registerControllers(AnimatableManager.ControllerRegistrar controllerRegistrar) {
-        controllerRegistrar.add(new AnimationController(this, "controller", 0, this::predicate));
-    }
-
-    @Override
-    public AnimatableInstanceCache getAnimatableInstanceCache() {
-        return cache;
+    public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlot slot, String type) {
+        String layer = slot == EquipmentSlot.LEGS ? "2" : "1";
+        return SupersLegendMain.MOD_ID + ":textures/armor/dark_ore_armor_layer_" + layer + ".png";
     }
 
     @Override
